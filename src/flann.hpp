a20ce09f97bd6f1f22eca29aeadf372a888235bd
yafshar <ya.afshar@gmail.com>
2018-02-19 17:51:32 -0500

    adding a flann header files

---
+++ src/flann.hpp
@@ -0,0 +1,6 @@
+#ifndef FLANN_H
+#define FLANN_H
+
+#include <flann/flann.h>
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include "polynomial.hpp"
 #include "eigen.hpp"
+#include "flann.hpp"
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
 
     std::cout << " d =  " << d << std::endl;
     std::cout << " r =  " << r << std::endl;
-    
+
     int i, n;
     n = 0;
     for (i = 0; i < p.binomial_coefficient(d + r, r); i++)
@@ -50,8 +51,32 @@ int main()
     delete[] alpha;
     delete[] value;
 
-    EMatrixXd a;
-    
+    EMatrixXd A;
+    A.resize(3, 3);
+    A << 1, 2, 3, 4, 5, 6, 7, 8, 10;
+    std::cout << "Here is the matrix A:" << std::endl;
+    std::cout << A << std::endl;
 
+    EVectorXd B;
+    B.resize(3);
+    B << 3, 3, 4;
+    std::cout << "Here is the vector B:" << std::endl;
+    std::cout << B << std::endl;
+
+    EVectorXd X = A.fullPivLu().solve(B);
+    std::cout << "The solution is: " << std::endl;
+    std::cout << X << std::endl;
+
+    auto relative_error = (A * X - B).norm() / B.norm(); // norm() is L2 norm
+    std::cout << "The relative error is:" << std::endl;
+    std::cout << relative_error << std::endl;
+
+    EVectorXd Y = A.partialPivLu().solve(B);
+    std::cout << "The solution is: " << std::endl;
+    std::cout << Y << std::endl;
+    
+    relative_error = (A * Y - B).norm() / B.norm(); // norm() is L2 norm
+    std::cout << "The relative error is:" << std::endl;
+    std::cout << relative_error << std::endl;
     return 0;
 }
