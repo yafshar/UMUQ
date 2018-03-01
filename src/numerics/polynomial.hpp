1c21ecbfbdfc006abe8ff6a6456dfe3782a299e5
ya.afshar <ya.afshar@gmail.com>
2018-03-01 10:40:17 -0500

    adding doxygen format description

--- src/numerics/polynomial.hpp
+++ src/numerics/polynomial.hpp
@@ -1,6 +1,12 @@
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
 
+/*! \brief Multivariate monomials with the degree of r in a space of d dimensions.
+ *
+ *  A (univariate) monomial in 1 variable x is simply any (non-negative integer) power of x:
+ *      1, x, x^2, x^3, ..., x^r
+ *  The highest exponent of x is termed the degree of the monomial.
+ */
 class polynomial
 {
 
@@ -72,15 +78,18 @@ inline T polynomial::arraysum(int arraysize, T *array)
 
 int polynomial::binomial_coefficient(int n, int k)
 {
-    //******************************************************************************/
-    //   Purpose:
-    //       computes the binomial coefficient c(n, k).
-    //   The formula used is:
-    //       c(n,k) = n! / ( n! * (n-k)! )
-    //   Parameters:
-    //       Input : values of n and k.
-    //       Output: binomial
-    //******************************************************************************/
+    /*! 
+    *   Purpose:
+    *       Computes the binomial coefficient c(n, k).  
+    *   The formula used is:
+    *       c(n,k) = n! / ( n! * (n-k)! )  
+    *       $c(n,k) = \frac{n\!}{( n\! * (n-k)! )}$ 
+    *   Parameters:
+    *       @param[in] n
+    *       @param[in] k
+    *       @param[out] binomial The binomial coefficient
+    */
+
     if (k < 0 || n < 0)
     {
         std::cout << std::endl;
@@ -116,16 +125,18 @@ int polynomial::binomial_coefficient(int n, int k)
 
 void polynomial::graded_reverse_lexicographic_order(int d, int r, int *x)
 {
-    //*****************************************************************************/
-    //   Purpose:
-    //     uses a reverse lexicographic order for next monomial, degree between 0 and r
-    //     all monomials in a d dimensional space, with degree r
-    //   Parameters:
-    //     Input : d, the spatial dimension
-    //             r, maximum degree
-    //             x, current monomial
-    //     Output: x, next monomial, last value in the sequence is r.
-    //*****************************************************************************/
+    /*! 
+    *   Purpose:
+    *       Use a reverse lexicographic order for next monomial, degree between 0 and r
+    *       all monomials in a d dimensional space, with degree r.
+    *
+    *   Parameters:
+    *       @param[in]  d   The spatial dimension
+    *       @param[in]  r   Maximum degree
+    *       @param[in]  x   Current monomial
+    *       @param[out] x   Next monomial, last value in the sequence is r.
+    */
+
     if (r < 0)
     {
         std::cout << std::endl;
@@ -200,33 +211,36 @@ void polynomial::graded_reverse_lexicographic_order(int d, int r, int *x)
 
 void polynomial::monomial_basis(int d, int r, int *&alpha)
 {
-    //******************************************************************************/
-    //   Purpose:
-    //     All monomials in a d dimensional space, with total degree r.
-    //   For example:
-    //       d = 2
-    //       r = 2
-    //
-    //       alpha[ 0],[ 1] = 0, 0 = x^0 y^0
-    //       alpha[ 2],[ 3] = 1, 0 = x^1 y^0
-    //       alpha[ 4],[ 5] = 0, 1 = x^0 y^1
-    //       alpha[ 6],[ 7] = 2, 0 = x^2 y^0
-    //       alpha[ 8],[ 9] = 1, 1 = x^1 y^1
-    //       alpha[10],[11] = 0, 2 = x^0 y^2
-    //
-    //       monomial_basis(2,2) = {1,    x,   y,  x^2, xy,  y^2}
-    //                     alpha = {0,0, 1,0, 0,1, 2,0, 1,1, 0,2}
-    //
-    //
-    //       monomial_basis(3,2) = {1,       x,     y,     z,    x^2,  xy,    xz,   y^2,    yz,    z^2  }
-    //                     alpha = {0,0,0, 1,0,0, 0,1,0, 0,0,1, 2,0,0 1,1,0, 1,0,1, 0,2,0, 0,1,1, 0,0,2 }
-    //
-    //   Parameters:
-    //     Input : d,     the spatial dimension
-    //             r,     maximum degree
-    //             alpha, undefined pointer
-    //     Output: alpha, pointer to monomial sequence
-    //******************************************************************************/
+    /*! 
+    *   Purpose:
+    *       All monomials in a d dimensional space, with total degree r.
+    *   
+    *   For example:
+    *       d = 2
+    *       r = 2
+    *
+    *       alpha[ 0],[ 1] = 0, 0 = x^0 y^0
+    *       alpha[ 2],[ 3] = 1, 0 = x^1 y^0
+    *       alpha[ 4],[ 5] = 0, 1 = x^0 y^1
+    *       alpha[ 6],[ 7] = 2, 0 = x^2 y^0
+    *       alpha[ 8],[ 9] = 1, 1 = x^1 y^1
+    *       alpha[10],[11] = 0, 2 = x^0 y^2
+    *
+    *       monomial_basis(2,2)   = {1,    x,   y,  x^2, xy,  y^2}
+    *                       alpha = {0,0, 1,0, 0,1, 2,0, 1,1, 0,2}
+    *
+    *
+    *       monomial_basis(3,2)   = {1,       x,     y,     z,    x^2,  xy,    xz,   y^2,    yz,    z^2  }
+    *                       alpha = {0,0,0, 1,0,0, 0,1,0, 0,0,1, 2,0,0 1,1,0, 1,0,1, 0,2,0, 0,1,1, 0,0,2 }
+    *
+    *
+    *   Parameters:
+    *       @param[in]  d       The spatial dimension
+    *       @param[in]  r       Maximum degree
+    *       @param[in]  alpha   Undefined pointer
+    *       @param[out] alpha   Pointer to monomial sequence
+    */
+    
     int j;
     int n;
 
@@ -268,16 +282,16 @@ void polynomial::monomial_basis(int d, int r, int *&alpha)
 template <typename T>
 void polynomial::monomial_value(int d, int r, int *alpha, T *x, T *&value)
 {
-    //******************************************************************************/
-    //   Purpose:
-    //     Evaluates a monomial at a point x
-    //   Parameters:
-    //     Input : d,     the spatial dimension
-    //             r,     maximum degree
-    //             alpha, the exponents of the monomial
-    //             x,     the coordinates of the evaluation points
-    //     Output: monomial_value, the array value of the monomial at point x
-    //******************************************************************************/
+    /*! \breif Evaluates a monomial at a point x.
+    *
+    *   Parameters:
+    *       @param[in]  d       The spatial dimension
+    *       @param[in]  r       Maximum degree
+    *       @param[in]  alpha   The exponents of the monomial
+    *       @param[in]  x       The coordinates of the evaluation points
+    *       @param[out] value   Monomial_value, the array value of the monomial at point x
+    */
+
     int n;
     n = polynomial::binomial_coefficient(d + r, r);
 
