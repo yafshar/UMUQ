39459231bc00e477b94ad855dd2686e56a55604a
Yaser Afshar <ya.afshar@gmail.com>
2018-03-11 16:56:09 -0400

    minor indentaion to space

--- src/misc/spawner.hpp
+++ src/misc/spawner.hpp
@@ -35,208 +35,208 @@
 class spawner
 {
   public:
-	void parse(char *line, char **argv)
-	{
-		/* if not the end of line ....... */
-		while (*line != '\0')
-		{
-			while (*line == ' ' || *line == '\t' || *line == '\n')
-			{
-				/* replace white spaces with 0 */
-				*line++ = '\0';
-			}
+    void parse(char *line, char **argv)
+    {
+        /* if not the end of line ....... */
+        while (*line != '\0')
+        {
+            while (*line == ' ' || *line == '\t' || *line == '\n')
+            {
+                /* replace white spaces with 0 */
+                *line++ = '\0';
+            }
 
-			/* save the argument position */
-			*argv++ = line;
+            /* save the argument position */
+            *argv++ = line;
 
-			/* skip the argument until ...*/
-			while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
-			{
-				line++;
-			}
-		}
+            /* skip the argument until ...*/
+            while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+            {
+                line++;
+            }
+        }
 
-		/* mark the end of argument list */
-		*argv = NULL;
-	}
+        /* mark the end of argument list */
+        *argv = NULL;
+    }
 
-	int execute_cmd(int me, char **argv, char *dir)
-	{
-		pid_t pid;
+    int execute_cmd(int me, char **argv, char *dir)
+    {
+        pid_t pid;
 
-		// fork a child process
-		if ((pid = fork()) < 0)
-		{
-			std::cout << "spanwer(" << me << "): forking child process failed!!!!" << std::endl;
-			return 1;
-		}
-		else if (pid == 0) // child process
-		{
-			if (dir != NULL)
-			{
-				// move to the specified directory
-				chdir(dir);
-			}
+        // fork a child process
+        if ((pid = fork()) < 0)
+        {
+            std::cout << "spanwer(" << me << "): forking child process failed!!!!" << std::endl;
+            return 1;
+        }
+        else if (pid == 0) // child process
+        {
+            if (dir != NULL)
+            {
+                // move to the specified directory
+                chdir(dir);
+            }
 
-			execvp(*argv, argv);
-		}
+            execvp(*argv, argv);
+        }
 
-		// wait for process to change state
-		waitpid(pid, NULL, 0);
+        // wait for process to change state
+        waitpid(pid, NULL, 0);
 
-		return 0;
-	}
+        return 0;
+    }
 
-	static int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
-	{
-		// deletes a name from the file system. It calls unlink for files, and rmdir for directories.
-		int rv = remove(fpath);
+    static int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+    {
+        // deletes a name from the file system. It calls unlink for files, and rmdir for directories.
+        int rv = remove(fpath);
 
-		// produces a message on the standard error output, describing the last error encountered during
-		// a call to a system or library function
-		if (rv)
-			perror(fpath);
+        // produces a message on the standard error output, describing the last error encountered during
+        // a call to a system or library function
+        if (rv)
+            perror(fpath);
 
-		return rv;
-	}
+        return rv;
+    }
 
-	int rmrf(char *path)
-	{
-		// file tree walk
-		// FTW_DEPTH: do a post-order traversal
-		// FTW_PHYS: do not follow symbolic links
-		return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
-	}
+    int rmrf(char *path)
+    {
+        // file tree walk
+        // FTW_DEPTH: do a post-order traversal
+        // FTW_PHYS: do not follow symbolic links
+        return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+    }
 
-	int cp(const char *from, const char *to)
-	{
-		int fd_to, fd_from;
-		char buf[4096];
-		ssize_t nread;
-		int saved_errno;
-		struct stat sb;
+    int cp(const char *from, const char *to)
+    {
+        int fd_to, fd_from;
+        char buf[4096];
+        ssize_t nread;
+        int saved_errno;
+        struct stat sb;
 
-		// O_RDONLY: Open for reading only
-		fd_from = open(from, O_RDONLY);
-		if (fd_from < 0)
-			return -1;
+        // O_RDONLY: Open for reading only
+        fd_from = open(from, O_RDONLY);
+        if (fd_from < 0)
+            return -1;
 
-		// get file status
-		fstat(fd_from, &sb);
-		if (S_ISDIR(sb.st_mode))
-		{
-			fd_to = -1;
-			goto out_error;
-		}
+        // get file status
+        fstat(fd_from, &sb);
+        if (S_ISDIR(sb.st_mode))
+        {
+            fd_to = -1;
+            goto out_error;
+        }
 
-		fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, sb.st_mode);
-		if (fd_to < 0)
-			goto out_error;
+        fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, sb.st_mode);
+        if (fd_to < 0)
+            goto out_error;
 
-		while (nread = read(fd_from, buf, sizeof buf), nread > 0)
-		{
-			char *out_ptr = buf;
-			ssize_t nwritten;
+        while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+        {
+            char *out_ptr = buf;
+            ssize_t nwritten;
 
-			do
-			{
-				nwritten = write(fd_to, out_ptr, nread);
-				if (nwritten >= 0)
-				{
-					nread -= nwritten;
-					out_ptr += nwritten;
-				}
-				else if (errno != EINTR)
-				{
-					goto out_error;
-				}
-			} while (nread > 0);
-		}
+            do
+            {
+                nwritten = write(fd_to, out_ptr, nread);
+                if (nwritten >= 0)
+                {
+                    nread -= nwritten;
+                    out_ptr += nwritten;
+                }
+                else if (errno != EINTR)
+                {
+                    goto out_error;
+                }
+            } while (nread > 0);
+        }
 
-		if (nread == 0)
-		{
-			if (close(fd_to) < 0)
-			{
-				fd_to = -1;
-				goto out_error;
-			}
-			else
-			{ // peh: added due to some issues on monte rosa
-				fsync(fd_to);
-			}
-			close(fd_from);
+        if (nread == 0)
+        {
+            if (close(fd_to) < 0)
+            {
+                fd_to = -1;
+                goto out_error;
+            }
+            else
+            { // peh: added due to some issues on monte rosa
+                fsync(fd_to);
+            }
+            close(fd_from);
 
-			/* Success! */
-			return 0;
-		}
+            /* Success! */
+            return 0;
+        }
 
-	out_error:
-		saved_errno = errno;
+    out_error:
+        saved_errno = errno;
 
-		close(fd_from);
-		if (fd_to >= 0)
-			close(fd_to);
+        close(fd_from);
+        if (fd_to >= 0)
+            close(fd_to);
 
-		errno = saved_errno;
-		return -1;
-	}
+        errno = saved_errno;
+        return -1;
+    }
 
-	int copy_from_dir(char *name)
-	{
-		DIR *dir;
-		struct dirent *ent;
+    int copy_from_dir(char *name)
+    {
+        DIR *dir;
+        struct dirent *ent;
 
-		// open a directory
-		dir = opendir(name);
-		if (dir == NULL)
-		{
-			// could not open directory
-			perror("The following error occurred");
-			return 1;
-		}
-		else
-		{
-			// read a directory
-			/* print all the files and directories within directory */
-			while ((ent = readdir(dir)) != NULL)
-			{
-				char source[256], dest[256];
+        // open a directory
+        dir = opendir(name);
+        if (dir == NULL)
+        {
+            // could not open directory
+            perror("The following error occurred");
+            return 1;
+        }
+        else
+        {
+            // read a directory
+            /* print all the files and directories within directory */
+            while ((ent = readdir(dir)) != NULL)
+            {
+                char source[256], dest[256];
 
-				sprintf(source, "%s/%s", name, ent->d_name);
-				sprintf(dest, "./%s", ent->d_name);
-				cp(source, dest);
-			}
-			closedir(dir);
-		}
-		return 0;
-	}
+                sprintf(source, "%s/%s", name, ent->d_name);
+                sprintf(dest, "./%s", ent->d_name);
+                cp(source, dest);
+            }
+            closedir(dir);
+        }
+        return 0;
+    }
 
-	int copy_file(const char *dirname, const char *filename)
-	{
-		DIR *dir;
+    int copy_file(const char *dirname, const char *filename)
+    {
+        DIR *dir;
 
-		// open a directory
-		dir = opendir(dirname);
-		if (dir == NULL)
-		{
-			// could not open directory
-			perror("The following error occurred");
-			return 1;
-		}
-		else
-		{
-			char source[256], dest[256];
+        // open a directory
+        dir = opendir(dirname);
+        if (dir == NULL)
+        {
+            // could not open directory
+            perror("The following error occurred");
+            return 1;
+        }
+        else
+        {
+            char source[256], dest[256];
 
-			sprintf(source, "%s/%s", dirname, filename);
-			sprintf(dest, "./%s", filename);
+            sprintf(source, "%s/%s", dirname, filename);
+            sprintf(dest, "./%s", filename);
 
-			cp(source, dest);
+            cp(source, dest);
 
-			closedir(dir);
-		}
+            closedir(dir);
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 };
 
 #endif
--- src/misc/timer.hpp
+++ src/misc/timer.hpp
@@ -15,45 +15,45 @@
 class UMTimer
 {
   public:
-	UMTimer() {
-		tic();
-	}
+    UMTimer() {
+        tic();
+    }
 
-	void tic()
-	{
-		t1_internal = my_gettime();
-	}
+    void tic()
+    {
+        t1_internal = my_gettime();
+    }
 
-	void toc()
-	{
-		t2_internal = my_gettime();
+    void toc()
+    {
+        t2_internal = my_gettime();
 
-		double elapsed_seconds = t2_internal - t1_internal;
+        double elapsed_seconds = t2_internal - t1_internal;
 
-		//output the elapsed time to terminal 
-		std::cout << " It took " << std::to_string(elapsed_seconds) << " seconds" << std::endl;
-	}
+        //output the elapsed time to terminal 
+        std::cout << " It took " << std::to_string(elapsed_seconds) << " seconds" << std::endl;
+    }
 
-	void toc(std::string timing_name)
-	{
-		t2_internal = my_gettime();
+    void toc(std::string timing_name)
+    {
+        t2_internal = my_gettime();
 
-		double elapsed_seconds = t2_internal - t1_internal;
+        double elapsed_seconds = t2_internal - t1_internal;
 
-		//output the elapsed time to terminal 
-		std::cout << timing_name << " took " << std::to_string(elapsed_seconds) << " seconds" << std::endl;
-	}
+        //output the elapsed time to terminal 
+        std::cout << timing_name << " took " << std::to_string(elapsed_seconds) << " seconds" << std::endl;
+    }
 
   private:
-	double t1_internal;
-	double t2_internal;
+    double t1_internal;
+    double t2_internal;
 
-	double my_gettime()
-	{
-		struct timeval t;
-		gettimeofday(&t, NULL);
-		return (double)t.tv_sec + (double)t.tv_usec * 1.0E-6;
-	}
+    double my_gettime()
+    {
+        struct timeval t;
+        gettimeofday(&t, NULL);
+        return (double)t.tv_sec + (double)t.tv_usec * 1.0E-6;
+    }
 };
 
 #endif
