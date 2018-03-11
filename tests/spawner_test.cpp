3c971adda815a274c438ee49d99d8d1ce3ba7eaa
Yaser Afshar <ya.afshar@gmail.com>
2018-03-11 16:57:48 -0400

    minor indentaion to space

--- tests/spawner_test.cpp
+++ tests/spawner_test.cpp
@@ -9,62 +9,62 @@
 // Tests parse
 TEST(parse_test, HandlesZeroInput)
 {
-	spawner s;
+    spawner s;
 
-	char line[BUFLEN];
-	char *largv[BUFLEN];
+    char line[BUFLEN];
+    char *largv[BUFLEN];
 
-	sprintf(line, " ");
-	s.parse(line, largv);
+    sprintf(line, " ");
+    s.parse(line, largv);
 
-	const char *targv[0];
-	targv[0] = "";
+    const char *targv[0];
+    targv[0] = "";
 
-	EXPECT_STREQ(largv[0], targv[0]);
+    EXPECT_STREQ(largv[0], targv[0]);
 
-	//checking for tab and next line
-	sprintf(line, "\t  \n");
+    //checking for tab and next line
+    sprintf(line, "\t  \n");
 
-	s.parse(line, largv);
-	EXPECT_STREQ(largv[0], targv[0]);
+    s.parse(line, largv);
+    EXPECT_STREQ(largv[0], targv[0]);
 };
 
 TEST(parse_test, HandlesInput)
 {
-	spawner s;
+    spawner s;
 
-	char line[BUFLEN];
-	char *largv[BUFLEN];
+    char line[BUFLEN];
+    char *largv[BUFLEN];
 
-	sprintf(line, "sh doall.sh");
-	s.parse(line, largv);
+    sprintf(line, "sh doall.sh");
+    s.parse(line, largv);
 
-	const char *targv[2];
-	targv[0] = "sh";
-	targv[1] = "doall.sh";
+    const char *targv[2];
+    targv[0] = "sh";
+    targv[1] = "doall.sh";
 
-	for (int i = 0; i < 2; i++)
-	{
-		EXPECT_STREQ(largv[i], targv[i]);
-	}
+    for (int i = 0; i < 2; i++)
+    {
+        EXPECT_STREQ(largv[i], targv[i]);
+    }
 
-	sprintf(line, "bash doall.sh out 2>&1");
-	s.parse(line, largv);
+    sprintf(line, "bash doall.sh out 2>&1");
+    s.parse(line, largv);
 
-	const char *ttargv[4];
-	ttargv[0] = "bash";
-	ttargv[1] = "doall.sh";
-	ttargv[2] = "out";
-	ttargv[3] = "2>&1";
+    const char *ttargv[4];
+    ttargv[0] = "bash";
+    ttargv[1] = "doall.sh";
+    ttargv[2] = "out";
+    ttargv[3] = "2>&1";
 
-	for (int i = 0; i < 4; i++)
-	{
-		EXPECT_STREQ(largv[i], ttargv[i]);
-	}
+    for (int i = 0; i < 4; i++)
+    {
+        EXPECT_STREQ(largv[i], ttargv[i]);
+    }
 };
 
 int main(int argc, char **argv)
 {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
