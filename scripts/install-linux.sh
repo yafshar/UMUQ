e7cfdcb9ff37fa80afd565c4782d4e981a81965d
ya.afshar <ya.afshar@gmail.com>
2018-03-15 11:14:00 -0400

    minor correction

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -13,24 +13,22 @@ if [ "${TRAVIS_SUDO}" = "true" ]; then
     sudo update-alternatives --remove-all gcc 
     sudo update-alternatives --remove-all g++
     sudo update-alternatives --remove-all gfortran
+    
     sudo apt-get install -y gcc-5 g++-5 gfortran-5 clang-3.7
 
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
-    sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-5 10
+    # sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10
+    # sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10
+    # sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-5 10
     
-    sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 20
-    sudo update-alternatives --set cc /usr/bin/gcc
+    # sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 20
+    # sudo update-alternatives --set cc /usr/bin/gcc
 
-    sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 20
-    sudo update-alternatives --set c++ /usr/bin/g++
+    # sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 20
+    # sudo update-alternatives --set c++ /usr/bin/g++
 
-    sudo update-alternatives --install /usr/bin/fc fc /usr/bin/gfortran 20
-    sudo update-alternatives --set fc /usr/bin/fc
-
-    sudo update-alternatives --config gcc
-    sudo update-alternatives --config g++
-    sudo update-alternatives --config gfortran
+    # sudo update-alternatives --config gcc
+    # sudo update-alternatives --config g++
+    # sudo update-alternatives --config gfortran
 
 	sudo apt-get install -y mpich 
 	sudo apt-get install -y libmpich-dev
--- scripts/log-config.sh
+++ scripts/log-config.sh
@@ -20,3 +20,7 @@ ${CXX} -v || echo "${CXX} does not seem to support the -v flag"
 echo "C compiler version:"
 ${CC} --version || echo "${CXX} does not seem to support the --version flag"
 ${CC} -v || echo "${CXX} does not seem to support the -v flag"
+
+echo "FORTRAN compiler version:"
+${FC} --version || echo "${FC} does not seem to support the --version flag"
+${FC} -v || echo "${FC} does not seem to support the -v flag"
