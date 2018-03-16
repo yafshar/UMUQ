d5f87a86424377e358ceb5cd6586a6e37d68479e
ya.afshar <ya.afshar@gmail.com>
2018-03-16 10:42:44 -0400

    adding to arraywrapper test

--- tests/array_test.cpp
+++ tests/array_test.cpp
@@ -3,19 +3,72 @@
 #include "../src/misc/array.hpp"
 #include "gtest/gtest.h"
 
-TEST(ArrayWrapper, HandlesVectors)
+TEST(arraywrapper_test, HandlesVectors)
 {
-    int *iarray;
-    iarray = new int[10];
+    int j;
 
-    for (int j = 0; j < 10; j++)
-        iarray[j] = j * 10;
-
-    ArrayWrapper<int> it(iarray, 10);
-    for (auto i = it.begin(); i != it.end(); i++)
+    // check for array of int
+    int *iarray = nullptr;
+    iarray = new int[1000];
+    for (j = 0; j < 1000; j++)
     {
-        std::cout << *i << std::endl;
+        iarray[j] = j * 10;
     }
+
+    j = 0;
+    ArrayWrapper<int> it(iarray, 1000);
+    for (auto i = it.begin(); i != it.end(); i++, j++)
+    {
+        EXPECT_EQ(*i, j * 10);
+    }
+
+    delete[] iarray;
+
+    // check for array of double
+    double *darray = nullptr;
+    darray = new double[100];
+    for (j = 0; j < 100; j++)
+    {
+        darray[j] = (double)j * 10.0;
+    }
+
+    j = 0;
+    ArrayWrapper<double> id(darray, 100);
+    for (auto i = id.begin(); i != id.end(); i++)
+    {
+        EXPECT_EQ(*i, (double)j * 10);
+        j++;
+    }
+
+    delete[] darray;
+
+    // check for array of structure
+    struct srt
+    {
+        int i;
+        int n;
+        double s;
+    };
+
+    srt *srtarray = nullptr;
+    srtarray = new srt[10];
+    for (j = 0; j < 10; j++)
+    {
+        srtarray[j].i = j;
+        srtarray[j].n = j * 10;
+        srtarray[j].s = (double)j * 10.0;
+    }
+
+    j = 0;
+    ArrayWrapper<srt> is(srtarray, 10);
+    for (auto i = is.begin(); i != is.end(); i++, j++)
+    {
+        EXPECT_EQ((*i).i, j);
+        EXPECT_EQ((*i).n, j * 10);
+        EXPECT_EQ((*i).s, (double)j * 10.0);
+    }
+
+    delete[] srtarray;
 };
 
 int main(int argc, char **argv)
