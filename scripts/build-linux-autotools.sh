bed69c57dbcaa4ff8eef53505f06c6c03454862a
Yaser Afshar <ya.afshar@gmail.com>
2018-04-08 17:59:42 -0400

    updating the script to make sure aclocal.m4 with macros from libtool 2.4.2

--- scripts/build-linux-autotools.sh
+++ scripts/build-linux-autotools.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+
+rm m4/libtool.m4
+rm m4/ltoptions.m4
+rm m4/ltsugar.m4
+rm m4/ltversion.m4
+rm m4/lt\~obsolete.m4
+
 aclocal
 # Create the configuration script
 autoreconf -i
