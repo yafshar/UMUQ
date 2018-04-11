cf4195c2780eb980321c35ec68aba8bdb551f9ce
ya.afshar <ya.afshar@gmail.com>
2018-04-11 18:10:08 -0400

    new update

--- src/data/runinfo.hpp
+++ src/data/runinfo.hpp
@@ -1,6 +1,6 @@
 #ifndef UMHBM_RUNINFO_H
 #define UMHBM_RUNINFO_H
-
+#include "../io/io.hpp"
 /*!
 *  \brief run info structure
 *    
@@ -35,6 +35,114 @@ struct runinfo_t
                   acceptance(nullptr),
                   SS(nullptr),
                   meantheta(nullptr){};
+
+    bool save(const char *fileName = "runinfo.txt")
+    {
+        io f;
+        if (f.openFile(fileName, f.out | f.app))
+        {
+            std::fstream &fs = f.getFstream();
+            fs << "Gen=\n";
+            fs << Gen << "\n";
+
+
+
+            f.closeFile();
+            return true;
+        }
+        return false;
+    }
 };
+
+void save_runinfo()
+{
+    int i, j;
+
+    /* allocate and initialize runinfo */
+    FILE *f = fopen("runinfo.txt", "w");
+
+    fprintf(f, "Gen=\n");
+    fprintf(f, "%d\n", runinfo.Gen);
+
+    fprintf(f, "CoefVar[%d]=\n", data.MaxStages);
+    for (i = 0; i < data.MaxStages; i++)
+        fprintf(f, "%.16lf\n", runinfo.CoefVar[i]);
+
+    fprintf(f, "p[%d]=\n", data.MaxStages);
+    for (i = 0; i < data.MaxStages; i++)
+        fprintf(f, "%.16lf\n", runinfo.p[i]);
+
+    fprintf(f, "currentuniques[%d]=\n", data.MaxStages);
+    for (i = 0; i < data.MaxStages; i++)
+        fprintf(f, "%d\n", runinfo.currentuniques[i]);
+
+    fprintf(f, "logselection[%d]=\n", data.MaxStages);
+    for (i = 0; i < data.MaxStages; i++)
+        fprintf(f, "%.16lf\n", runinfo.logselection[i]);
+
+    fprintf(f, "acceptance[%d]=\n", data.MaxStages);
+    for (i = 0; i < data.MaxStages; i++)
+        fprintf(f, "%.16lf\n", runinfo.acceptance[i]);
+
+    fprintf(f, "SS[%d][%d]=\n", data.Nth, data.Nth);
+    for (i = 0; i < data.Nth; i++)
+        for (j = 0; j < data.Nth; j++)
+            fprintf(f, "%.16lf\n", runinfo.SS[i][j]);
+
+    fprintf(f, "meantheta[%d][%d]\n", data.MaxStages, data.Nth);
+    for (i = 0; i < data.MaxStages; i++)
+        for (j = 0; j < data.Nth; j++)
+            fprintf(f, "%.16lf\n", runinfo.meantheta[i][j]);
+
+    fclose(f);
+}
+
+int load_runinfo()
+{
+    int i, j;
+    char header[256];
+
+    /* allocate and initialize runinfo */
+    FILE *f = fopen("runinfo.txt", "r");
+    if (f == NULL)
+        return 1;
+
+    fscanf(f, "%s", header);
+    fscanf(f, "%d", &runinfo.Gen);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        fscanf(f, "%lf\n", &runinfo.CoefVar[i]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        fscanf(f, "%lf\n", &runinfo.p[i]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        fscanf(f, "%d\n", &runinfo.currentuniques[i]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        fscanf(f, "%lf\n", &runinfo.logselection[i]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        fscanf(f, "%lf\n", &runinfo.acceptance[i]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.Nth; i++)
+        for (j = 0; j < data.Nth; j++)
+            fscanf(f, "%lf\n", &runinfo.SS[i][j]);
+
+    fscanf(f, "%s", header);
+    for (i = 0; i < data.MaxStages; i++)
+        for (j = 0; j < data.Nth; j++)
+            fscanf(f, "%lf\n", &runinfo.meantheta[i][j]);
+
+    fclose(f);
+
+    return 0;
+}
 
 #endif
