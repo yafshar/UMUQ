aabefdc81c9a640db621a1312ae8c3cd59eb2259
ya.afshar <ya.afshar@gmail.com>
2018-04-11 16:11:38 -0400

    updating the test parser to use the new parser class

--- tests/misc/parser_test.cpp
+++ tests/misc/parser_test.cpp
@@ -1,9 +1,8 @@
-#include <stdio.h>
-
+#include "core/core.hpp"
 #include "misc/parser.hpp"
 #include "gtest/gtest.h"
 
-#define BUFLEN 1024
+#define BUFLEN 256
 
 /*! Tests parse class in case of empty line or line with tabs and spaces
  *
@@ -12,22 +11,18 @@ TEST(parse_test, HandlesZeroInput)
 {
     parser p;
 
-    char line[BUFLEN];
-    char *largv[BUFLEN];
+    p.parse("     ");
 
-    sprintf(line, " ");
-    p.parse(line, largv);
+    std::string word = p.at<std::string>(0);
 
-    const char *targv[0];
-    targv[0] = "";
-
-    EXPECT_STREQ(largv[0], targv[0]);
+    EXPECT_EQ(word, "");
 
     //!checking for tab and next line
-    sprintf(line, "\t  \n");
+    p.parse("\t  \n");
 
-    p.parse(line, largv);
-    EXPECT_STREQ(largv[0], targv[0]);
+    word = p.at<std::string>(0);
+
+    EXPECT_EQ(word, "");
 };
 
 /*! 
@@ -38,40 +33,38 @@ TEST(parse_test, HandlesInput)
     parser p;
 
     char line[BUFLEN];
-    char *largv[BUFLEN];
 
     sprintf(line, "sh doall.sh");
-    p.parse(line, largv);
+    p.parse(line);
 
-    const char *targv[2];
-    targv[0] = "sh";
-    targv[1] = "doall.sh";
+    std::string word[4];
+    word[0] = "sh";
+    word[1] = "doall.sh";
 
     for (int i = 0; i < 2; i++)
     {
-        EXPECT_STREQ(largv[i], targv[i]);
+        EXPECT_EQ(word[i], p.at<std::string>(i));
     }
 
     //!testing space at the start of line
     sprintf(line, "   sh doall.sh");
-    p.parse(line, largv);
+    p.parse(line);
     for (int i = 0; i < 2; i++)
     {
-        EXPECT_STREQ(largv[i], targv[i]);
+        EXPECT_EQ(word[i], p.at<std::string>(i));
     }
 
     sprintf(line, "bash doall.sh out 2>&1");
-    p.parse(line, largv);
+    p.parse(line);
 
-    const char *ttargv[4];
-    ttargv[0] = "bash";
-    ttargv[1] = "doall.sh";
-    ttargv[2] = "out";
-    ttargv[3] = "2>&1";
+    word[0] = "bash";
+    word[1] = "doall.sh";
+    word[2] = "out";
+    word[3] = "2>&1";
 
     for (int i = 0; i < 4; i++)
     {
-        EXPECT_STREQ(largv[i], ttargv[i]);
+        EXPECT_EQ(word[i], p.at<std::string>(i));
     }
 };
 
@@ -83,56 +76,32 @@ TEST(parse_cmd, HandlesCmd)
     parser p;
 
     char line[BUFLEN];
-    char *largv[4];
-
-    double lowerbound = 0;
-    double upperbound = 0;
-
-    double prior_mu1 = 0;
-    double prior_mu2 = 0;
-
-    int PopSize = 0;
 
     int n = 4;
     while (n--)
     {
         sprintf(line, "B%d              %d.05           10%d.012", n, n, n);
-        p.parse(line, largv);
+        p.parse(line);
 
-        std::string str(largv[0]);
-
-        EXPECT_EQ(str, "B" + std::to_string(n));
-
-        p.parse(largv[1], lowerbound);
-        EXPECT_DOUBLE_EQ(lowerbound, 0.05 + (double)n);
-
-        p.parse(largv[2], upperbound);
-        EXPECT_DOUBLE_EQ(upperbound, 100.012 + (double)n);
+        EXPECT_EQ(p.at<std::string>(0), "B" + std::to_string(n));
+        EXPECT_DOUBLE_EQ(p.at<double>(1), 0.05 + (double)n);
+        EXPECT_DOUBLE_EQ(p.at<double>(2), 100.012 + (double)n);
     }
 
     n = 8;
     while (n--)
     {
         sprintf(line, " PopSize 5000%d ", n);
-        p.parse(line, largv);
-
-        std::string str(largv[0]);
-        EXPECT_EQ(str, "PopSize");
-
-        p.parse(largv[1], PopSize);
-        EXPECT_EQ(PopSize, 50000 + n);
+        p.parse(line);
+        EXPECT_EQ(p.at<std::string>(0), "PopSize");
+        EXPECT_EQ(p.at<int>(1), 50000 + n);
     }
 
     sprintf(line, "   prior_mu    1.0,0.1 ");
-    p.parse(line, largv);
+    p.parse(line);
 
-    std::string str(largv[0]);
-
-    p.parse(largv[1], prior_mu1);
-    EXPECT_DOUBLE_EQ(prior_mu1, 1.0);
-
-    p.parse(largv[2], prior_mu2);
-    EXPECT_DOUBLE_EQ(prior_mu2, 0.1);
+    EXPECT_DOUBLE_EQ(p.at<double>(1), 1.0);
+    EXPECT_DOUBLE_EQ(p.at<double>(2), 0.1);
 };
 
 int main(int argc, char **argv)
