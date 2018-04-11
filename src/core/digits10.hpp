a1f484b4984b41a49785f592531f4a0d1d33c82f
ya.afshar <ya.afshar@gmail.com>
2018-04-11 16:31:42 -0400

    update on commenting

--- src/core/digits10.hpp
+++ src/core/digits10.hpp
@@ -3,8 +3,9 @@
 
 #include <cmath> //ceil, log10
 
-// default implementation of digits10(), based on numeric_limits if specialized,
-// 0 for integer types, and log10(epsilon()) otherwise.
+/*!
+ * \brief Default digits10() class based on std::numeric_limits
+ */
 template <typename T,
 		  bool use_numeric_limits = std::numeric_limits<T>::is_specialized,
 		  bool is_integer = std::numeric_limits<T>::is_integer>
@@ -13,18 +14,30 @@ struct default_digits10
 	static int run() { return std::numeric_limits<T>::digits10; }
 };
 
+/*!
+ * \brief Floating point
+ */
 template <typename T>
-struct default_digits10<T, false, false> // Floating point
+struct default_digits10<T, false, false> 
 {
 	static int run() { return int(std::ceil(-std::log10(std::numeric_limits<T>::epsilon()))); }
 };
 
+/*!
+ * \brief Integer
+ */
 template <typename T>
-struct default_digits10<T, false, true> // Integer
+struct default_digits10<T, false, true> 
 {
 	static int run() { return 0; }
 };
 
+/*!
+ * \brief digits10(), implementation 
+ * It is based on std::numeric_limits if specialized,
+ * 0 for integer types, and log10(epsilon()) otherwise.
+ * \tparam T data type
+ */
 template <typename T>
 static inline int digits10()
 {
