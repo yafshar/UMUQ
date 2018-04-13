694d7556716959f6aae1d5145454640d89d37410
ya.afshar <ya.afshar@gmail.com>
2018-04-13 10:43:52 -0400

    Stats class with minelement, maxelement, sum, mean, stddev functionality

---
+++ src/numerics/stats.hpp
@@ -0,0 +1,123 @@
+#ifndef UMHBM_STATS_H
+#define UMHBM_STATS_H
+
+/*! \class stats
+*   \brief stats is a class which includes some statictics functionality
+*	
+*/
+struct stats
+{
+    /*!
+     * \brief Finds the smallest element in the array of data
+     * 
+     * \tparam T data type
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The smallest element in the array of data
+     */
+    template <typename T>
+    inline T minelement(T const *idata, int const nSize)
+    {
+        return *std::min_element(idata, idata + nSize);
+    }
+
+    /*!
+     * \brief Finds the greatest element in the array of data
+     * 
+     * \tparam T data type
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The greatest element in the array of data
+     */
+    template <typename T>
+    inline T maxelement(T const *idata, int const nSize)
+    {
+        return *std::max_element(idata, idata + nSize);
+    }
+
+    /*!
+     * \brief Finds the position of the smallest element in the array of data
+     * 
+     * \tparam T data type
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * 
+     * \returns The the position of the smallest element
+     */
+    template <typename T>
+    inline int min_at(T const *idata, int const nSize)
+    {
+        return (int)std::distance(idata, std::min_element(idata, idata + nSize));
+    }
+
+    /*!
+     * \brief Finds the position of the greatest element in the array of data
+     * 
+     * \tparam T data type
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The the position of the greatest element
+     */
+    template <typename T>
+    inline int max_at(T const *idata, int const nSize)
+    {
+        return (int)std::distance(idata, std::max_element(idata, idata + nSize));
+    }
+
+    /*!
+     * \brief Computes the sum of the elements in the array of data
+     * 
+     * \tparam T data type
+     * \tparam TOut type of return output result (default is double)
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The sum of the elements in the array of data
+     */
+    template <typename T, typename TOut = double>
+    inline TOut sum(T const *idata, int const nSize)
+    {
+        return (TOut)std::accumulate(idata, idata + nSize, (T)0);
+    }
+
+    /*!
+     * \brief Computes the mean of the elements in the array of data
+     * 
+     * \tparam T data type
+     * \tparam TOut type of return output result (default is double)
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The mean of the elements in the array of data
+     */
+    template <typename T, typename TOut = double>
+    inline TOut mean(T const *idata, const int nSize)
+    {
+        return (TOut)sum<T, TOut>(idata, nSize) / nSize;
+    }
+
+    /*!
+     * \brief Computes the standard deviation of the elements in the array of data
+     * 
+     * \tparam T data type
+     * \tparam TOut type of return output result (default is double)
+     * \param idata array of data
+     * \param nSize size of the array
+     * 
+     * \returns The standard deviation of the elements in the array of data
+     */
+    template <typename T, typename TOut = double>
+    inline TOut stddev(T const *idata, const int nSize)
+    {
+        TOut m = mean<T, TOut>(idata, nSize);
+        TOut s(0);
+        std::for_each(idata, idata + nSize, [&](T const d) { s += (d - m) * (d - m); });
+        return std::sqrt(s / (nSize - 1));
+    }
+};
+
+#endif
