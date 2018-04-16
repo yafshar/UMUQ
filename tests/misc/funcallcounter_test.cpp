2ff43fa29a9103e07ff27e2aaf9a7171c259446c
ya.afshar <ya.afshar@gmail.com>
2018-04-16 12:35:31 -0400

    makeing the function counter test file to work with MPI

--- tests/misc/funcallcounter_test.cpp
+++ tests/misc/funcallcounter_test.cpp
@@ -3,26 +3,78 @@
 #include "misc/funcallcounter.hpp"
 #include "gtest/gtest.h"
 
+class MPIEnvironment : public ::testing::Environment
+{
+  public:
+    virtual void SetUp()
+    {
+        char **argv;
+        int argc = 0;
+
+        torc_init(argc, argv, 0);
+
+        // ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+        // if (torc_node_id() != 0)
+        // {
+        //     delete listeners.Release(listeners.default_result_printer());
+        // }
+    }
+
+    virtual void TearDown()
+    {
+        torc_finalize();
+    }
+
+    virtual ~MPIEnvironment() {}
+};
+
 funcallcounter f;
+void taskf()
+{
+    f.increment();
+};
 
 /*! 
  * Test to check random functionality
  */
 TEST(funcallcounter_test, HandlesFunctioncounter)
 {
+    EXPECT_TRUE(f.init());
+    torc_register_task((void *)taskf);
+
+    //Number of nodes
+    int nnodes = torc_num_nodes();
+    int ntasks = 10;
+
+    for (int i = 0; i < ntasks; i++)
+    {
+        torc_create(-1, (void (*)())taskf, 0);
+    }
+    torc_waitall();
+
+    f.count();
+    f.reset();
+
+    EXPECT_EQ(f.get_nglobalfc(), nnodes * ntasks);
+    EXPECT_EQ(f.get_ntotalfc(), nnodes * ntasks);
+
+    for (int i = 0; i < ntasks * 2; i++)
+    {
+        torc_create(-1, (void (*)())taskf, 0);
+    }
+    torc_waitall();
+
+    f.count();
+    f.reset();
+
+    EXPECT_EQ(f.get_nglobalfc(), nnodes * ntasks * 2);
+    EXPECT_EQ(f.get_ntotalfc(), nnodes * ntasks * 3);
 }
 
 int main(int argc, char **argv)
 {
-	torc_register_task((void *)f.reset_Task);
-	torc_register_task((void *)f.get_Task);
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new MPIEnvironment);
 
-	torc_init(argc, argv, 0);
-
-	::testing::InitGoogleTest(&argc, argv);
-	int res = RUN_ALL_TESTS();
-
-	torc_finalize();
-
-	return res;
+    return RUN_ALL_TESTS();
 }
