15acbe29cefb80619a1523560ed3baaa4ab7a30b
ya.afshar <ya.afshar@gmail.com>
2018-04-23 18:07:47 -0400

    minor

--- tests/numerics/psrandom_test.cpp
+++ tests/numerics/psrandom_test.cpp
@@ -7,26 +7,26 @@
 class TORCEnvironment : public ::testing::Environment
 {
   public:
-	virtual void SetUp()
-	{
-		char **argv;
-		int argc = 0;
+    virtual void SetUp()
+    {
+        char **argv;
+        int argc = 0;
 
-		torc_init(argc, argv, 0);
+        torc_init(argc, argv, 0);
 
-		// ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
-		// if (torc_node_id() != 0)
-		// {
-		//     delete listeners.Release(listeners.default_result_printer());
-		// }
-	}
+        // ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+        // if (torc_node_id() != 0)
+        // {
+        //     delete listeners.Release(listeners.default_result_printer());
+        // }
+    }
 
-	virtual void TearDown()
-	{
-		torc_finalize();
-	}
+    virtual void TearDown()
+    {
+        torc_finalize();
+    }
 
-	virtual ~TORCEnvironment() {}
+    virtual ~TORCEnvironment() {}
 };
 
 /*! 
@@ -34,25 +34,32 @@ class TORCEnvironment : public ::testing::Environment
  */
 TEST(random_test, HandlesRandoms)
 {
-	psrandom r;
-	EXPECT_TRUE(r.init());
+    //get an instance of a random object and seed it
+    psrandom r(123);
+    EXPECT_TRUE(r.init());
 
-	EMatrix2d e;
-	e << 1, 3. / 5., 3. / 5., 2.;
-	EVector2d d;
-	d.Zero();
-	std::cout << "e=" << e << std::endl;
-	std::cout << "d=" << d << std::endl;
+    //create a matrix
+    EMatrix2d EM2d;
+    EM2d << 1, 3. / 5., 3. / 5., 2.;
 
-	mvnormdist<EMatrix2d, EVector2d> m(e);
+    //create a zero vector
+    EVector2d EV2d = EVector2d::Zero();
 
-	std::cout << "m=" << m() << std::endl;
+    //create an object of type Multivariate normal distribution
+    mvnormdist<double> MVNOBJ1(EM2d);
+
+    //create an object of type Multivariate normal distribution
+    Mvnormdist<double> MVNOBJ2(EV2d, EM2d);
+
+    //TODO Add the test for mvnormdist
+
+    std::cout << MVNOBJ2() << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-	::testing::InitGoogleTest(&argc, argv);
-	::testing::AddGlobalTestEnvironment(new TORCEnvironment);
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new TORCEnvironment);
 
-	return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();
 }
