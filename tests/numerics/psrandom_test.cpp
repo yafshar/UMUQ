a4e3dce86b5fba0c7a753326a515af1d2faab4f5
Yaser Afshar <ya.afshar@gmail.com>
2018-04-23 07:36:17 -0400

    minor

--- tests/numerics/psrandom_test.cpp
+++ tests/numerics/psrandom_test.cpp
@@ -7,26 +7,26 @@
 class TORCEnvironment : public ::testing::Environment
 {
   public:
-    virtual void SetUp()
-    {
-        char **argv;
-        int argc = 0;
+	virtual void SetUp()
+	{
+		char **argv;
+		int argc = 0;
 
-        torc_init(argc, argv, 0);
+		torc_init(argc, argv, 0);
 
-        // ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
-        // if (torc_node_id() != 0)
-        // {
-        //     delete listeners.Release(listeners.default_result_printer());
-        // }
-    }
+		// ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+		// if (torc_node_id() != 0)
+		// {
+		//     delete listeners.Release(listeners.default_result_printer());
+		// }
+	}
 
-    virtual void TearDown()
-    {
-        torc_finalize();
-    }
+	virtual void TearDown()
+	{
+		torc_finalize();
+	}
 
-    virtual ~TORCEnvironment() {}
+	virtual ~TORCEnvironment() {}
 };
 
 /*! 
@@ -34,14 +34,25 @@ class TORCEnvironment : public ::testing::Environment
  */
 TEST(random_test, HandlesRandoms)
 {
-    psrandom r;
-    EXPECT_TRUE(r.init());
+	psrandom r;
+	EXPECT_TRUE(r.init());
+
+	EMatrix2d e;
+	e << 1, 3. / 5., 3. / 5., 2.;
+	EVector2d d;
+	d.Zero();
+	std::cout << "e=" << e << std::endl;
+	std::cout << "d=" << d << std::endl;
+
+	mvnormdist<EMatrix2d, EVector2d> m(e);
+
+	std::cout << "m=" << m() << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::AddGlobalTestEnvironment(new TORCEnvironment);
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::AddGlobalTestEnvironment(new TORCEnvironment);
 
-    return RUN_ALL_TESTS();
+	return RUN_ALL_TESTS();
 }
