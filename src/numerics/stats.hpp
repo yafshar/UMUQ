7514da47b6a47ff53fb6bf7952073d4283abedec
ya.afshar <ya.afshar@gmail.com>
2018-04-25 17:48:52 -0400

    adding the coefvar function

--- src/numerics/stats.hpp
+++ src/numerics/stats.hpp
@@ -111,45 +111,58 @@ struct stats
      * \returns The standard deviation of the elements in the array of data
      */
     template <typename T, typename TOut = double>
-    inline TOut stddev(T const *idata, const int nSize) const
+    inline TOut stddev(T const *idata, int const nSize, TOut const idatamean = std::numeric_limits<TOut>::max()) const
     {
-        TOut m = mean<T, TOut>(idata, nSize);
+        TOut m = (idatamean < std::numeric_limits<TOut>::max() ? idatamean : mean<T, TOut>(idata, nSize));
         TOut s(0);
         std::for_each(idata, idata + nSize, [&](T const d) { s += (d - m) * (d - m); });
         return std::sqrt(s / (nSize - 1));
     }
+
+    /*!
+     * \brief Computes the square of the coefficient of variation (COV) of the plausibility weights to a prescribed threshold
+     * 
+     * \tparam T       data type
+     * \tparam TOut    data type of return output result (default is double)
+     * 
+     * \param  fValue  An array of log value
+     * \param  fSize   size of the fValue array 
+     * \param  x       
+     * \param  p 
+     * \param  tol     a prescribed tolerance
+     * 
+     * \returns the square of the coefficient of variation (COV)
+     */
+    template <typename T, typename TOut = double>
+    TOut CoefVar(T const *fValue, int const fSize, T const x, T const p, T const tol)
+    {
+        //Find the maximum value in the array of fValue of size fSize
+        T fMaxValue = maxelement<T>(fValue, fSize);
+
+        TOut weight[fSize];
+        TOut diff = static_cast<TOut>(x - p);
+
+        //Compute the weight
+        for (int i = 0; i < fSize; i++)
+        {
+            weight[i] = std::exp((fValue[i] - fMaxValue) * diff);
+        }
+
+        //Compute the summation of weight
+        TOut weightsum = sum<TOut, TOut>(weight, fSize);
+
+        //Normalize the weight
+        std::for_each(weight, weight + fSize, [&](T &w) { w /= weightsum; });
+
+        //Compute the mean
+        TOut weightmean = mean<TOut, TOut>(weight, fSize);
+        
+        //Compute the standard deviation
+        TOut weightstddev = stddev<TOut, TOut>(weight, fSize, weightmean);
+
+        //return the coefficient of variation (COV)
+        return std::pow(weightstddev / weightmean - tol, 2);
+    }
 };
 
 #endif
-
-
-
-// double Objlogp(double x, double *fj, int fn, double pj, double tol)
-// {
-//     double fjmax = compute_max(fj, fn);
-
-//     double weight[fn];
-
-//     for (i = 0; i < fn; i++)
-//         weight[i] = exp((fj[i]-fjmax)*(x-pj));
-
-//     double sum_weight = compute_sum(weight, fn);
-
-//     double q[fn];
-
-//     for (i = 0; i < fn; i++)
-//         q[i] = weight[i]/sum_weight;
-
-//     double mean_q = compute_mean(q, fn);
-//     double std_q = compute_std(q, fn, mean_q);
-//     double CoefVar = pow(std_q/mean_q-tol, 2);    /* result */
-//     return CoefVar;
-// }
-
-
-
-
-
-
-
-
