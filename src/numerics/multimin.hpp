63a70328cda887519fc92632d6ed514186cf2e2c
ya.afshar <ya.afshar@gmail.com>
2018-04-27 17:30:31 -0400

    multimin

--- src/numerics/multimin.hpp
+++ src/numerics/multimin.hpp
@@ -1,221 +1,283 @@
 #ifndef UMHBM_MULTIMIN_H
 #define UMHBM_MULTIMIN_H
 
-/*! \class multimin_function
-  * \brief Definition of an arbitrary real-valued function with vector input and parameters
-  */
-template <typename T, class Tparams>
-struct multimin_function
-{
-    Tparams params;
-    size_t n;
-
-    T f(T const *x, Tparams params_);
-};
-
-/*! \class multimin_function_fdf
-  * \brief Definition of an arbitrary differentiable real-valued function with vector input and parameters
-  */
-template <typename T, class Tparams>
-struct multimin_function_fdf
-{
-    size_t n;
-    Tparams params;
-
-    T f(T const *x, Tparams params_);
-    T df(T const *x, Tparams params_, T *df_);
-    T fdf(T const *x, Tparams params_, T *f_, T *df_);
-};
-
 #define MULTIMIN_FN_EVAL(F, x) (*((F)->f))(x, (F)->params)
 #define MULTIMIN_FN_EVAL_F(F, x) (*((F)->f))(x, (F)->params)
 #define MULTIMIN_FN_EVAL_DF(F, x, g) (*((F)->df))(x, (F)->params, (g))
 #define MULTIMIN_FN_EVAL_F_DF(F, x, y, g) (*((F)->fdf))(x, (F)->params, (y), (g))
 
-template <typename T>
-int multimin_diff(multimin_function<T> const *f, T const *x, T *g);
+// The goal is finding minima of arbitrary multidimensional functions.
+
+
+
+/*! \class multimin_function
+  * \brief Definition of an arbitrary function with vector input and parameters
+  * 
+  * \tparam T     data type
+  * \tparan TMF   multimin function type
+  */
+template <typename T, class TMF>
+class multimin_function
+{
+  public:
+    T f(T const *x)
+    {
+        return static_cast<TMF *>(this)->f(x);
+    }
+
+    multimin_function(size_t n_) : n(n_) {}
+
+    size_t n;
+
+  private:
+    friend TMF;
+};
 
 /*! \class multimin_fminimizer_type
   * \brief minimization of non-differentiable functions
+  * 
+  * \tparam T     data type
+  * \tparam TMFMT multimin function minimizer type
+  * \tparan TMF   multimin function type
   */
-template <class T>
-struct multimin_fminimizer_type
+template <typename T, class TMFMT, class TMF>
+class multimin_fminimizer_type
 {
-    T fminimizer;
+  public:
     const char *name;
 
-    multimin_fminimizer_type(const char *name_, T &&multimin_fminimizer_) : name(name_)
+    bool alloc(size_t n)
     {
-        fminimizer = std::move(multimin_fminimizer_);
+        return static_cast<TMFMT *>(this)->alloc(n);
     }
 
-    ~multimin_fminimizer_type()
+    bool set(TMF *tmf, T const *x, T *size, T const *step_size)
     {
-        fminimizer.free();
+        return static_cast<TMFMT *>(this)->set(tmf, x, size, step_size);
     }
+
+    bool iterate(TMF *tmf, T const *x, T *size, T *fval)
+    {
+        return static_cast<TMFMT *>(this)->iterate(tmf, x, size, fval);
+    }
+
+    void free()
+    {
+        static_cast<TMFMT *>(this)->free();
+    }
+
+    multimin_fminimizer_type(const char *name_) : name(name_) {}
+    multimin_fminimizer_type(multimin_fminimizer_type const &m) : name(m.name) {}
+  
+  private:
+    friend TMFMT;
 };
 
-template <typename T>
-struct multimin_fminimizer
+/*! \class multimin_fminimizer
+  * \brief This class is for minimizing functions without derivatives.
+  * 
+  * \tparam T      data type
+  * \tparam TMFDMT multimin differentiable function minimizer type
+  * \tparan TMFD   multimin differentiable function type
+  */
+template <typename T, class TMFM, class TMFMT, class TMF>
+class multimin_fminimizer
 {
-    /* multi dimensional part */
-    multimin_fminimizer_type<T> typeT;
-    multimin_function<T> *f;
+  public:
+    //Multi dimensional part
+    TMFMT type;
+    TMF f;
     T fval;
     T *x;
     T size;
-    void *state;
 };
 
-template <typename T>
-multimin_fminimizer<T> *multimin_fminimizer_alloc(const multimin_fminimizer_type<T> *type, size_t n);
+/*! \class multimin_function_fdf
+  * \brief Definition of an arbitrary differentiable function with vector input and parameters
+  *  
+  * \tparam T      data type
+  * \tparan TMFD   multimin differentiable function type
+  */
+template <typename T, class TMFD>
+struct multimin_function_fdf
+{
+  public:
+    T f(T const *x)
+    {
+        return static_cast<TMFD *>(this)->f(x);
+    }
 
-template <typename T>
-int multimin_fminimizer_set(multimin_fminimizer<T> *s, multimin_function<T> *f, T const *x, T const *step_size);
+    T df(T const *x, T *df_)
+    {
+        return static_cast<TMFD *>(this)->df(x, df_);
+    }
 
-template <typename T>
-void multimin_fminimizer_free(multimin_fminimizer<T> *s);
+    T fdf(T const *x, T *f_, T *df_)
+    {
+        return static_cast<TMFD *>(this)->fdf(x, f_, df_);
+    }
 
-template <typename T>
-const char *multimin_fminimizer_name(multimin_fminimizer<T> const *s);
+    multimin_function_fdf(size_t n_) : n(n_) {}
 
-template <typename T>
-int multimin_fminimizer_iterate(multimin_fminimizer<T> *s);
+    size_t n;
 
-template <typename T>
-T *multimin_fminimizer_x(multimin_fminimizer<T> const *s);
-
-template <typename T>
-T multimin_fminimizer_minimum(multimin_fminimizer<T> *s);
-
-template <typename T>
-T multimin_fminimizer_size(multimin_fminimizer<T> const *s);
-
-/* Convergence test functions */
-template <typename T>
-int multimin_test_gradient(T const *g, T epsabs);
-
-template <typename T>
-int multimin_test_size(T const size, T epsabs);
+  private:
+    friend TMFD;
+};
 
 /*! \class multimin_fdfminimizer_type
-  * \brief minimisation of differentiable functions
+  * \brief differentiable function minimizer type
+  * 
+  * \tparam T      data type
+  * \tparam TMFDMT multimin differentiable function minimizer type
+  * \tparan TMFD   multimin differentiable function type
   */
-template <class T>
-struct multimin_fdfminimizer_type
+template <typename T, class TMFDMT, class TMFD>
+class multimin_fdfminimizer_type
 {
-    T fdfminimizer;
+  public:
     const char *name;
 
-    multimin_fdfminimizer_type(const char *name_, T &&multimin_fdfminimizer_) : name(name_)
+    bool alloc(size_t n)
     {
-        fdfminimizer = std::move(multimin_fdfminimizer_);
+        return static_cast<TMFDMT *>(this)->alloc(n);
     }
 
-    ~multimin_fdfminimizer_type()
+    bool set(TMFD *tmfd, T const *x, T *f, T *gradient, T step_size, T tol)
     {
-        fdfminimizer.free();
+        return static_cast<TMFDMT *>(this)->set(tmfd, x, f, gradient, step_size, tol);
     }
+
+    bool iterate(TMFD *tmfd, T *x, T *f, T *gradient, T *dx);
+    {
+        return static_cast<TMFDMT *>(this)->iterate(tmfd, x, f, gradient, dx);
+    }
+
+    bool restart()
+    {
+        return static_cast<TMFDMT *>(this)->restart();
+    }
+
+    void free()
+    {
+        static_cast<TMFDMT *>(this)->free();
+    }
+
+    multimin_fdfminimizer_type(const char *name_) : name(name_) {}
+    multimin_fdfminimizer_type(multimin_fdfminimizer_type const &m) : name(m.name) {}
+
+  private:
+    friend TMFDMT;
 };
 
 /*! \class multimin_fdfminimizer
-  *
+  * \brief This class is for minimizing functions using derivatives. 
+  * 
+  * \tparam T      data type
+  * \tparam TMFDMT multimin differentiable function minimizer type
+  * \tparan TMFD   multimin differentiable function type
   */
-template <typename T>
-struct multimin_fdfminimizer
+template <typename T, class TMFDMT, class TMFD>
+class multimin_fdfminimizer
 {
-    // multi dimensional part
-    multimin_fdfminimizer_type<T> typeT;
-    multimin_function_fdf<T> *fdf;
-
-    T f;
-    T *x;
-    T *gradient;
-    T *dx;
-
-    //size of array
-    size_t n;
-
+  public:
     /*!
-     * \brief Default constructor
+     * \brief alloc
      * 
      * \param Ttype pointer to \a multimin_fdfminimizer_type object 
      * \param n_ size of array
+     * 
+     * \returns true if everything goes OK
      */
-    multimin_fdfminimizer(multimin_fdfminimizer_type<T> &&Ttype, size_t n_) : typeT(Ttype.name, Ttype), n(n_)
+    bool alloc(multimin_fdfminimizer_type<T, TMFDMT, TMFD> *Ttype, size_t n_)
     {
+        n = n_;
+        type = Ttype;
+
         try
         {
-            x = new T[n]();
+            x = new T[n];
+            //set to zero
             gradient = new T[n]();
+            //set to zero
             dx = new T[n]();
         }
         catch (std::bad_alloc &e)
         {
             std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
             std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+            return false;
         }
+
+        if (!type->alloc(n))
+        {
+            free();
+            return false;
+        }
+
+        return true;
     }
 
     /*!
-     * \brief alloc
+     * \brief set
      * 
-     * \param fdf_ Function
-     * \param x
-     * \param xsize 
-     * \param step_size
-     * \param tol
+     * \param mfdf      pointer to an arbitrary differentiable real-valued function
+     * \param x         input array
+     * \param n_        size of array n_  
+     * \param step_size step size
+     * \param tol       tol
      *  
-     * returns 
+     * returns true if everything goes OK
      */
-    int alloc(multimin_function_fdf<T> *fdf_, T const *x_, size_t const xsize, T step_size, T tol)
+    bool set(multimin_function_fdf<T, TMFD> *mfdf, T const *x_, size_t n_, T step_size, T tol)
     {
-        if (n != fdf_->n)
+        if (n != mfdf->n)
         {
             std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
             std::cerr << " Function incompatible with solver size! " << std::endl;
-            throw(std::runtime_error("Function incompatible with solver size!"));
+            return false;
         }
 
-        if (xsize != fdf_->n)
+        if (n_ != mfdf->n)
         {
             std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
             std::cerr << "Vector length not compatible with function" << std::endl;
-            throw(std::runtime_error("Vector length not compatible with function!"));
+            return false;
         }
 
-        fdf = fdf_;
+        //set the pointer
+        fdf = mfdf;
 
         //copy array x_ to array x
-        std::copy(x_, x_ + xsize, x);
+        std::copy(x_, x_ + n_, x);
 
         //set dx to zero
         std::fill(dx, dx + n, (T)0);
 
-        return (type->set)(state, fdf, x, &f, gradient, step_size, tol);
+        return type->set(fdf, x, &f, gradient, step_size, tol);
     }
 
     /*!
      * \brief iterate
      * 
      */
-    int iterate()
+    bool iterate()
     {
-        return (type->iterate)(state, fdf, x, &f, gradient, dx);
+        return type->iterate(fdf, x, &f, gradient, dx);
     }
 
     /*!
      * \brief restart
      * 
      */
-    int restart()
+    bool restart()
     {
-        return (type->restart)(state);
+        return type->restart();
     }
 
     /*!
      * \brief name
-     * \returns the name 
+     * \returns the name of the minimization type
      */
     const char *name()
     {
@@ -232,6 +294,33 @@ struct multimin_fdfminimizer
     }
 
     /*!
+     * \brief get function x
+     * \returns x
+     */
+    T *get_x()
+    {
+        return x;
+    }
+
+    /*!
+     * \brief get function dx
+     * \returns dx
+     */
+    T *get_dx()
+    {
+        return dx;
+    }
+
+    /*!
+     * \brief get function x
+     * \returns x
+     */
+    T *get_gradient()
+    {
+        return gradient;
+    }
+
+    /*!
      * \brief destructor
      * 
      */
@@ -239,47 +328,36 @@ struct multimin_fdfminimizer
 
     void free()
     {
-        (type->free)(state);
-        destroy();
-    }
+        type->free();
+        type = nullptr;
 
-    void destroy()
-    {
-        delete state;
+        fdf = nullptr;
+
         delete[] x;
         x = nullptr;
+
         delete[] gradient;
         gradient = nullptr;
+
         delete[] dx;
         dx = nullptr;
+
         n = 0;
         f = 0;
     }
+
+  private:
+    // multi dimensional part
+    multimin_fdfminimizer_type<T, TMFDMT, TMFD> *type;
+    multimin_function_fdf<T, TMFD> *fdf;
+
+    T f;
+    T *x;
+    T *gradient;
+    T *dx;
+
+    size_t n;
 };
-
-template <typename T>
-multimin_fdfminimizer_type<T> const *multimin_fdfminimizer_steepest_descent;
-
-template <typename T>
-multimin_fdfminimizer_type<T> const *multimin_fdfminimizer_conjugate_pr;
-
-template <typename T>
-multimin_fdfminimizer_type<T> const *multimin_fdfminimizer_conjugate_fr;
-
-template <typename T>
-multimin_fdfminimizer_type<T> const *multimin_fdfminimizer_vector_bfgs;
-
-template <typename T>
-multimin_fdfminimizer_type<T> const *multimin_fdfminimizer_vector_bfgs2;
-
-template <typename T>
-multimin_fminimizer_type<T> const *multimin_fminimizer_nmsimplex;
-
-template <typename T>
-multimin_fminimizer_type<T> const *multimin_fminimizer_nmsimplex2;
-
-template <typename T>
-multimin_fminimizer_type<T> const *multimin_fminimizer_nmsimplex2rand;
 
 /*! \class multimin
 * \brief 
@@ -290,131 +368,3 @@ struct multimin
 };
 
 #endif
-
-template <typename T>
-struct steepest_descent_state
-{
-    T step;
-    T max_step;
-    T tol;
-    T *x1;
-    T *g1;
-    size_t n;
-
-    steepest_descent_state() x1(nullptr), g1(nullptr), step(0), max_step(0), tol(0), n(0) {}
-
-    bool alloc(size_t n_)
-    {
-        n = n_;
-        try
-        {
-            x1 = new T[n_];
-            g1 = new T[n_];
-        }
-        catch (std::bad_alloc &e)
-        {
-            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
-            return false;
-        }
-        return true;
-    }
-
-    static bool set(multimin_function_fdf<T> *fdf, T const *x, T *f, T *gradient, T step_size, T tol)
-    {
-        (*(fdf->fdf))(x, fdf->params, f, gradient);
-        step = step_size;
-        max_step = step_size;
-        tol = tol;
-        the minimizer is unable to improve on its current estimate, either due to numerical difficulty or because a genuine local minimum has been reached.return true;
-    }
-
-    static void free()
-    {
-        delete[] x1;
-        x1 = nullptr;
-        delete[] g1;
-        g1 = nullptr;
-    }
-
-    static bool restart()
-    {
-        step = max_step;
-        return true;
-    }
-
-    static bool iterate(multimin_function_fdf<T> *fdf, T *x, T *f, T *gradient, T *dx)
-    {
-        T f0 = *f;
-        T f1;
-
-        int failed = 0;
-
-        //Compute new trial point at x1= x - step * dir, where dir is the normalized gradient
-
-        //First compute the Euclidean norm \f$ ||x||_2 = \sqrt {\sum x_i^2} of the vector x = gradient. \f$
-        T gnorm(0);
-        std::for_each(gradient, gradient + n, [](T const g) { gnorm += g * g; });
-        if (gnorm <= 0.0)
-        {
-            //set dx to zero
-            std::fill(dx, dx + n, (T)0);
-
-            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-            std::cerr << " The minimizer is unable to improve on its current estimate, either due" << std::endl;
-            std::cerr << " to numerical difficulty or because a genuine local minimum has been reached." << std::endl;
-            return false;
-        }
-
-        gnorm = std::sqrt(gnorm);
-
-    trial:
-
-        //Compute the sum \f$y = \alpha x + y\f$ for the vectors x and y.
-        //(set dx to zero)
-        T alpha = -step / gnorm;
-
-        for (size_t i = 0; i < n; i++)
-        {
-            dx[i] = alpha * gradient[i];
-        }
-
-        std::copy(x, x + n, x1);
-
-        for (size_t i = 0; i < n; i++)
-        {
-            x1[i] += dx[i];
-        }
-
-        //Evaluate function and gradient at new point x1
-        (*(fdf->fdf))(x1, fdf->params, &f1, g1);
-
-        if (f1 > f0)
-        {
-            // Downhill step failed, reduce step-size and try again
-            failed = 1;
-            step *= tol;
-            goto trial;
-        }
-
-        if (failed)
-        {
-            step *= tol;
-        }
-        else
-        {
-            step *= 2.0;
-        }
-
-        std::copy(x1, x1 + n, x);
-        std::copy(g1, g1 + n, gradient);
-
-        *f = f1;
-
-        return true;
-    }
-};
-
-template <typename T>
-static multimin_fdfminimizer_type<T> const steepest_descent_type = {"steepest_descent", /* name */ sizeof(steepest_descent_state_t), &steepest_descent_alloc, &steepest_descent_set, &steepest_descent_iterate, &steepest_descent_restart, &steepest_descent_free};
-const multimin_fdfminimizer_type *multimin_fdfminimizer_steepest_descent = &steepest_descent_type;
