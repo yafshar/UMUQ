10a00b12232a97c0532a3137d916d12adcc82493
ya.afshar <ya.afshar@gmail.com>
2018-04-30 18:04:36 -0400

    start to add the multimin test

---
+++ tests/numerics/multimin_test.cpp
@@ -0,0 +1,16 @@
+#include "core/core.hpp"
+#include "numerics/multimin.hpp"
+#include "gtest/gtest.h"
+
+/*! 
+ * Test to check multimin functionality
+ */
+TEST(multimin_test, HandlesANewFunction)
+{
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
