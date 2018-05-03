d68f825a94619279dc01460daf3acde10594da95
ya.afshar <ya.afshar@gmail.com>
2018-05-03 16:52:09 -0400

    clean up and correction for polynomial class

--- src/numerics/polynomial.hpp
+++ src/numerics/polynomial.hpp
@@ -1,101 +1,73 @@
 #ifndef UMHBM_POLYNOMIAL_H
 #define UMHBM_POLYNOMIAL_H
 
-/*! \brief Multivariate monomials with the degree of r in a space of d dimensions.
+/*! \class polynomial
+ *
+ * \brief Multivariate monomials with the degree of r in a space of d dimensions.
  *
  *  A (univariate) monomial in 1 variable x is simply any (non-negative integer) power of x:
- *      1, x, x^2, x^3, ..., x^r
+ *  \f$  1, x, x^2, x^3, \cdots, x^r \f$
  *  The highest exponent of x is termed the degree of the monomial.
  */
+template <typename T>
 class polynomial
 {
   public:
-    polynomial() : dim(2), degree(2){};
-    inline polynomial(unsigned int dm);
-    inline polynomial(unsigned int dm, unsigned int dg);
+    /*! 
+     * \brief Default constructor
+     */
+    polynomial() : nDim(0), Degree(0) {}
 
-    template <class T>
-    inline T const min_value(T const a, T const b);
-    template <class T>
-    inline T const max_value(T const a, T const b);
-
-    template <typename T>
-    inline T arraysum(int const arraysize, T const *array);
+    /*! 
+     * \brief constructor
+     * 
+     * \param dm Dimension (default dimension is 2)
+     * \param dg Degree (default degree of accuracy is 2)
+     */
+    polynomial(unsigned int dm, unsigned int dg) : nDim(dm), Degree(dg) {}
 
     int binomial_coefficient(int const n, int const k);
 
-    void monomial_basis(int const d, int const r, int *&alpha);
+    bool monomial_basis(int const d, int const r, int *&alpha);
 
-    template <typename T>
-    void monomial_value(int const d, int const r, int *alpha, T *x, T *&value);
+    int monomial_value(int const d, int const r, int *alpha, T *x, T *&value);
+    int monomial_value(int const d, int const r, T *x, T *&value);
+    int monomial_value(T *x, T *&value)
+    {
+        if (nDim == 0 && Degree == 0)
+        {
+            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+            std::cerr << "There is no Dimesnion and Degree in the constructed object!" << std::endl;
+            return 0;
+        }
+        return monomial_value(nDim, Degree, x, value);
+    }
 
   private:
-    unsigned int dim;
-    unsigned int degree;
+    int nDim;
+    int Degree;
 
-    void graded_reverse_lexicographic_order(int const d, int const r, int *x);
-};
-
-inline polynomial::polynomial(unsigned int dm)
-{
-    polynomial::dim = dm;
-    polynomial::degree = 2;
-};
-
-inline polynomial::polynomial(unsigned int dm, unsigned int dg)
-{
-    polynomial::dim = dm;
-    polynomial::degree = dg;
-};
-
-/*! \fn min_value(a, b)
-*   \brief A function that returns the minimum of \a a and \a b.
-*/
-template <typename T>
-inline T const polynomial::min_value(T const a, T const b)
-{
-    if (a > b)
-        return b;
-    return a;
-};
-
-/*! \fn max_value(a, b)
-*   \brief A function that returns the maximum of \a a and \a b.
-*/
-template <typename T>
-inline T const polynomial::max_value(T const a, T const b)
-{
-    if (b > a)
-        return b;
-    return a;
-};
-
-/*! \fn arraysum(arraysize, array)
-*   \brief A function that returns the sum of an array of arraysize.
-*/
-template <typename T>
-inline T polynomial::arraysum(int const arraysize, T const *array)
-{
-    return std::accumulate(array, array + arraysize, 0);
+    bool graded_reverse_lexicographic_order(int const d, int const r, int *x);
 };
 
 /*! 
  * \brief Computes the binomial coefficient C(n, k).
  *
- * 1) A binomial coefficient C(n, k) can be defined as the coefficient of X ^ k in the expansion of(1 + X) ^ n
+ * 1) A binomial coefficient C(n, k) can be defined as the coefficient of \f$ X ^ k \f$ in the expansion of \f$ (1 + X) ^ n \f$
  * 2) A binomial coefficient C(n, k) also gives the number of ways, disregarding order, that k objects can be 
  * chosen from among n objects; 
  * more formally, the number of k-element subsets (or k-combinations) of an n-element set.
  * 
  * The formula used is:
- *   c(n,k) = n! / ( n! * (n-k)! )  
- *  $c(n,k) = \frac{n\!}{( n\! * (n-k)! )}$ 
+ * \f$ c(n,k) = \frac{n!}{ n! * (n-k)! } \f$ 
  * 
- * @param[in] n
- * @param[in] k
- * @param[out] binomial The binomial coefficient
+ * \param n Input parameter
+ * \param k Input parameter
+ * 
+ * \returns The binomial coefficient \f$ C(n, k) \f$
  */
-int polynomial::binomial_coefficient(int const n, int const k)
+template <typename T>
+int polynomial<T>::binomial_coefficient(int const n, int const k)
 {
     if ((k < 0) || (n < 0))
     {
@@ -106,15 +78,17 @@ int polynomial::binomial_coefficient(int const n, int const k)
     if (k < n)
     {
         if (k == 0)
+        {
             return 1;
+        }
         if ((k == 1) || (k == n - 1))
+        {
             return n;
-        int mn;
-        int mx;
-        int value;
-        mn = polynomial::min_value(k, n - k);
-        mx = polynomial::max_value(k, n - k);
-        value = mx + 1;
+        }
+
+        int mn = std::min(k, n - k);
+        int mx = std::max(k, n - k);
+        int value = mx + 1;
         for (int i = 2; i <= mn; i++)
         {
             value = (value * (mx + i)) / i;
@@ -133,41 +107,41 @@ int polynomial::binomial_coefficient(int const n, int const k)
 };
 
 /*! 
- *   \brief Use a reverse lexicographic order for next monomial, degree between 0 and r
- *   all monomials in a d dimensional space, with degree r.
+ * \brief Use a reverse lexicographic order for next monomial, degree between 0 and r
+ *  all monomials in a d dimensional space, with degree r.
  *
- *   Parameters:
- *       @param[in]  d   The spatial dimension
- *       @param[in]  r   Maximum degree
- *       @param[in]  x   Current monomial
- *       @param[out] x   Next monomial, last value in the sequence is r.
+ * \param  d   The spatial dimension
+ * \param[in]  r   Maximum degree
+ * \param[in]  x   Current monomial
+ * \param[out] x   Next monomial, last value in the sequence is r.
  */
-void polynomial::graded_reverse_lexicographic_order(int const d, int const r, int *x)
+template <typename T>
+bool polynomial<T>::graded_reverse_lexicographic_order(int const d, int const r, int *x)
 {
     if (r < 0)
     {
         std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
         std::cerr << " Fatal error! maximum degree r < 0" << std::endl;
-        throw(std::runtime_error("Wrong Input!"));
+        return false;
     }
     if (r == 0)
-        return;
+    {
+        return true;
+    }
 
-    int asum;
-    asum = polynomial::arraysum(d, x);
-
+    int asum = std::accumulate(x, x + d, 0);
     if (asum < 0)
     {
         std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
         std::cerr << " Fatal error! input sums < 0" << std::endl;
-        throw(std::runtime_error("Wrong Input!"));
+        return false;
     }
 
     if (r < asum)
     {
         std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
         std::cerr << " Fatal error! input sums > maximum degree r" << std::endl;
-        throw(std::runtime_error("Wrong Input!"));
+        return false;
     }
 
     if (x[0] == r)
@@ -178,11 +152,10 @@ void polynomial::graded_reverse_lexicographic_order(int const d, int const r, in
     else
     {
         int i;
-        int j;
         int tmp;
 
-        // Seeking the first index in which x > 0.
-        j = 0;
+        //Seeking the first index in which x > 0.
+        int j = 0;
         for (i = 1; i < d; i++)
         {
             if (x[i] > 0)
@@ -213,7 +186,7 @@ void polynomial::graded_reverse_lexicographic_order(int const d, int const r, in
             x[j] = x[j] - 1;
         }
     }
-    return;
+    return true;
 };
 
 /*! 
@@ -244,76 +217,141 @@ void polynomial::graded_reverse_lexicographic_order(int const d, int const r, in
  *       @param[in]  alpha   Undefined pointer
  *       @param[out] alpha   Pointer to monomial sequence
  */
-void polynomial::monomial_basis(int const d, int const r, int *&alpha)
+template <typename T>
+bool polynomial<T>::monomial_basis(int const d, int const r, int *&alpha)
 {
-    int j;
-    int n;
-
-    if (alpha != NULL)
+    if (alpha != nullptr)
     {
         delete[] alpha;
     }
 
-    n = d * polynomial::binomial_coefficient(d + r, r);
     try
     {
-        alpha = new int[n];
+        int const N = d * polynomial<T>::binomial_coefficient(d + r, r);
+        alpha = new int[N];
     }
     catch (std::bad_alloc &e)
     {
         std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
         std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+        return false;
     }
 
-    int x[d];
-    for (int i = 0; i < d; i++)
-    {
-        x[i] = 0;
-    };
+    int x[d] = {};
+    int n = 0;
 
-    n = 0;
     for (;;)
     {
-        for (j = d - 1; j >= 0; j--, n++)
+        for (int j = d - 1; j >= 0; j--, n++)
         {
             alpha[n] = x[j];
         }
 
         if (x[0] == r)
-            break;
+        {
+            return true;
+        }
 
-        polynomial::graded_reverse_lexicographic_order(d, r, x);
+        if (!polynomial<T>::graded_reverse_lexicographic_order(d, r, x))
+        {
+            return false;
+        }
     }
-    return;
+    return true;
 };
 
 /*! \brief Evaluates a monomial at a point x.
  *
- *   Parameters:
- *       @param[in]  d       The spatial dimension
- *       @param[in]  r       Maximum degree
- *       @param[in]  alpha   The exponents of the monomial
- *       @param[in]  x       The coordinates of the evaluation points
- *       @param[out] value   Monomial_value, the array value of the monomial at point x
+ * @param[in]  d       The spatial dimension
+ * @param[in]  r       Maximum degree
+ * @param[in]  alpha   The exponents of the monomial
+ * @param[in]  x       The coordinates of the evaluation points
+ * @param[out] value   Monomial_value, the array value of the monomial at point x
+ * 
+ * \returns the size of the monomial array
  */
 template <typename T>
-void polynomial::monomial_value(int const d, int const r, int *alpha, T *x, T *&value)
+int polynomial<T>::monomial_value(int const d, int const r, int *alpha, T *x, T *&value)
 {
-    const int n = polynomial::binomial_coefficient(d + r, r);
+    int const n = polynomial<T>::binomial_coefficient(d + r, r);
 
-    T v;
-    int k;
-
-    k = 0;
-    for (int i = 0; i < n; i++)
+    if (value == nullptr)
     {
-        v = 1;
+        try
+        {
+            value = new T[n];
+        }
+        catch (std::bad_alloc &e)
+        {
+            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+            return 0;
+        }
+    }
+
+    for (int i = 0, k = 0; i < n; i++)
+    {
+        T v = static_cast<T>(1);
         for (int j = 0; j < d; j++, k++)
         {
             v *= std::pow(x[j], alpha[k]);
         }
         value[i] = v;
     }
+
+    return n;
+};
+
+/*! 
+ * \brief Evaluates a monomial at a point x.
+ *
+ * @param[in]  d       The dimension
+ * @param[in]  r       Maximum degree
+ * @param[in]  x       The coordinates of the evaluation point
+ * @param[out] value   Monomial_value, the array value of the monomial at point x
+ * 
+ * \returns the size of the monomial array
+ */
+template <typename T>
+int polynomial<T>::monomial_value(int const d, int const r, T *x, T *&value)
+{
+    int const n = polynomial<T>::binomial_coefficient(d + r, r);
+
+    if (value == nullptr)
+    {
+        try
+        {
+            value = new T[n];
+        }
+        catch (std::bad_alloc &e)
+        {
+            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+        }
+    }
+
+    int *alpha = nullptr;
+
+    if (polynomial<T>::monomial_basis(d, r, alpha))
+    {
+
+        for (int i = 0, k = 0; i < n; i++)
+        {
+            T v = static_cast<T>(1);
+            for (int j = 0; j < d; j++, k++)
+            {
+                v *= std::pow(x[j], alpha[k]);
+            }
+            value[i] = v;
+        }
+
+        delete[] alpha;
+        return n;
+    }
+
+    delete[] value;
+    value = nullptr;
+    return 0;
 };
 
 #endif
