cafda1c159b1477065f202d42ce057420155425b
ya.afshar <ya.afshar@gmail.com>
2018-05-03 13:14:19 -0400

    test case for flanlib class for fast approximate nearest neighbor searches

---
+++ tests/numerics/flannlib_test.cpp
@@ -0,0 +1,129 @@
+#include "core/core.hpp"
+#include "io/io.hpp"
+#include "numerics/flannlib.hpp"
+#include "gtest/gtest.h"
+
+/*! 
+ * Test to check flannlib functionality
+ */
+TEST(flannlib_test, HandlesKNN)
+{
+    io f;
+    EXPECT_TRUE(f.isFileExist("numerics/flannlib_test.txt"));
+    EXPECT_TRUE(f.openFile("numerics/flannlib_test.txt"));
+
+    int n = 0;
+    while (f.readLine())
+    {
+        //Count the number of non empty and not commented line with "#" as default comment
+        n++;
+    }
+
+    //This data type has two dimensions
+    int nDim = 2;
+    //Number of nearest neighbors to find
+    int nn = 3;
+
+    double *data = nullptr;
+    double *dtest = nullptr;
+    int *knntest = nullptr;
+
+    //Allocate memory for reading the data
+    try
+    {
+        data = new double[n * nDim];
+        dtest = new double[n + 1];
+        knntest = new int[n * nn];
+    }
+    catch (std::bad_alloc &e)
+    {
+        std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+        std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+    }
+
+    //!Rewind the file
+    f.rewindFile();
+
+    //!Read the array of data
+    EXPECT_TRUE(f.loadMatrix<double>(data, n, nDim));
+
+    //Close the file
+    f.closeFile();
+
+    // kNearestNeighbor<double, flann::L2<double>> KNN(n, nDim, nn);
+    L2NearestNeighbor<double> KNN(n, nDim, nn);
+
+    KNN.buildIndex(data);
+
+    //using brute force to find neighbors    
+    dtest[n] = std::numeric_limits<double>::max();
+
+    for (int i = 0; i < n; i++)
+    {
+        int const IdI = i * nDim;
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            int const IdJ = j * nDim;
+            double const dd[2] = {data[IdJ] - data[IdI], data[IdJ + 1] - data[IdI + 1]};
+            dtest[j] = dd[0] * dd[0] + dd[1] * dd[1];
+        }
+        int const Id = i * nn + nn;
+        std::fill(knntest + Id - nn, knntest + Id, n);
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            int const Id3 = knntest[Id - 1];
+            if (dtest[j] < dtest[Id3])
+            {
+                int const Id2 = knntest[Id - 2];
+                if (dtest[j] < dtest[Id2])
+                {
+                    int const Id1 = knntest[Id - 3];
+                    if (dtest[j] < dtest[Id1])
+                    {
+                        knntest[Id - 1] = knntest[Id - 2];
+                        knntest[Id - 2] = knntest[Id - 3];
+                        knntest[Id - 3] = j;
+                    }
+                    else
+                    {
+                        knntest[Id - 1] = knntest[Id - 2];
+                        knntest[Id - 2] = j;
+                    }
+                }
+                else
+                {
+                    knntest[Id - 1] = j;
+                }
+            }
+        }
+    }
+
+    delete[] data;
+    delete[] dtest;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int *p = KNN.NearestNeighbors(i);
+        int const Id = i * nn;
+        for (int j = 0; j < nn; j++)
+        {
+            EXPECT_EQ(p[j], knntest[Id + j]);
+        }
+    }
+
+    delete[] knntest;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
