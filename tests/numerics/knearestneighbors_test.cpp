08037f4ebad2e695b018b5cee8d9bbb2405d7a99
Yaser Afshar <ya.afshar@gmail.com>
2018-05-07 07:37:01 -0400

    minor

--- tests/numerics/knearestneighbors_test.cpp
+++ tests/numerics/knearestneighbors_test.cpp
@@ -12,152 +12,154 @@ TEST(knearestneighbors_test, HandlesKNN)
 
 #if HAVE_FLANN
 
-    io f;
-    EXPECT_TRUE(f.isFileExist("numerics/knearestneighbors_test.txt"));
-    EXPECT_TRUE(f.openFile("numerics/knearestneighbors_test.txt"));
+	io f;
+	EXPECT_TRUE(f.isFileExist("numerics/knearestneighbors_test.txt"));
+	EXPECT_TRUE(f.openFile("numerics/knearestneighbors_test.txt"));
 
-    int nRows = 0;
-    while (f.readLine())
-    {
-        //Count the number of non empty and not commented line with "#" as default comment
-        nRows++;
-    }
+	int nRows = 0;
+	while (f.readLine())
+	{
+		//Count the number of non empty and not commented line with "#" as default comment
+		nRows++;
+	}
 
-    //This data type has two dimensions
-    int nDim = 2;
-    //Number of nearest neighbors to find
-    int nn = 3;
+	//This data type has two dimensions
+	int nDim = 2;
+	//Number of nearest neighbors to find
+	int nn = 3;
 
-    double *data = nullptr;
-    double *dtest = nullptr;
-    int *knntest = nullptr;
+	double *data = nullptr;
+	double *dtest = nullptr;
+	int *knntest = nullptr;
 
-    //Allocate memory for reading the data
-    try
-    {
-        data = new double[nRows * nDim];
-        dtest = new double[nRows + 1];
-        knntest = new int[nRows * nn];
-    }
-    catch (std::bad_alloc &e)
-    {
-        std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-        std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
-    }
+	//Allocate memory for reading the data
+	try
+	{
+		data = new double[nRows * nDim];
+		dtest = new double[nRows + 1];
+		knntest = new int[nRows * nn];
+	}
+	catch (std::bad_alloc &e)
+	{
+		std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+		std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+	}
 
-    //!Rewind the file
-    f.rewindFile();
+	//!Rewind the file
+	f.rewindFile();
 
-    //!Read the array of data
-    EXPECT_TRUE(f.loadMatrix<double>(data, nRows, nDim));
+	//!Read the array of data
+	EXPECT_TRUE(f.loadMatrix<double>(data, nRows, nDim));
 
-    //Close the file
-    f.closeFile();
+	//Close the file
+	f.closeFile();
 
-    // kNearestNeighbor<double, flann::L2<double>> KNN(n, nDim, nn);
-    L2NearestNeighbor<double> KNN(nRows, nDim, nn);
+	// kNearestNeighbor<double, flann::L2<double>> KNN(n, nDim, nn);
+	L2NearestNeighbor<double> KNN(nRows, nDim, nn);
 
-    KNN.buildIndex(data);
+	EXPECT_EQ(KNN.numNearestNeighbors(), nn);
 
-    //using brute force to find neighbors
-    dtest[nRows] = std::numeric_limits<double>::max();
+	KNN.buildIndex(data);
 
-    for (int i = 0; i < nRows; i++)
-    {
-        int const IdI = i * nDim;
-        for (int j = 0; j < nRows; j++)
-        {
-            if (i == j)
-            {
-                continue;
-            }
-            int const IdJ = j * nDim;
-            double const dd[2] = {data[IdJ] - data[IdI], data[IdJ + 1] - data[IdI + 1]};
-            dtest[j] = dd[0] * dd[0] + dd[1] * dd[1];
-        }
-        int const Id = i * nn + nn;
-        std::fill(knntest + Id - nn, knntest + Id, nRows);
-        for (int j = 0; j < nRows; j++)
-        {
-            if (i == j)
-            {
-                continue;
-            }
-            int const Id3 = knntest[Id - 1];
-            if (dtest[j] < dtest[Id3])
-            {
-                int const Id2 = knntest[Id - 2];
-                if (dtest[j] < dtest[Id2])
-                {
-                    int const Id1 = knntest[Id - 3];
-                    if (dtest[j] < dtest[Id1])
-                    {
-                        knntest[Id - 1] = knntest[Id - 2];
-                        knntest[Id - 2] = knntest[Id - 3];
-                        knntest[Id - 3] = j;
-                    }
-                    else
-                    {
-                        knntest[Id - 1] = knntest[Id - 2];
-                        knntest[Id - 2] = j;
-                    }
-                }
-                else
-                {
-                    knntest[Id - 1] = j;
-                }
-            }
-        }
-    }
+	//using brute force to find neighbors
+	dtest[nRows] = std::numeric_limits<double>::max();
 
-    delete[] dtest;
+	for (int i = 0; i < nRows; i++)
+	{
+		int const IdI = i * nDim;
+		for (int j = 0; j < nRows; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			int const IdJ = j * nDim;
+			double const dd[2] = {data[IdJ] - data[IdI], data[IdJ + 1] - data[IdI + 1]};
+			dtest[j] = dd[0] * dd[0] + dd[1] * dd[1];
+		}
+		int const Id = i * nn + nn;
+		std::fill(knntest + Id - nn, knntest + Id, nRows);
+		for (int j = 0; j < nRows; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+			int const Id3 = knntest[Id - 1];
+			if (dtest[j] < dtest[Id3])
+			{
+				int const Id2 = knntest[Id - 2];
+				if (dtest[j] < dtest[Id2])
+				{
+					int const Id1 = knntest[Id - 3];
+					if (dtest[j] < dtest[Id1])
+					{
+						knntest[Id - 1] = knntest[Id - 2];
+						knntest[Id - 2] = knntest[Id - 3];
+						knntest[Id - 3] = j;
+					}
+					else
+					{
+						knntest[Id - 1] = knntest[Id - 2];
+						knntest[Id - 2] = j;
+					}
+				}
+				else
+				{
+					knntest[Id - 1] = j;
+				}
+			}
+		}
+	}
 
-    for (int i = 0; i < nRows; ++i)
-    {
-        int *p = KNN.NearestNeighbors(i);
-        int const Id = i * nn;
-        for (int j = 0; j < nn; j++)
-        {
-            //TODO ON MACOS there is a problem for one neighbor
-            //FIXME!
-            if (p[j] != knntest[Id + j])
-            {
-                std::cerr << "There is a difference in found neighbor:" << std::endl;
-                std::cerr << "Point i = " << i << "Flann neighbor = " << p[j] << " Brute force neighbor=" << knntest[Id + j] << std::endl;
-                continue;
-            }
-            EXPECT_EQ(p[j], knntest[Id + j]);
-        }
-    }
+	delete[] dtest;
 
-    delete[] knntest;
+	for (int i = 0; i < nRows; ++i)
+	{
+		int *p = KNN.NearestNeighbors(i);
+		int const Id = i * nn;
+		for (int j = 0; j < nn; j++)
+		{
+			//TODO ON MACOS there is a problem for one neighbor
+			//FIXME!
+			if (p[j] != knntest[Id + j])
+			{
+				std::cerr << "There is a difference in found neighbor:" << std::endl;
+				std::cerr << "Point i = " << i << "Flann neighbor = " << p[j] << " Brute force neighbor=" << knntest[Id + j] << std::endl;
+				continue;
+			}
+			EXPECT_EQ(p[j], knntest[Id + j]);
+		}
+	}
 
-    double *dists = nullptr;
-    dists = KNN.minDist();
+	delete[] knntest;
 
-    EXPECT_TRUE(dists != nullptr);
+	double *dists = nullptr;
+	dists = KNN.minDist();
 
-    for (int i = 0; i < nRows; ++i)
-    {
-        int const IdI = i * nDim;
-        int *p = KNN.NearestNeighbors(i);
-        for (int j = 2; j < nn; j++)
-        {
-            int const IdJ = p[j] * nDim;
-            double const dd[2] = {data[IdJ] - data[IdI], data[IdJ + 1] - data[IdI + 1]};
-            double const d = std::sqrt(dd[0] * dd[0] + dd[1] * dd[1]);
-            EXPECT_TRUE((dists[i] <= d));
-        }
-    }
+	EXPECT_TRUE(dists != nullptr);
 
-    delete[] data;
-    delete[] dists;
+	for (int i = 0; i < nRows; ++i)
+	{
+		int const IdI = i * nDim;
+		int *p = KNN.NearestNeighbors(i);
+		for (int j = 2; j < nn; j++)
+		{
+			int const IdJ = p[j] * nDim;
+			double const dd[2] = {data[IdJ] - data[IdI], data[IdJ + 1] - data[IdI + 1]};
+			double const d = std::sqrt(dd[0] * dd[0] + dd[1] * dd[1]);
+			EXPECT_TRUE((dists[i] <= d));
+		}
+	}
+
+	delete[] data;
+	delete[] dists;
 
 #endif //HAVE_FLANN
 }
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
