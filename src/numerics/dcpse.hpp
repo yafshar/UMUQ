c58bbab1d785920cf9a5f7e1cfc61356ba519ceb
ya.afshar <ya.afshar@gmail.com>
2018-05-07 17:55:08 -0400

    dc-pse

--- src/numerics/dcpse.hpp
+++ src/numerics/dcpse.hpp
@@ -16,13 +16,13 @@ template <typename T, class TF>
 class primitive
 {
   public:
-	inline T f(T const *x)
-	{
-		return static_cast<TF *>(this)->f(x);
-	}
+    inline T f(T const *x)
+    {
+        return static_cast<TF *>(this)->f(x);
+    }
 
   private:
-	friend TF;
+    friend TF;
 };
 
 /*!
@@ -36,10 +36,10 @@ template <typename T>
 class quartic_spline : public primitive<T, quartic_spline<T>>
 {
   public:
-	inline T f(T const *x)
-	{
-		return (*x > static_cast<T>(1)) ? T{} : 1 + (*x) * (*x) * (-6 + (*x) * (8 - 3 * (*x)));
-	}
+    inline T f(T const *x)
+    {
+        return (*x > static_cast<T>(1)) ? T{} : 1 + (*x) * (*x) * (-6 + (*x) * (8 - 3 * (*x)));
+    }
 };
 
 /*! \class dcpse
@@ -52,174 +52,239 @@ template <typename T>
 class dcpse
 {
   public:
-	/*!
+    /*!
      * \brief constructor
      * 
      * \param ndim             Dimensiononality
      */
-	dcpse(int ndim) : nDim(ndim) {}
+    dcpse(int ndim) : nDim(ndim) {}
 
-	/*!
+    /*!
      * \brief 
      * 
      * \param idata            A pointer to input data 
      * \param nPoints          Number of data points
      * \param minDist          Local characteristic inter-points spacing
      * \param kernel           Operator kernel
+     * 
      * \param beta             In multi-dimensional notation \f$ \beta=\left(\beta_1, \cdots, \beta_d \right) \f$
      *                         Notation for partial derivatives:
      *                         \f$ \begin{align} D^\beta = \frac{\partial^{|\beta|}} {\partial x_1^{\beta_1} \partial x_2^{\beta_2}\cdots\partial x_d^{\beta_d}}. \label{eq:1} \end{align} \f$
      * \param order            Order of accuracy (default is 2nd order accurate)
-	 * \param nENN             Number of extra nearest neighbors to aid in case of Vandermonde matrix sigularity (default is 2)
+	 * \param nENN             Number of extra nearest neighbors to aid in case of sigularity of the Vandermonde matrix (default is 2)
      * \param ratio            The \f$ \frac{h}{\epsilon} \f$ the default vale is one
      */
-	bool dcops(T *idata, int const nPoints, T *kernel, int *beta, int order = 2, int nENN = 2, T ratio = static_cast<T>(1))
-	{
-		// \f$ |\beta| = \beta_1 + \cdots + \beta_d \f$
-		int Beta = std::accumulate(beta, beta + nDim, 0);
-		if (Beta == 0)
-		{
-			std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-			std::cerr << "Zero order degree derivative is an interpolation!" << std::endl;
-			std::cerr << "Use the interpolation function!" << std::endl;
-			return false;
-		}
+    bool dcops(T *idata, int const nPoints, T *kernel, int *beta, int order = 2, int nENN = 2, T ratio = static_cast<T>(1))
+    {
+        // \f$ |\beta| = \beta_1 + \cdots + \beta_d \f$
+        int Beta = std::accumulate(beta, beta + nDim, 0);
+        if (Beta == 0)
+        {
+            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+            std::cerr << "Zero order degree derivative is an interpolation!" << std::endl;
+            std::cerr << "Use the interpolation function!" << std::endl;
+            return false;
+        }
 
-		T rhscoeff = (Beta % 2 == 0) ? static_cast<T>(1) : -static_cast<T>(1);
+        //\f$ (-1)^{|\beta|} \f$
+        T rhscoeff = (Beta % 2 == 0) ? static_cast<T>(1) : -static_cast<T>(1);
 
-		//Create an instance of polynomial object
-		polynomial<T> poly(nDim, order);
+        //Create an instance of polynomial object with polynomial degree of \f$ |\beta| + r -1 \f$
+        polynomial<T> poly(nDim, order + Beta - 1);
 
-		//size of the monomials
-		int msize = poly.monomialsize();
+        //Get the monomials size
+        //\f$ msize = \left(\begin{matrix} |\beta| + r + d -1 \\ d \end{matrix}\right) - \alpha_{\min} \f$
+        int msize = poly.monomialsize() - ((Beta % 2 == 0) ? 1 : 0);
 
-		//Extra check on the number of extra nearest neighbors
-		nENN = (nENN >= 0) ? nENN : 0;
+        if (kernel == nullptr)
+        {
+            try
+            {
+                kernel = new T[nPoints * msize];
+            }
+            catch (std::bad_alloc &e)
+            {
+                std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+                std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
+                return false;
+            }
+        }
 
-		//Finding \f$ K = monomial size + 2 \f$ nearest neighbors
-		L2NearestNeighbor<T> KNN(nPoints, nDim, msize + nENN);
+        //Extra check on the number of extra nearest neighbors
+        nENN = (nENN >= 0) ? nENN : 0;
 
-		//Construct a kd-tree index & do nearest neighbors search
-		KNN.buildIndex(idata);
+        //The number of points in the neighborhood of the operator
+        //\f$ = \text{monomial size} + \text{number of extra neighbors} \f$ nearest neighbors
+        //Finding nearest neighbors
+        L2NearestNeighbor<T> KNN(nPoints, nDim, msize + nENN);
 
-		EVectorX<T> B0;
-		B0.resize(msize);
+        //Construct a kd-tree index & do nearest neighbors search
+        KNN.buildIndex(idata);
 
-		//Filling the right hand side \f$ b \f$ of the linear system for the kernel coefficients
-		//\f$  {\mathbf A} ({\mathbf x}) {\mathbf a}^T({\mathbf x})={\mathbf b}  \f$
-		{
-			//get the monomial basis
-			int *alpha = poly.monomial_basis();
+        //Filling the right hand side \f$ b \f$ of the linear system for the kernel coefficients
+        //\f$  {\mathbf A} ({\mathbf x}) {\mathbf a}^T({\mathbf x})={\mathbf b}  \f$
+        EVectorX<T> B0;
+        B0.resize(msize);
+        {
+            //Get a pointer to the monomial basis
+            int *alpha = poly.monomial_basis();
 
-			for (int i = 0, id = 0; i < msize; i++)
-			{
-				int maxalpha = 0;
-				for (int j = 0; j < nDim; j++, id++)
-				{
-					maxalpha = std::max(maxalpha, std::abs(alpha[id] - beta[j]));
-				}
-				if (maxalpha)
-				{
-					B0(i) = T{};
-				}
-				else
-				{
-					T fact = static_cast<T>(1);
-					std::for_each(beta, beta + nDim, [&](int const b_j) { fact *= factorial<T>(b_j); });
-					B0(i) = rhscoeff * fact;
-				}
-			}
+            for (int i = 0, id = 0; i < msize; i++)
+            {
+                int maxalpha = 0;
+                for (int j = 0; j < nDim; j++, id++)
+                {
+                    maxalpha = std::max(maxalpha, std::abs(alpha[id] - beta[j]));
+                }
+                if (maxalpha)
+                {
+                    B0(i) = T{};
+                }
+                else
+                {
+                    T fact = static_cast<T>(1);
+                    std::for_each(beta, beta + nDim, [&](int const b_j) { fact *= factorial<T>(b_j); });
+                    B0(i) = rhscoeff * fact;
+                }
+            }
 
-			//TODO : check this again
-			//When applicable, and for stability reasons, set the zeroth moment to 5
-			if (rhscoeff > T{})
-			{
-				if (std::accumulate(alpha, alpha + nDim, 0) == 0)
-				{
-					B0(0) = static_cast<T>(5);
-				}
-			}
-		}
+            //TODO : check this again
+            //{It is mentioned that it should be always zero in order to obtain
+            //kernels with a vanishing zeroth-order moment that can be consistently
+            //evaluated at off-particle locations!}
+            //
+            //When applicable, and for stability reasons, set the zeroth moment to 5
+            if (rhscoeff > T{})
+            {
+                if (std::accumulate(alpha, alpha + nDim, 0) == 0)
+                {
+                    B0(0) = static_cast<T>(5);
+                }
+            }
+        }
 
-		//Number of nearest neighbours for each point
-		int nNN = KNN.numNearestNeighbors();
+        //Total number of nearest neighbours for each point
+        int nNN = KNN.numNearestNeighbors();
 
-		//Array for keeping the component-wise L1 distances
-		T L1Dist[nNN * nDim];
+        //Array for keeping the component-wise L1 distances
+        T L1Dist[nNN * nDim];
 
-		//Loop over all points
-		for (int i = 0; i < nPoints; i++)
-		{
-			int const IdI = i * nDim;
+        //Creating a transpose of the Vandermonde matrix
+        //with the size of monomials * monomials \f$  = l \times l \f$
+        EMatrixX<T> VMT;
+        VMT.resize(msize, nNN);
 
-			{
-				//A pointer to nearest neighbors indices of point i
-				int *nearestneighbors = KNN.nearestneighbors(i);
+        EVectorX<T> EM;
+        EM.resize(nNN);
 
-				//A pointer to nearest neighbors distances from the point i
-				T *nnDist = KNN.NearestNeighborsDistances(i);
+        EMatrixX<T> AM;
+        AM.resize(msize, msize);
 
-				//A pointer to the array for keeping the component-wise L1 distances
-				T *l1dist = L1Dist;
+        EVectorX<T> SV;
+        SV.resize(msize);
 
-				for (int j = 0; j < nNN; j++)
-				{
-					int const IdJ = nearestneighbors[j] * nDim;
+        //Loop over all points
+        for (int i = 0; i < nPoints; i++)
+        {
+            int const IdI = i * nDim;
 
-					T *Idata = idata + IdI;
-					T *Jdata = idata + IdJ;
+            //For each point \f$ {\mathbf x} \f$ we define \f$ \left\{{\mathbf z}_p({\mathbf x}) \right\}_{p=1}^{k} = \left\{{\mathbf x}_p - {\mathbf x} \right\}, \f$
+            //as the set of vectors pointing to \f$ {\mathbf x} \f$ from all neighboring points \f${\mathbf x}_p\f$ in the support of \f${\mathbf x}\f$.
+            {
+                //A pointer to nearest neighbors indices of point i
+                int *nearestneighbors = KNN.nearestneighbors(i);
 
-					for (int d = 0; d < nDim; d++)
-					{
-						*l1dist++ = *Jdata++ - *Idata++;
-					}
-				}
-			}
+                //A pointer to the array for keeping the component-wise L1 distances
+                T *l1dist = L1Dist;
 
-			//Compute component-wise average neighbor spacing
-			T sum(0);
-			std::for_each(L1Dist, L1Dist + nNN * nDim, [&](T const l_i) { sum += std::abs(l_i); });
+                //\f$ $\left\{{\mathbf z}_p({\mathbf x}) \right\}_{p=1}^{k} = \left\{{\mathbf x} - {\mathbf x}_p \right\} \f$
+                for (int j = 0; j < nNN; j++)
+                {
+                    int const IdJ = nearestneighbors[j] * nDim;
 
-			//Component-wise average neighbor spacing
-			sum /= static_cast<T>(nNN);
+                    T *Idata = idata + IdI;
+                    T *Jdata = idata + IdJ;
 
-			//Computing the smoothing length for each point \frac{h}{\epsilon} \sim ratio \f$
-			T const epsilon = sum / ratio;
+                    for (int d = 0; d < nDim; d++)
+                    {
+                        *l1dist++ = *Idata++ - *Jdata++;
+                    }
+                }
+            }
 
-			//Vectors pointing to x from all neighboring points
-			std::for_each(L1Dist, L1Dist + nNN * nDim, [&](T &l_i) { l_i /= epsilon; });
+            //Compute component-wise average neighbor spacing
+            T sum(0);
+            std::for_each(L1Dist, L1Dist + nNN * nDim, [&](T const l_i) { sum += std::abs(l_i); });
 
-			//Creating a transpose of the Vandermonde matrix
-			//with the size of monomials * monomials
-			EMatrixX<T> VT;
-			VT.resize(msize, msize);
+            //Component-wise average neighbor spacing \f$ h \f$
+            sum /= static_cast<T>(nNN);
 
-			for (int j = 0; j < msize; j++)
-			{
-				int const Id = j * nDim;
+            //Computing the smoothing length for each point \f$ \frac{h}{\epsilon} \sim ratio \f$
+            T const epsilon = sum / ratio;
 
-				//Evaluates a monomial at a point x
-				T col[msize];
-				poly.monomial_value(L1Dist + Id, col);
+            //Vectors pointing to \f$ {\mathbf x} \f$ from all neighboring points
+            std::for_each(L1Dist, L1Dist + nNN * nDim, [&](T &l_i) { l_i /= epsilon; });
 
-				VT.block(0, j, msize, 1) << col;
-			}
+            for (int j = 0; j < nNN; j++)
+            {
+                int const Id = j * nDim;
 
-			//LU decomposition of a matrix with complete pivoting, and related features.
-			Eigen::FullPivLU<EMatrixX<T>> lu(VT);
+                //Evaluates a monomial at a point \f$ {\mathbf x} \f$
+                T col[msize];
+                poly.monomial_value(L1Dist + Id, col);
 
-			if (lu.rank() < msize)
-			{
-			}
-			else
-			{
-			}
-		}
-	}
+                VMT.block(0, j, msize, 1) << Eigen::Map<EMatrixX<T>>(col);
+            }
+
+            {
+                T const epsilonsq = 2 * epsilon * epsilon;
+
+                //A pointer to nearest neighbors distances from the point i
+                T *nnDist = KNN.NearestNeighborsDistances(i);
+
+                for (int j = 0; j < nNN; j++)
+                {
+                    EM(j) = std::exp(-nnDist[j] * nnDist[j] / epsilonsq);
+                }
+            }
+
+            //\f$ \begin{matrix} {\mathbf A} ({\mathbf x}) = {\mathbf B}^T ({\mathbf x}) {\mathbf B} ({\mathbf x}) & \in \mathbb{R}^{l\times l} \\
+            //{\mathbf B} ({\mathbf x}) = {\mathbf E} ({\mathbf x}) {\mathbf V} ({\mathbf x}) & \in \mathbb{R}^{k\times l}\\
+            //{\mathbf b} = (-1)^{|\beta|} D^\beta {\mathbf P}({\mathbf x}) |_{{\mathbf x}=0}   & \in \mathbb{R}^{l\times 1}
+            //\end{matrix}
+            EMatrixX<T> BMT = VMT * EMatrixX<T>(EM.asDiagonal());
+
+            //LU decomposition of a matrix with complete pivoting, and related features.
+            Eigen::FullPivLU<EMatrixX<T>> lu(BMT);
+
+            //if necessary, remove redundant equations/coefficients
+            if (lu.rank() < msize)
+            {
+                std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
+                std::cerr << "There are some singularities! we use a least-squares solution!" << std::endl;
+
+                AM = BMT * BMT.transpose();
+
+                Eigen::JacobiSVD<EMatrixX<T>> svd(AM);
+                SV = svd.solve(B0);
+            }
+            else
+            {
+                EMatrixX<T> image = lu.image();
+                AM = image * image.transpose();
+
+                lu.compute(AM);
+
+                SV = lu.solve(B0);
+            }
+        } //Loop over all points
+
+        return true;
+    }
 
   private:
-	//Dimensiononality
-	int nDim;
+    //Dimensiononality
+    int nDim;
 };
 
 #endif
