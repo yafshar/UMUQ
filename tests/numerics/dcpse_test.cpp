46b2a7e16d2509996b2dc5caeba73dbb474704b6
ya.afshar <ya.afshar@gmail.com>
2018-05-16 16:52:52 -0400

    start the test file

--- tests/numerics/dcpse_test.cpp
+++ tests/numerics/dcpse_test.cpp
@@ -8,7 +8,20 @@
  */
 TEST(dcpse_test, HandlesDCoperators)
 {
+    dcpse<double> dc(1);
 
+    int npoints = 10;
+    double *idata = new double[npoints];
+    int nqpoints = npoints / 2;
+    double *qdata = new double[nqpoints];
+    double dx = 1. / npoints;
+    std::fill(idata, idata + npoints, dx);
+    std::partial_sum(idata, idata + npoints, idata, std::plus<double>());
+
+    for (int i = 0; i < npoints, i++)
+    {
+        std::cout << i << " " << idata[i] << std::endl;
+    }
 }
 
 int main(int argc, char **argv)
