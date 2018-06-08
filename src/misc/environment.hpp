82b2027e9026d6a32cae0532db0eb6cda70d41fc
ya.afshar <ya.afshar@gmail.com>
2018-06-08 15:27:40 -0400

    adding the new environment class for setting the torc environment

---
+++ src/misc/environment.hpp
@@ -0,0 +1,93 @@
+#ifndef UMUQ_ENVIRONMENT_H
+#define UMUQ_ENVIRONMENT_H
+
+#ifdef HAVE_CONFIG_H
+#include <UMUQ_config.h>
+#endif /* HAVE_CONFIG_H */
+
+#ifdef HAVE_LIBTORC
+#ifdef HAVE_LIBGTEST
+/*!
+ * \brief torcEnvironment class
+ * 
+ */
+class torcEnvironment : public ::testing::Environment
+#else
+class torcEnvironment
+#endif
+{
+  public:
+    /*!
+     * \brief Set up the torc object
+     * 
+     */
+    virtual void SetUp()
+    {
+        char **argv;
+        int argc = 0;
+
+        torc_init(argc, argv, 0);
+    }
+
+    /*!
+     * \brief Set up the torc object
+     * 
+     * \param argc 
+     * \param argv 
+     */
+    virtual void SetUp(int argc, char **argv)
+    {
+        torc_init(argc, argv, 0);
+    }
+
+    /*!
+     * \brief Finish the torc
+     * 
+     */
+    virtual void TearDown()
+    {
+        torc_finalize();
+    }
+
+    /*!
+     * \brief Destroy the torc Environment object
+     * 
+     */
+    virtual ~torcEnvironment() {}
+};
+#else
+/*!
+ * \brief torcEnvironment class
+ * 
+ */
+class torcEnvironment
+{
+  public:
+    /*!
+     * \brief Set up the torc object
+     * 
+     */
+    virtual void SetUp() {}
+   
+    /*!
+     * \brief Set up the torc object
+     * 
+     * \param argc 
+     * \param argv 
+     */
+    virtual void SetUp(int argc, char **argv) {}
+
+    /*!
+     * \brief Finish the torc
+     * 
+     */
+    virtual void TearDown() {}
+
+    /*!
+     * \brief Destroy the torc Environment object
+     * 
+     */
+    virtual ~torcEnvironment() {}
+};
+#endif //HAVE_LIBTORC
+#endif //UMUQ_ENVIRONMENT_H
