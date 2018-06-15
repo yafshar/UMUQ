88cde65549709b955d54ee916948d72c8b8196ff
ya.afshar <ya.afshar@gmail.com>
2018-06-15 17:39:10 -0400

    refactoring and fixing the datatype class completely

--- src/data/datatype.hpp
+++ src/data/datatype.hpp
@@ -1,366 +1,141 @@
 #ifndef UMUQ_DATATYPE_H
 #define UMUQ_DATATYPE_H
 
-#include <iostream>
-#include "../io/io.hpp"
+#include "database.hpp"
 
 /*!
-* \brief structure for sorting Fvalue for entires of database structure
-* 
-* \tparam T data type
-*
-* \param idx      an intger argument for indexing
-* \param nsel     an integer argument for selection of leaders only
-* \param Fvalue   a double argument for function value
-*/
+ * \brief Instance of database object for current database
+ * 
+ */
+database<double> currentData;
+
+/*!
+ * \brief Instance of database object for the full database information
+ * 
+ */
+database<double> fullData;
+
+/*!
+ * \brief Instance of database object for experimental data
+ * 
+ */
+database<double> exprimentData;
+
+/*!
+ * \brief Updating the data information at each point @iParray 
+ * 
+ * \tparam T          Data type (T is a floating-point type)
+ * 
+ * \param iParray     Points or sampling points array
+ * \param iFvalue     Function value at the sampling point 
+ * \param iGarray     Array of data @iParray 
+ * \param ndimGarray  Dimension of G array
+ * \param iSurrogate  Surrogate
+ */
 template <typename T>
-struct sort_t
+void current_updateTask(T const *iParray, T const *iFvalue, T const *iGarray, int const *ndimGarray, int const *iSurrogate)
 {
-    int idx;
-    int nsel;
-    T Fvalue;
-};
+    std::size_t pos;
 
-/*!
- * \brief database structure
- *
- * \tparam T data type
- * \tparam T type of database structure
- * 
- * \param entry
- * \param entries an integer argument shows the size of entry
- * \param m A mutex object
- */
-template <class DBT>
-class database
-{
-  public:
-    /*!
-     *  \brief constructor for the database structure
-     *    
-     */
-    database() : entry(nullptr),
-                 entries(0)
+    pthread_mutex_lock(&currentData.m);
+    pos = currentData.idxPos;
+    currentData.idxPos++;
+    pthread_mutex_unlock(&currentData.m);
+
+    if (pos < currentData.entries)
     {
-        pthread_mutex_init(&m, nullptr);
-    }
+        std::copy(iParray, iParray + currentData.ndimParray, currentData.Parray.get() + pos * currentData.ndimParray);
 
-    /*!
-     * /brief Init function taking two arguments and initialize the structure.
-     *
-     *  \param nsize1 an integer argument.
-     */
-    bool init(int nsize1);
+        currentData.Fvalue[pos] = *iFvalue;
 
-    /*!
-     * /brief A member updating the database
-     *
-     *  \param Parray     a double array of points.
-     *  \param ndimParray an integer argument, shows the size of Parray
-     *  \param Fvalue     a double value 
-     *  \param Garray     a double array
-     *  \param ndimGarray an integer argument, shows the size of Garray
-     *  \param surrogate  an integer argument for the surrogate model (default 0, no surrogate)
-     */
-    bool update(double *Parray, int ndimParray, double Fvalue, double *Garray, int ndimGarray, int surrogate = 0);
-
-    /*!
-     * /brief function for sorting elemnts of an array for database elements.
-     *
-     *  Sorts the entries elements of the array pointed to by list, each 
-     *  element size bytes long, using the compar function to determine the order.
-     */
-    inline void sort(sort_t<double> *list);
-
-    /*!
-     * /brief function for printing  the data
-     *
-     */
-    bool print()
-    {
-        if (entry != nullptr)
+        if (*ndimGarray > 0)
         {
-            std::cout << "---- database priniting ----" << std::endl;
-
-            for (int pos = 0; pos < entries; pos++)
-            {
-                if (entry[pos].Parray != nullptr)
-                {
-                    int j;
-                    std::cout << "ENTRY"
-                              << std::setw(5) << pos << " : POINT(";
-                    for (j = 0; j < entry[pos].ndimParray - 1; j++)
-                    {
-                        std::cout << std::setw(20) << entry[pos].Parray[j] << ", ";
-                    }
-                    std::cout << std::setw(20) << entry[pos].Parray[j] << ") Fvalue="
-                              << std::setw(20) << entry[pos].Fvalue << " Surrogate="
-                              << std::setw(20) << entry[pos].surrogate << std::endl;
-                }
-                if (entry[pos].Garray != nullptr)
-                {
-                    int i;
-                    std::cout << "Garray=[";
-                    for (i = 0; i < entry[pos].ndimGarray - 1; i++)
-                    {
-                        std::cout << std::setw(20) << entry[pos].Garray[i] << ", ";
-                    }
-                    std::cout << std::setw(20) << entry[pos].Garray[i] << "]" << std::endl;
-                }
-            }
-
-            std::cout << "----------------------------" << std::endl;
-
-            return true;
-        }
-        return false;
-    }
-
-    /*!
-     * /brief function for dumping the data
-     *
-     */
-    bool dump(const char *fname = "")
-    {
-        if (entry != nullptr)
-        {
-            char fileName[256];
-            if (strlen(fname) == 0)
-            {
-                sprintf(fileName, "db_%03d.txt", entries - 1);
-            }
-            else
-            {
-                sprintf(fileName, "%s_%03d.txt", fname, entries - 1);
-            }
-
-            io f;
-            if (f.openFile(fileName, f.out | f.trunc))
-            {
-                double **tmp = nullptr;
-                int nRows = 2 + (int)(entry[0].Garray != nullptr);
-                tmp = new double *[3];
-
-                for (int pos = 0; pos < entries - 1; pos++)
-                {
-                    tmp[0] = entry[pos].Parray;
-                    tmp[1] = &entry[pos].Fvalue;
-                    tmp[2] = entry[pos].Garray;
-
-                    int nCols = entry[pos].ndimParray + 1 + entry[pos].ndimGarray;
-
-                    if (!f.saveMatrix<double>(tmp, nRows, nCols, 2))
-                    {
-                        return false;
-                    }
-                }
-
-                delete[] tmp;
-
-                f.closeFile();
-
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
-
-    /*!
-     * /brief function for loading the data
-     *
-     */
-    bool load(const char *fname)
-    {
-        if (entry != nullptr)
-        {
-            char fileName[256];
-            if (strlen(fname) == 0)
-            {
-                sprintf(fileName, "db_%03d.txt", entries - 1);
-            }
-            else
-            {
-                sprintf(fileName, "%s_%03d.txt", fname, entries - 1);
-            }
-
-            io f;
-            if (f.openFile(fileName, f.in))
-            {
-                double **tmp = nullptr;
-
-                int nRows = 2 + (int)(entry[0].Garray != nullptr);
-                tmp = new double *[3];
-
-                for (int pos = 0; pos < entries - 1; pos++)
-                {
-                    tmp[0] = entry[pos].Parray;
-                    tmp[1] = &entry[pos].Fvalue;
-                    tmp[2] = entry[pos].Garray;
-
-                    int nCols = entry[pos].ndimParray + 1;
-
-                    if (!f.loadMatrix<double>(tmp, nRows, nCols, 1))
-                    {
-                        return false;
-                    }
-                }
-
-                delete[] tmp;
-
-                f.closeFile();
-
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
-
-  public:
-    //! Data container
-    DBT *entry;
-
-    //! Number of entries
-    int entries;
-
-  private:
-  
-    //! Mutex object
-    pthread_mutex_t m;
-};
-
-template <class T>
-bool database<T>::init(int nsize1)
-{
-    if (entry == nullptr)
-    {
-        try
-        {
-            entry = new T[nsize1];
-        }
-        catch (std::bad_alloc &e)
-        {
-            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
-            return false;
-        };
-
-        for (int i = 0; i < nsize1; i++)
-        {
-            entry[i] = (T)0;
-        }
-        return true;
-    }
-    return false;
-}
-
-template <class T>
-bool database<T>::update(double *Parray, int ndimParray, double Fvalue, double *Garray, int ndimGarray, int surrogate)
-{
-    int pos;
-
-    pthread_mutex_lock(&m);
-    pos = entries;
-    entries++;
-    pthread_mutex_unlock(&m);
-
-    if (ndimParray > entry[pos].ndimParray)
-    {
-        if (entry[pos].Parray != nullptr)
-        {
-            delete[] entry[pos].Parray;
+            std::copy(iGarray, iGarray + currentData.ndimGarray, currentData.Garray.get() + pos * currentData.ndimGarray);
         }
 
-        try
+        if (*iSurrogate < std::numeric_limits<int>::max())
         {
-            entry[pos].Parray = new double[ndimParray];
-        }
-        catch (std::bad_alloc &e)
-        {
-            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
-            return false;
+            currentData.Surrogate[pos] = *iSurrogate;
         }
     }
-    entry[pos].ndimParray = ndimParray;
-
-    for (int i = 0; i < ndimParray; i++)
-    {
-        entry[pos].Parray[i] = Parray[i];
-    }
-
-    entry[pos].Fvalue = Fvalue;
-
-    if (ndimGarray > entry[pos].ndimGarray)
-    {
-        if (entry[pos].Garray != nullptr)
-        {
-            delete[] entry[pos].Garray;
-        }
-
-        try
-        {
-            entry[pos].Garray = new double[ndimGarray];
-        }
-        catch (std::bad_alloc &e)
-        {
-            std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-            std::cerr << " Failed to allocate memory : " << e.what() << std::endl;
-            return false;
-        }
-    }
-    entry[pos].ndimGarray = ndimGarray;
-
-    for (int i = 0; i < ndimGarray; i++)
-    {
-        entry[pos].Garray[i] = Garray[i];
-    }
-
-    entry[pos].surrogate = surrogate;
-    return true;
 }
 
 /*!
- * \brief Pointer to a function that compares two elements.
- *   
- *  This function is called repeatedly by qsort to compare two elements.
+ * \brief Updating the data information at each point @iParray 
+ * 
+ * \tparam T          Data type (T is a floating-point type)
+ * 
+ * \param iParray     Points or sampling points array
+ * \param iFvalue     Function value at the sampling point 
+ * \param iGarray     Array of data @iParray 
+ * \param ndimGarray  Dimension of G array
+ * \param iSurrogate  Surrogate
  */
-int compar_desc(const void *p1, const void *p2)
+template <typename T>
+void full_updateTask(T const *iParray, T const *iFvalue, T const *iGarray, int const *ndimGarray, int const *iSurrogate)
 {
-    const sort_t<double> *s1 = static_cast<const sort_t<double> *>(p1);
-    const sort_t<double> *s2 = static_cast<const sort_t<double> *>(p2);
+    std::size_t pos;
 
-    /* -: ascending order, +: descending order */
-    return (s2->nsel - s1->nsel);
+    pthread_mutex_lock(&fullData.m);
+    pos = fullData.idxPos;
+    fullData.idxPos++;
+    pthread_mutex_unlock(&fullData.m);
+
+    if (pos < fullData.entries)
+    {
+        std::copy(iParray, iParray + fullData.ndimParray, fullData.Parray.get() + pos * fullData.ndimParray);
+
+        fullData.Fvalue[pos] = *iFvalue;
+
+        if (*ndimGarray > 0)
+        {
+            std::copy(iGarray, iGarray + fullData.ndimGarray, fullData.Garray.get() + pos * fullData.ndimGarray);
+        }
+
+        if (*iSurrogate < std::numeric_limits<int>::max())
+        {
+            fullData.Surrogate[pos] = *iSurrogate;
+        }
+    }
 }
 
 /*!
- * \brief quick sort
+ * \brief Updating the data information at each point @iParray 
  * 
- * \tparam T 
+ * \tparam T          Data type (T is a floating-point type)
  * 
- * \param list array which we want to sort it 
+ * \param iParray     Points or sampling points array
+ * \param iFvalue     Function value at the sampling point 
+ * \param iGarray     Array of data @iParray 
+ * \param ndimGarray  Dimension of G array
+ * \param iSurrogate  Surrogate
  */
-template <class T>
-inline void database<T>::sort(sort_t<double> *list)
+template <typename T>
+void expr_updateTask(T const *iParray, T const *iFvalue, T const *iGarray, int const *ndimGarray, int const *iSurrogate)
 {
-    qsort(list, entries, sizeof(sort_t<double>), compar_desc);
+    std::size_t pos;
+
+    pthread_mutex_lock(&exprimentData.m);
+    pos = exprimentData.idxPos;
+    exprimentData.idxPos++;
+    pthread_mutex_unlock(&exprimentData.m);
+
+    if (pos < exprimentData.entries)
+    {
+        std::copy(iParray, iParray + exprimentData.ndimParray, exprimentData.Parray.get() + pos * exprimentData.ndimParray);
+
+        exprimentData.Fvalue[pos] = *iFvalue;
+
+        if (*ndimGarray > 0)
+        {
+            std::copy(iGarray, iGarray + exprimentData.ndimGarray, exprimentData.Garray.get() + pos * exprimentData.ndimGarray);
+        }
+
+        if (*iSurrogate < std::numeric_limits<int>::max())
+        {
+            exprimentData.Surrogate[pos] = *iSurrogate;
+        }
+    }
 }
-
-template<typename T>
-struct cgdb_t : public database<cgdbp_t<T>>
-{
-};
-
-template<typename T>
-struct db_t : public database<dbp_t<T>>
-{
-};
-
-template<typename T>
-struct resdb_t : public database<resdbp_t<T>>
-{
-};
 
 #endif
