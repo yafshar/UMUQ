19d3816b36d1214654a6befdb4f93b09cd801e49
ya.afshar <ya.afshar@gmail.com>
2018-06-18 14:34:02 -0400

    update to the new format

--- tests/data/stdata_test.cpp
+++ tests/data/stdata_test.cpp
@@ -5,28 +5,27 @@
 //! Tests parse
 TEST(streamdatatype, HandlesConstruction)
 {
-    stdata *d1 = new stdata;
+    stdata<double> *d1 = new stdata<double>;
 
-    EXPECT_EQ(0, d1->Nth);
-    EXPECT_EQ(0, d1->MaxStages);
-    EXPECT_EQ(0, d1->PopSize);
-    EXPECT_EQ(0, d1->auxil_size);
-    EXPECT_EQ(0, d1->MinChainLength);
-    EXPECT_EQ(1e6, d1->MaxChainLength);
+    EXPECT_EQ(0, d1->nDim);
+    EXPECT_EQ(0, d1->maxGenerations);
+    EXPECT_EQ(0, d1->populationSize);
+    EXPECT_EQ(0, d1->auxilSize);
+    EXPECT_EQ(0, d1->minChainLength);
+    EXPECT_EQ(1e6, d1->maxChainLength);
     EXPECT_DOUBLE_EQ(0, d1->lb);
     EXPECT_DOUBLE_EQ(0, d1->ub);
     EXPECT_DOUBLE_EQ(1.0, d1->TolCOV);
     EXPECT_DOUBLE_EQ(0.2, d1->bbeta);
     EXPECT_EQ(280675, d1->seed);
-    EXPECT_EQ(0, d1->prior_type);
-    EXPECT_EQ(0, d1->prior_count);
-    EXPECT_EQ(0, d1->iplot);
-    EXPECT_EQ(1, d1->icdump);
-    EXPECT_EQ(0, d1->ifdump);
-    EXPECT_EQ(0, d1->LastNum);
-    EXPECT_EQ(0, d1->use_proposal_cma);
-    EXPECT_EQ(0, d1->use_local_cov);
-    EXPECT_EQ(0, d1->local_scale);
+    EXPECT_EQ(0, d1->priorType);
+    EXPECT_EQ(0, d1->priorCount);
+    EXPECT_EQ(0, d1->iPlot);
+    EXPECT_EQ(1, d1->saveData);
+    EXPECT_EQ(0, d1->lastPopulationSize);
+    EXPECT_EQ(0, d1->useCmaProposal);
+    EXPECT_EQ(0, d1->useLocalCovariance);
+    EXPECT_EQ(0, d1->localScale);
 
     EXPECT_EQ(100, d1->options.MaxIter);
     EXPECT_DOUBLE_EQ(1e-6, d1->options.Tolerance);
@@ -35,28 +34,27 @@ TEST(streamdatatype, HandlesConstruction)
 
     delete d1;
 
-    stdata d2(4, 20, 1024);
+    stdata<double> d2(4, 20, 1024);
 
-    EXPECT_EQ(4, d2.Nth);
-    EXPECT_EQ(20, d2.MaxStages);
-    EXPECT_EQ(1024, d2.PopSize);
-    EXPECT_EQ(0, d2.auxil_size);
-    EXPECT_EQ(0, d2.MinChainLength);
-    EXPECT_EQ(1e6, d2.MaxChainLength);
+    EXPECT_EQ(4, d2.nDim);
+    EXPECT_EQ(20, d2.maxGenerations);
+    EXPECT_EQ(1024, d2.populationSize);
+    EXPECT_EQ(0, d2.auxilSize);
+    EXPECT_EQ(0, d2.minChainLength);
+    EXPECT_EQ(1e6, d2.maxChainLength);
     EXPECT_DOUBLE_EQ(-6, d2.lb);
     EXPECT_DOUBLE_EQ(+6, d2.ub);
     EXPECT_DOUBLE_EQ(1.0, d2.TolCOV);
     EXPECT_DOUBLE_EQ(0.2, d2.bbeta);
     EXPECT_EQ(280675, d2.seed);
-    EXPECT_EQ(0, d2.prior_type);
-    EXPECT_EQ(0, d2.prior_count);
-    EXPECT_EQ(0, d2.iplot);
-    EXPECT_EQ(1, d2.icdump);
-    EXPECT_EQ(0, d2.ifdump);
-    EXPECT_EQ(1024, d2.LastNum);
-    EXPECT_EQ(0, d2.use_proposal_cma);
-    EXPECT_EQ(0, d2.use_local_cov);
-    EXPECT_EQ(0, d2.local_scale);
+    EXPECT_EQ(0, d2.priorType);
+    EXPECT_EQ(0, d2.priorCount);
+    EXPECT_EQ(0, d2.iPlot);
+    EXPECT_EQ(1, d2.saveData);
+    EXPECT_EQ(1024, d2.lastPopulationSize);
+    EXPECT_EQ(0, d2.useCmaProposal);
+    EXPECT_EQ(0, d2.useLocalCovariance);
+    EXPECT_EQ(0, d2.localScale);
 
     EXPECT_EQ(100, d2.options.MaxIter);
     EXPECT_DOUBLE_EQ(1e-6, d2.options.Tolerance);
@@ -65,50 +63,49 @@ TEST(streamdatatype, HandlesConstruction)
 
     d2.destroy();
 
-    EXPECT_EQ(NULL, d2.lowerbound);
+    EXPECT_EQ(NULL, d2.lowerBound);
 }
 
 //! Tests load input file
 TEST(streamdatatype, HandlesIO)
 {
-    stdata d1;
-    EXPECT_TRUE(d1.load("test.txt"));
+    stdata<double> d1;
+    EXPECT_TRUE(d1.load("./data/test.txt"));
 
-    EXPECT_EQ(4, d1.Nth);
-    EXPECT_EQ(20, d1.MaxStages);
-    EXPECT_EQ(5000, d1.PopSize);
-    EXPECT_EQ(0, d1.auxil_size);
-    EXPECT_EQ(1, d1.MinChainLength);
-    EXPECT_EQ(1, d1.MaxChainLength);
+    EXPECT_EQ(4, d1.nDim);
+    EXPECT_EQ(20, d1.maxGenerations);
+    EXPECT_EQ(5000, d1.populationSize);
+    EXPECT_EQ(0, d1.auxilSize);
+    EXPECT_EQ(1, d1.minChainLength);
+    EXPECT_EQ(1, d1.maxChainLength);
     EXPECT_DOUBLE_EQ(0, d1.lb);
     EXPECT_DOUBLE_EQ(0, d1.ub);
     EXPECT_DOUBLE_EQ(1.0, d1.TolCOV);
     EXPECT_DOUBLE_EQ(0.04, d1.bbeta);
     EXPECT_EQ(280675, d1.seed);
-    EXPECT_EQ(0, d1.prior_type);
-    EXPECT_EQ(1, d1.prior_count);
-    EXPECT_EQ(0, d1.iplot);
-    EXPECT_EQ(1, d1.icdump);
-    EXPECT_EQ(1, d1.ifdump);
-    EXPECT_EQ(5000, d1.LastNum);
-    EXPECT_EQ(0, d1.use_proposal_cma);
-    EXPECT_EQ(0, d1.use_local_cov);
-    EXPECT_DOUBLE_EQ(0, d1.local_scale);
+    EXPECT_EQ(0, d1.priorType);
+    EXPECT_EQ(1, d1.priorCount);
+    EXPECT_EQ(0, d1.iPlot);
+    EXPECT_EQ(1, d1.saveData);
+    EXPECT_EQ(5000, d1.lastPopulationSize);
+    EXPECT_EQ(0, d1.useCmaProposal);
+    EXPECT_EQ(0, d1.useLocalCovariance);
+    EXPECT_DOUBLE_EQ(0, d1.localScale);
 
     EXPECT_EQ(1000, d1.options.MaxIter);
     EXPECT_DOUBLE_EQ(1e-12, d1.options.Tolerance);
     EXPECT_EQ(1, d1.options.Display);
     EXPECT_DOUBLE_EQ(1e-4, d1.options.Step);
 
-    EXPECT_DOUBLE_EQ(0.05, d1.lowerbound[0]);
-    EXPECT_DOUBLE_EQ(3.0, d1.lowerbound[1]);
-    EXPECT_DOUBLE_EQ(6.01, d1.lowerbound[2]);
-    EXPECT_DOUBLE_EQ(0.0001, d1.lowerbound[3]);
+    EXPECT_DOUBLE_EQ(0.05, d1.lowerBound[0]);
+    EXPECT_DOUBLE_EQ(3.0, d1.lowerBound[1]);
+    EXPECT_DOUBLE_EQ(6.01, d1.lowerBound[2]);
+    EXPECT_DOUBLE_EQ(0.0001, d1.lowerBound[3]);
 
-    EXPECT_DOUBLE_EQ(10.0, d1.upperbound[0]);
-    EXPECT_DOUBLE_EQ(4.0, d1.upperbound[1]);
-    EXPECT_DOUBLE_EQ(15.0, d1.upperbound[2]);
-    EXPECT_DOUBLE_EQ(1.0, d1.upperbound[3]);  
+    EXPECT_DOUBLE_EQ(10.0, d1.upperBound[0]);
+    EXPECT_DOUBLE_EQ(4.0, d1.upperBound[1]);
+    EXPECT_DOUBLE_EQ(15.0, d1.upperBound[2]);
+    EXPECT_DOUBLE_EQ(1.0, d1.upperBound[3]);  
 }
 
 int main(int argc, char **argv)
