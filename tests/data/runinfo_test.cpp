258bf7efe076f54f17fa2e9078ac0efa3ef82ec8
ya.afshar <ya.afshar@gmail.com>
2018-06-18 12:22:45 -0400

    complete the test and add IO functionality test

--- tests/data/runinfo_test.cpp
+++ tests/data/runinfo_test.cpp
@@ -5,32 +5,93 @@
 //! Tests parse
 TEST(runinfo_test, HandlesConstruction)
 {
-    runinfo<double> r;
+    runinfo<double> runinfoObj;
 
-    EXPECT_EQ(0, r.nDim);
-    EXPECT_EQ(0, r.maxGenerations);
-    EXPECT_EQ(0, r.Generation);
+    EXPECT_EQ(0, runinfoObj.nDim);
+    EXPECT_EQ(0, runinfoObj.maxGenerations);
+    EXPECT_EQ(0, runinfoObj.Generation);
 
-    EXPECT_TRUE(r.reset(2, 40));
+    EXPECT_TRUE(runinfoObj.reset(2, 40));
 
-    EXPECT_EQ(2, r.nDim);
-    EXPECT_EQ(40, r.maxGenerations);
+    EXPECT_EQ(2, runinfoObj.nDim);
+    EXPECT_EQ(40, runinfoObj.maxGenerations);
 
-    for (int i = 0; i < r.maxGenerations; i++)
+    for (int i = 0; i < runinfoObj.maxGenerations; i++)
     {
         if (i == 0)
         {
-            EXPECT_DOUBLE_EQ(std::numeric_limits<double>::max(), r.CoefVar[i]);
+            EXPECT_DOUBLE_EQ(std::numeric_limits<double>::max(), runinfoObj.CoefVar[i]);
         }
         else
         {
-            EXPECT_DOUBLE_EQ(0.0, r.CoefVar[i]);
+            EXPECT_DOUBLE_EQ(0.0, runinfoObj.CoefVar[i]);
         }
-        EXPECT_DOUBLE_EQ(0.0, r.p[i]);
-        EXPECT_EQ(0, r.currentuniques[i]);
-        EXPECT_DOUBLE_EQ(0.0, r.logselection[i]);
-        EXPECT_DOUBLE_EQ(0.0, r.acceptance[i]);
+        EXPECT_DOUBLE_EQ(0.0, runinfoObj.p[i]);
+        EXPECT_EQ(0, runinfoObj.currentuniques[i]);
+        EXPECT_DOUBLE_EQ(0.0, runinfoObj.logselection[i]);
+        EXPECT_DOUBLE_EQ(0.0, runinfoObj.acceptance[i]);
     }
+}
+
+//! Test IO functionality
+TEST(runinfo_test, HandlesIO)
+{
+    std::remove("runinfo.txt");
+
+    //Create an instance of runinfo object, initialize it
+    //to some random value and save it to a file @"runinfo.txt"
+    {
+        runinfo<double> runinfoObj(2, 10);
+
+        EXPECT_EQ(2, runinfoObj.nDim);
+        EXPECT_EQ(10, runinfoObj.maxGenerations);
+
+        runinfoObj.Generation = 9;
+
+        for (int i = 0; i < runinfoObj.maxGenerations; i++)
+        {
+            runinfoObj.CoefVar[i] = static_cast<double>(i);
+            runinfoObj.p[i] = static_cast<double>(i * i);
+            runinfoObj.currentuniques[i] = i;
+            runinfoObj.logselection[i] = static_cast<double>(i * i * i);
+            runinfoObj.acceptance[i] = static_cast<double>(i) / 10.;
+            runinfoObj.meantheta[i * runinfoObj.nDim] = static_cast<double>(i);
+            runinfoObj.meantheta[i * runinfoObj.nDim + 1] = static_cast<double>(i);
+        }
+        runinfoObj.SS[0] = 12.;
+        runinfoObj.SS[1] = 123.;
+        runinfoObj.SS[2] = 112.;
+        runinfoObj.SS[3] = 13.;
+
+        EXPECT_TRUE(runinfoObj.save());
+    }
+    
+    //Create an instance of runinfo object, initialize it
+    //from a file @"runinfo.txt"
+    {
+        runinfo<double> runinfoObj;
+
+        EXPECT_TRUE(runinfoObj.load());
+        EXPECT_EQ(2, runinfoObj.nDim);
+        EXPECT_EQ(10, runinfoObj.maxGenerations);
+        for (int i = 0; i < runinfoObj.maxGenerations; i++)
+        {
+            EXPECT_DOUBLE_EQ(static_cast<double>(i), runinfoObj.CoefVar[i]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i * i), runinfoObj.p[i]);
+            EXPECT_EQ(i, runinfoObj.currentuniques[i]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i * i * i), runinfoObj.logselection[i]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i) / 10., runinfoObj.acceptance[i]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i), runinfoObj.meantheta[i * runinfoObj.nDim]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i), runinfoObj.meantheta[i * runinfoObj.nDim + 1]);
+        }
+
+        EXPECT_DOUBLE_EQ(12., runinfoObj.SS[0]);
+        EXPECT_DOUBLE_EQ(123., runinfoObj.SS[1]);
+        EXPECT_DOUBLE_EQ(112., runinfoObj.SS[2]);
+        EXPECT_DOUBLE_EQ(13., runinfoObj.SS[3]);
+    }
+
+    std::remove("runinfo.txt");
 }
 
 int main(int argc, char **argv)
