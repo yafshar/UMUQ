99d93d50013e8a1eb96c4c39eedbb94c87d4a508
ya.afshar <ya.afshar@gmail.com>
2018-06-18 08:39:52 -0400

    fixing the runinfo test

--- tests/data/runinfo_test.cpp
+++ tests/data/runinfo_test.cpp
@@ -5,19 +5,31 @@
 //! Tests parse
 TEST(runinfo_test, HandlesConstruction)
 {
-    runinfo_t r;
+    runinfo<double> r;
 
-    EXPECT_EQ(0, r.Gen);
-    EXPECT_EQ(0, r.probdim);
-    EXPECT_EQ(0, r.maxgens);
+    EXPECT_EQ(0, r.nDim);
+    EXPECT_EQ(0, r.maxGenerations);
+    EXPECT_EQ(0, r.Generation);
 
-    r.init(2, 40);
-    EXPECT_EQ(2, r.probdim);
-    EXPECT_EQ(40, r.maxgens);
+    EXPECT_TRUE(r.reset(2, 40));
 
-    for (int i = 0; i < r.maxgens; i++)
+    EXPECT_EQ(2, r.nDim);
+    EXPECT_EQ(40, r.maxGenerations);
+
+    for (int i = 0; i < r.maxGenerations; i++)
     {
+        if (i == 0)
+        {
+            EXPECT_DOUBLE_EQ(std::numeric_limits<double>::max(), r.CoefVar[i]);
+        }
+        else
+        {
+            EXPECT_DOUBLE_EQ(0.0, r.CoefVar[i]);
+        }
         EXPECT_DOUBLE_EQ(0.0, r.p[i]);
+        EXPECT_EQ(0, r.currentuniques[i]);
+        EXPECT_DOUBLE_EQ(0.0, r.logselection[i]);
+        EXPECT_DOUBLE_EQ(0.0, r.acceptance[i]);
     }
 }
 
