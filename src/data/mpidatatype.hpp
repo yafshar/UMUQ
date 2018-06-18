e94272d78e6bf93337d3ab8243604c3e96c0d38e
ya.afshar <ya.afshar@gmail.com>
2018-06-18 13:23:37 -0400

    this header file is a wrapper for getting the MPI data type of a templated type using variable templates c++14 feature

--- src/data/mpidatatype.hpp
+++ src/data/mpidatatype.hpp
@@ -1,27 +1,16 @@
 #ifndef UMUQ_MPIDATATYPE_H
 #define UMUQ_MPIDATATYPE_H
 
-/*! \class MPITypeWrapper
- * \brief This struct wrappers the MPI data type value for the given C++ type.
+/*! 
+ * \brief MPI data types variable template wrapper for the given C++ type.
  *
+ * \tparam T Data type
  * Any valid MPI data type value must have a corresponding explicit template instantiation below.
  */
 template <typename T>
-struct MPITypeWrapper
-{
-	/*!
-     * \brief Construct a new MPITypeWrapper object
-     * 
-     */
-	MPITypeWrapper()
-	{
-		std::cerr << "Error : " << __FILE__ << ":" << __LINE__ << " : " << std::endl;
-		std::cerr << " This type is not currently supported!" << std::endl;
-		throw(std::runtime_error("Wrong type!"));
-	}
-	//! MPI data type
-	MPI_Datatype type;
-};
+constexpr MPI_Datatype MPIDatatype = MPI_DATATYPE_NULL; // variable template
+
+//!TODO: Complete the list
 
 /*!
  * \brief Explicit instantiation for 
@@ -38,49 +27,41 @@ struct MPITypeWrapper
  * \b unsigned short
  * \b unsigned int
  * \b unsigned long
- * \b std::size_t
  */
 template <>
-MPITypeWrapper<char>::MPITypeWrapper() : type(MPI_CHAR) {}
-template <>
-MPITypeWrapper<signed short>::MPITypeWrapper() : type(MPI_SHORT) {}
-template <>
-MPITypeWrapper<int>::MPITypeWrapper() : type(MPI_INT) {}
-template <>
-MPITypeWrapper<signed long>::MPITypeWrapper() : type(MPI_LONG) {}
-template <>
-MPITypeWrapper<signed long long>::MPITypeWrapper() : type(MPI_LONG_LONG_INT) {}
-template <>
-MPITypeWrapper<float>::MPITypeWrapper() : type(MPI_FLOAT) {}
-template <>
-MPITypeWrapper<double>::MPITypeWrapper() : type(MPI_DOUBLE) {}
-template <>
-MPITypeWrapper<long double>::MPITypeWrapper() : type(MPI_LONG_DOUBLE) {}
-template <>
-MPITypeWrapper<unsigned char>::MPITypeWrapper() : type(MPI_UNSIGNED_CHAR) {}
-template <>
-MPITypeWrapper<unsigned short>::MPITypeWrapper() : type(MPI_UNSIGNED_SHORT) {}
-template <>
-MPITypeWrapper<unsigned int>::MPITypeWrapper() : type(MPI_UNSIGNED) {}
-template <>
-MPITypeWrapper<unsigned long>::MPITypeWrapper() : type(MPI_UNSIGNED_LONG) {}
+constexpr MPI_Datatype MPIDatatype<char> = MPI_CHAR;
 
-/*!
- * \brief MPITypeof maps the type of T to an MPI data type, e.g. (float -> MPI_FLOAT), (double -> MPI_DOUBLE), etc..
- * 
- * \returns MPI_Datatype 
- */
-inline MPI_Datatype MPITypeof(char) { return MPI_CHAR; }
-inline MPI_Datatype MPITypeof(signed short) { return MPI_SHORT; }
-inline MPI_Datatype MPITypeof(signed int) { return MPI_INT; }
-inline MPI_Datatype MPITypeof(signed long) { return MPI_LONG; }
-inline MPI_Datatype MPITypeof(signed long long) { return MPI_LONG_LONG_INT; }
-inline MPI_Datatype MPITypeof(float) { return MPI_FLOAT; }
-inline MPI_Datatype MPITypeof(double) { return MPI_DOUBLE; }
-inline MPI_Datatype MPITypeof(long double) { return MPI_LONG_DOUBLE; }
-inline MPI_Datatype MPITypeof(unsigned char) { return MPI_UNSIGNED_CHAR; }
-inline MPI_Datatype MPITypeof(unsigned short) { return MPI_UNSIGNED_SHORT; }
-inline MPI_Datatype MPITypeof(unsigned) { return MPI_UNSIGNED; }
-inline MPI_Datatype MPITypeof(unsigned long) { return MPI_UNSIGNED_LONG; }
+template <>
+constexpr MPI_Datatype MPIDatatype<signed short> = MPI_SHORT;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<int> = MPI_INT;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<signed long> = MPI_LONG;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<signed long long> = MPI_LONG_LONG_INT;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<float> = MPI_FLOAT;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<double> = MPI_DOUBLE;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<long double> = MPI_LONG_DOUBLE;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<unsigned char> = MPI_UNSIGNED_CHAR;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<unsigned short> = MPI_UNSIGNED_SHORT;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<unsigned int> = MPI_UNSIGNED;
+
+template <>
+constexpr MPI_Datatype MPIDatatype<unsigned long> = MPI_UNSIGNED_LONG;
 
 #endif
