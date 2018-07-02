2ab96c7c8a32fb5763b74a6b3976decab85582bc
ya.afshar <ya.afshar@gmail.com>
2018-07-02 16:46:33 -0400

    adding the new function for parsing command

--- src/misc/parser.hpp
+++ src/misc/parser.hpp
@@ -14,7 +14,7 @@ class parser
      * \brief Construct a new parser object
      * 
      */
-    parser(){}
+    parser() : lineArgNum(0) {}
 
     /*!
      * \brief Destroy the parser object
@@ -24,44 +24,13 @@ class parser
 
     /*!
      * \brief parse a line to seperate arguments
+     * 
      */
     void parse(std::string const &line_)
     {
         char *line = const_cast<char *>(&line_[0]);
 
-        //At the start of parsing each line set the argument number to zero
-        lineArgNum = 0;
-
-        /* if not the end of line ....... */
-        while (*line != '\0')
-        {
-            while (*line == ' ' || *line == '\t' || *line == '\n' || *line == ':' || *line == ',')
-            {
-                line++;
-            }
-
-            // Save the argument position
-            lineArg[lineArgNum++] = line;
-
-            // Skip the argument until ...
-            while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n' && *line != ':' && *line != ',')
-            {
-                line++;
-            }
-        }
-
-        // Mark the end of argument list
-        lineArg[lineArgNum] = nullptr;
-    }
-
-    /*!
-     * \brief parse a line to seperate arguments
-     */
-    void parse(const char *line_)
-    {
-        char *line = const_cast<char *>(line_);
-
-        //At the start of parsing each line set the argument number to zero
+        // At the start of parsing each line set the argument number to zero
         lineArgNum = 0;
 
         // if not the end of line .......
@@ -86,6 +55,69 @@ class parser
         lineArg[lineArgNum] = nullptr;
     }
 
+    /*!
+     * \brief parse a line to seperate arguments
+     * 
+     */
+    void parse(const char *line_)
+    {
+        char *line = const_cast<char *>(line_);
+
+        // At the start of parsing each line set the argument number to zero
+        lineArgNum = 0;
+
+        // if not the end of line .......
+        while (*line != '\0')
+        {
+            while (*line == ' ' || *line == '\t' || *line == '\n' || *line == ':' || *line == ',')
+            {
+                line++;
+            }
+
+            // Save the argument position
+            lineArg[lineArgNum++] = line;
+
+            // Skip the argument until ...
+            while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n' && *line != ':' && *line != ',')
+            {
+                line++;
+            }
+        }
+
+        // Mark the end of argument list
+        lineArg[lineArgNum] = nullptr;
+    }
+
+  private:
+    /*!
+     * \brief Parse a line to seperate arguments while replacing white spaces with 0
+     * 
+     */
+    void parse(char *line_, char **argv_)
+    {
+        // if not the end of line .......
+        while (*line_ != '\0')
+        {
+            while (*line_ == ' ' || *line_ == '\t' || *line_ == '\n' || *line_ == ':' || *line_ == ',')
+            {
+                *line_++ = '\0';
+            }
+
+            // Save the argument position
+            *argv_++ = line_;
+
+            // Skip the argument until ...
+            while (*line_ != '\0' && *line_ != ' ' && *line_ != '\t' && *line_ != '\n' && *line_ != ':' && *line_ != ',')
+            {
+                line_++;
+            }
+        }
+
+        // Mark the end of argument list
+        *argv_ = '\0';
+    }
+
+  public:
     /*!
      * \brief parse element 
      * 
@@ -160,21 +192,37 @@ class parser
 
     /*!
      * \brief Get the pointer to lineArg
+     * 
      */
     inline char **getLineArg()
     {
         return lineArg;
     }
 
-  private:
-    //! Array of pointers to each word in the parsed line
-    char *lineArg[LINESIZE];
+    /*!
+     * \brief Get the Line Arg object
+     * 
+     * \param argv         It should have fixed size (char *[])
+     * \param LineArgNum   Number of elements in the parsed line
+     */
+    inline void getLineArg(char **argv, std::size_t &LineArgNum)
+    {
+        LineArgNum = lineArgNum;
+        lineTmp = std::string(lineArg[0]);
+        parse(const_cast<char *>(lineTmp.c_str()), argv);
+    }
 
+  private:
     //! The number of last argument in the parsed line into different words
     std::size_t lineArgNum;
 
+    //! Array of pointers to each word in the parsed line
+    char *lineArg[LINESIZE];
+
     //! Word as an rvalue in parsing string
     std::string svalue;
+
+    std::string lineTmp;
 };
 
 //Template specialization for string input
