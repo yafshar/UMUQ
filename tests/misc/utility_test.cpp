67d56549802d790fea7916f4a7e3ba235f6af8f6
ya.afshar <ya.afshar@gmail.com>
2018-07-03 09:06:08 -0400

    complete the utility test

--- tests/misc/utility_test.cpp
+++ tests/misc/utility_test.cpp
@@ -4,22 +4,40 @@
 #include "misc/parser.hpp"
 #include "gtest/gtest.h"
 
-// Tests
-TEST(execute_cmd_test, HandlesZeroInput)
+// Test ExecuteCommand utility function
+TEST(utility_test, HandlesExecuteCommand)
 {
+    // Create an instance of parser
     parser p;
+    // Create an instance of utility object
     utility u;
+
     char *argv[LINESIZE];
-    p.parse("mkdir -p testUtility");
-    std::size_t i = 0;
-    p.getLineArg(argv, i);
-    EXPECT_TRUE(i == 3);
-    EXPECT_TRUE(u.execute_cmd(torc_node_id(), argv, nullptr));
+    std::size_t ArgNum;
+
+    // Create a new directory
+    p.parse("mkdir testUtility");
+    p.getLineArg(argv, ArgNum);
+
+    EXPECT_TRUE(ArgNum == 2);
+    EXPECT_TRUE(u.executeCommand(torc_node_id(), argv));
+
+    p.parse("test -d testUtility");
+    p.getLineArg(argv, ArgNum);
+
+    EXPECT_TRUE(ArgNum == 3);
+    EXPECT_TRUE(u.executeCommand(torc_node_id(), argv));
 
     p.parse("rm -fr testUtility");
-    p.getLineArg(argv, i);
-    EXPECT_TRUE(i == 3);
-    EXPECT_TRUE(u.execute_cmd(torc_node_id(), argv, nullptr));
+    p.getLineArg(argv, ArgNum);
+
+    EXPECT_TRUE(ArgNum == 3);
+    EXPECT_TRUE(u.executeCommand(torc_node_id(), argv));
+
+    EXPECT_TRUE(u.executeCommand("!(test -d testUtility)"));
+    EXPECT_TRUE(u.executeCommand("mkdir testUtility"));
+    EXPECT_TRUE(u.executeCommand("test -d testUtility"));
+    EXPECT_TRUE(u.executeCommand("rm -fr testUtility"));
 }
 
 int main(int argc, char **argv)
