a59a6bb189ef69e946a0603aacd0aadad6225936
ya.afshar <ya.afshar@gmail.com>
2018-07-06 17:41:12 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,12 @@
 #include <random>
 
 #include "inference/tmcmc/tmcmc.hpp"
-#include "inference/prior/densityfunction.hpp"
 
 int main(int argc, char **argv)
 {
 	torc_init(argc, argv, 0);
 
 	tmcmc<double> t;
-	uniformDistribution<double> u(1, 2);
-
-	std::cout << u.f(1.5) << " " << u.f(3) << std::endl;
 
 	torc_finalize();
 
