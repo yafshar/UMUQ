1e86fe333c88c673feefb5f2b1356e7a8cfaf6e0
ya.afshar <ya.afshar@gmail.com>
2018-07-06 17:33:32 -0400

    clean up the code and correct the class

--- src/numerics/residual.hpp
+++ src/numerics/residual.hpp
@@ -26,85 +26,36 @@ class residual
     /*!
      * \brief Construct a new residual object
      * 
-     * \param errorType_ Error type is a residual type (default AbsoluteError) 
+     * \param ierrorType Input error type is a residual type (default AbsoluteError) 
      */
-    residual(int const errorType_ = AbsoluteError) : errorType(errorType_) {}
+    residual(int const ierrorType = AbsoluteError);
 
     /*!
      * \brief Construct a new residual object
      * 
-     * \param errorType_ Error type is a residual type (default AbsoluteError) 
+     * \param ierrorType Input error type is a residual type (default AbsoluteError) 
      */
-    residual(std::string const &errorType_)
-    {
-        if (errorType_ == "absolute" || errorType_ == "Absolute" || errorType_ == "AbsoluteError")
-        {
-            errorType = AbsoluteError;
-        }
-        else if (errorType_ == "scaled" || errorType_ == "Scaled" || errorType_ == "ScaledError")
-        {
-            errorType = ScaledError;
-        }
-        else if (errorType_ == "squared" || errorType_ == "Squared" || errorType_ == "SquredError")
-        {
-            errorType = SquredError;
-        }
-        else
-        {
-			UMUQWARNING("Error type is unknown : Change to default absolute Error!");
-            errorType = AbsoluteError;
-        }
-    }
+    residual(std::string const &ierrorType);
 
     /*!
      * \brief set the new error type
      * 
-     * \param  errorType_  Error type in computing residual
+     * \param  ierrorType  Input error type in computing residual
      * 
      * \return true 
      * \return false  if the error type is unknown 
      */
-    bool set(std::string const &errorType_)
-    {
-        if (errorType_ == "absolute" || errorType_ == "Absolute" || errorType_ == "AbsoluteError")
-        {
-            errorType = AbsoluteError;
-        }
-        else if (errorType_ == "scaled" || errorType_ == "Scaled" || errorType_ == "ScaledError")
-        {
-            errorType = ScaledError;
-        }
-        else if (errorType_ == "squared" || errorType_ == "Squared" || errorType_ == "SquredError")
-        {
-            errorType = SquredError;
-        }
-        else
-        {
-			UMUQFAILRETURN("Error type is unknown!");
-        }
-        return true;
-    }
-    
+    bool set(std::string const &ierrorType);
+
     /*!
      * \brief set the new error type
      * 
-     * \param  errorType_  Error type in computing residual
+     * \param  ierrorType  Input error type in computing residual
      * 
      * \return true 
      * \return false  if the error type is unknown 
      */
-    bool set(int errorType_)
-    {
-        if (errorType_ == AbsoluteError || errorType_ == ScaledError || errorType_ == SquredError)
-        {
-            errorType = errorType_;
-        }
-        else
-        {
-			UMUQFAILRETURN("Error type is unknown!");
-        }
-        return true;
-    }
+    bool set(int ierrorType);
 
     /*!
      * \brief Compute the residual based on error type
@@ -114,24 +65,125 @@ class residual
      * 
      * \return Residual based on error type
      */
-    T operator()(T const &observed, T const &predicted)
-    {
-        switch (errorType)
-        {
-        case AbsoluteError:
-            return std::abs(observed - predicted);
-        case ScaledError:
-            return std::abs(observed - predicted) / std::abs(observed);
-        case SquredError:
-            return (observed - predicted) * (observed - predicted);
-        default:
-            return std::abs(observed - predicted);
-        }
-    }
+    T operator()(T const &observed, T const &predicted);
 
   private:
     //! Error type in computing residuals
     int errorType;
 };
+
+/*!
+ * \brief Construct a new residual object
+ * 
+ * \param ierrorType Input error type is a residual type (default AbsoluteError) 
+ */
+template <typename T>
+residual<T>::residual(int const ierrorType) : errorType(ierrorType) {}
+
+/*!
+ * \brief Construct a new residual object
+ * 
+ * \param ierrorType Input error type is a residual type (default AbsoluteError)
+ * 
+ */
+template <typename T>
+residual<T>::residual(std::string const &ierrorType)
+{
+    if (ierrorType == "absolute" || ierrorType == "Absolute" || ierrorType == "AbsoluteError")
+    {
+        this->errorType = AbsoluteError;
+    }
+    else if (ierrorType == "scaled" || ierrorType == "Scaled" || ierrorType == "ScaledError")
+    {
+        this->errorType = ScaledError;
+    }
+    else if (ierrorType == "squared" || ierrorType == "Squared" || ierrorType == "SquredError")
+    {
+        this->errorType = SquredError;
+    }
+    else
+    {
+        UMUQWARNING("Error type is unknown : Change to the default absolute Error!");
+        this->errorType = AbsoluteError;
+    }
+}
+
+/*!
+ * \brief set the new error type
+ * 
+ * \param  ierrorType  Input error type in computing residual
+ * 
+ * \return true
+ * \return false  if the error type is unknown
+ * 
+ */
+template <typename T>
+bool residual<T>::set(std::string const &ierrorType)
+{
+    if (ierrorType == "absolute" || ierrorType == "Absolute" || ierrorType == "AbsoluteError")
+    {
+        this->errorType = AbsoluteError;
+    }
+    else if (ierrorType == "scaled" || ierrorType == "Scaled" || ierrorType == "ScaledError")
+    {
+        this->errorType = ScaledError;
+    }
+    else if (ierrorType == "squared" || ierrorType == "Squared" || ierrorType == "SquredError")
+    {
+        this->errorType = SquredError;
+    }
+    else
+    {
+        UMUQFAILRETURN("Error type is unknown!");
+    }
+    return true;
+}
+
+/*!
+ * \brief set the new error type
+ * 
+ * \param  ierrorType  Input error type in computing residual
+ * 
+ * \return true 
+ * \return false  if the error type is unknown
+ * 
+ */
+template <typename T>
+bool residual<T>::set(int ierrorType)
+{
+    if (ierrorType == AbsoluteError || ierrorType == ScaledError || ierrorType == SquredError)
+    {
+        this->errorType = ierrorType;
+    }
+    else
+    {
+        UMUQFAILRETURN("Error type is unknown!");
+    }
+    return true;
+}
+
+/*!
+ * \brief Compute the residual based on error type
+ * 
+ * \param observed  Observed data
+ * \param predicted Predicted data
+ * 
+ * \return Residual based on error type
+ */
+template <typename T>
+T residual<T>::operator()(T const &observed, T const &predicted)
+{
+    switch (this->errorType)
+    {
+    case AbsoluteError:
+        return std::abs(observed - predicted);
+    case ScaledError:
+        return std::abs(observed - predicted) / std::abs(observed);
+    case SquredError:
+        return (observed - predicted) * (observed - predicted);
+    default:
+        return std::abs(observed - predicted);
+    }
+}
 
 #endif //UMUQ_RESIDUAL_H
