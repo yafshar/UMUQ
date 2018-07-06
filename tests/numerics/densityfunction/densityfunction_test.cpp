936b9047164f9777417548dca145ec936963b3a8
ya.afshar <ya.afshar@gmail.com>
2018-07-06 17:40:46 -0400

    minor

--- tests/numerics/densityfunction/densityfunction_test.cpp
+++ tests/numerics/densityfunction/densityfunction_test.cpp
@@ -19,21 +19,21 @@ pyplot plt;
  */
 TEST(densityFunction_test, HandlesConstruction)
 {
-	uniformDistribution<double> u(1, 2);
-	std::cout << "uniformDistribution=              " << std::cout.width(32) << u.f(1.5) << "  " << std::cout.width(32) << u.f(3) << std::endl;
+    uniformDistribution<double> u(1, 2);
+    std::cout << "uniformDistribution=              " << std::cout.width(32) << u.f(1.5) << "  " << std::cout.width(32) << u.f(3) << std::endl;
+    EXPECT_DOUBLE_EQ(u.f(3), double{});
 
-	exponentialDistribution<float> e(1);
-	std::cout << "exponentialDistribution=          " << std::cout.width(32) << e.f(1.5) << "  " << std::cout.width(32) << e.f(3) << std::endl;
+    exponentialDistribution<float> e(1);
+    std::cout << "exponentialDistribution=          " << std::cout.width(32) << e.f(1.5) << "  " << std::cout.width(32) << e.f(3) << std::endl;
 
-	gammaDistribution<double> g(0.5);
-	std::cout << "gammaDistribution=                " << std::cout.width(32) << g.f(1.5) << "  " << std::cout.width(20) << g.f(3) << std::endl;
+    gammaDistribution<double> g(0.5);
+    std::cout << "gammaDistribution=                " << std::cout.width(32) << g.f(1.5) << "  " << std::cout.width(20) << g.f(3) << std::endl;
 
-	gaussianDistribution<double> gu(2, 5);
-	std::cout << "gaussianDistribution=             " << std::cout.width(20) << gu.f(1.5) << "  " << std::cout.width(20) << gu.f(3) << std::endl;
+    gaussianDistribution<double> gu(2, 5);
+    std::cout << "gaussianDistribution=             " << std::cout.width(20) << gu.f(1.5) << "  " << std::cout.width(20) << gu.f(3) << std::endl;
 
-	multivariateGaussianDistribution<double> mvn(2);
-	std::cout << "multivariateGaussianDistribution= " << std::cout.width(20) << mvn.f(std::vector<double>{1.5, 2}.data()) << "  " << std::cout.width(20) << mvn.f(std::vector<double>{3, 2}.data()) << std::endl;
-
+    multivariateGaussianDistribution<double> mvn(2);
+    std::cout << "multivariateGaussianDistribution= " << std::cout.width(20) << mvn.f(std::vector<double>{1.5, 2}.data()) << "  " << std::cout.width(20) << mvn.f(std::vector<double>{3, 2}.data()) << std::endl;
 }
 
 int main(int argc, char **argv)
