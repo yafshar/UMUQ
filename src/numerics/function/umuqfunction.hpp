e392c2a84c95916342bbfa730808daaabe087873
ya.afshar <ya.afshar@gmail.com>
2018-08-15 10:45:07 -0400

    clean up and correct the implementation

--- src/numerics/function/umuqfunction.hpp
+++ src/numerics/function/umuqfunction.hpp
@@ -5,7 +5,7 @@
  * \brief umuqFunction is a general-purpose polymorphic function wrapper of n variables
  *
  * \tparam T  Data type
- * \tparam F  Function type 
+ * \tparam F  Function type (wrapped as std::function)
  */
 template <typename T, class F>
 class umuqFunction
@@ -44,6 +44,12 @@ class umuqFunction
     umuqFunction(std::vector<T> const &Params, char const *Name = "");
 
     /*!
+     * \brief Destroy the umuq Function object
+     * 
+     */
+    ~umuqFunction();
+
+    /*!
      * \brief Move constructor, Construct a new umuqFunction object
      * 
      * \param other umuqFunction object
@@ -55,6 +61,21 @@ class umuqFunction
      * 
      */
     umuqFunction<T, F> &operator=(umuqFunction<T, F> &&other);
+
+    /*!
+     * \brief Get the Name object
+     * 
+     * \return std::string const 
+     */
+    std::string const getName() const;
+
+    /*!
+     * \brief Checks whether *this stores a callable function target, i.e. is not empty. 
+     * 
+     * \return true   If it stores a callable function target at f
+     * \return false 
+     */
+    explicit operator bool() const noexcept;
 
   private:
     // Make it noncopyable
@@ -84,22 +105,36 @@ class umuqFunction
 
 template <typename T, class F>
 umuqFunction<T, F>::umuqFunction(char const *Name) : name(Name),
-                                                     numParams(0)
+                                                     numParams(0),
+                                                     f(nullptr)
 {
 }
 
 template <typename T, class F>
 umuqFunction<T, F>::umuqFunction(int const nDim, char const *Name) : name(Name),
-                                                                     numParams(nDim > 0 ? nDim : 0)
+                                                                     numParams(nDim > 0 ? nDim : 0),
+                                                                     f(nullptr)
 {
 }
 
 template <typename T, class F>
 umuqFunction<T, F>::umuqFunction(T const *Params, int const NumParams, char const *Name) : name(Name),
                                                                                            numParams(NumParams > 0 ? NumParams : 0),
-                                                                                           params(Params, Params + NumParams)
+                                                                                           params(Params, Params + NumParams),
+                                                                                           f(nullptr)
 {
 }
+
+template <typename T, class F>
+umuqFunction<T, F>::umuqFunction(std::vector<T> const &Params, char const *Name) : name(Name),
+                                                                                   numParams(Params.size()),
+                                                                                   params(Params),
+                                                                                   f(nullptr)
+{
+}
+
+template <typename T, class F>
+umuqFunction<T, F>::~umuqFunction() {}
 
 template <typename T, class F>
 umuqFunction<T, F>::umuqFunction(umuqFunction<T, F> &&other) : name(other.name),
@@ -112,12 +147,24 @@ umuqFunction<T, F>::umuqFunction(umuqFunction<T, F> &&other) : name(other.name),
 template <typename T, class F>
 umuqFunction<T, F> &umuqFunction<T, F>::operator=(umuqFunction<T, F> &&other)
 {
-    this->name = other.name;
-    this->numParams = other.numParams;
-    this->params = std::move(other.params);
-    this->f = std::move(other.f);
+    name = other.name;
+    numParams = other.numParams;
+    params = std::move(other.params);
+    f = std::move(other.f);
 
     return *this;
+}
+
+template <typename T, class F>
+std::string const umuqFunction<T, F>::getName() const
+{
+  return name;
+}
+
+template <typename T, class F>
+umuqFunction<T, F>::operator bool() const noexcept
+{
+  return f != nullptr;
 }
 
 /*!
