cca376b54f3a008694088c58362e42e7eb6c9402
ya.afshar <ya.afshar@gmail.com>
2018-08-18 18:33:48 -0400

    updating the test files

--- tests/numerics/multimin/conjugatefr_test.cpp
+++ tests/numerics/multimin/conjugatefr_test.cpp
@@ -1,5 +1,6 @@
 #include "core/core.hpp"
 #include "numerics/multimin/conjugatefr.hpp"
+#include "numerics/testfunctions/optimizationtestfunctions.hpp"
 #include "gtest/gtest.h"
 
 /*! 
@@ -7,8 +8,61 @@
  */
 TEST(conjugateFr_test, HandlesMinimizerConstruction)
 {
-    conjugateFr<double> cFr;
+    conjugateFr<double> fMinimizer;
 
+    //! First we have to set the minimizer dimension
+    EXPECT_TRUE(fMinimizer.reset(2));
+}
+
+/*!
+ * \brief Test if conjugateFr can handle a Rosenbrock function
+ * 
+ */
+TEST(conjugateFr_test, HandlesRosenbrockFunction)
+{
+    //! Starting point
+    std::vector<double> X = {-1.2, 1.0};
+
+    //! By default we consider stepSize as \f$ 0.1 ||x||_2 = 0.1 \sqrt {\sum x_i^2} \f$
+    double stepSize = 0.1 * std::sqrt(X[0] * X[0] + X[1] * X[1]);
+
+    //! Create an instance of the minimizer
+    conjugateFr<double> fMinimizer;
+
+    //! First we have to set the minimizer dimension
+    EXPECT_TRUE(fMinimizer.reset(2));
+
+    //! Second, we have to set the function, input vector, stepsize and tolerance
+    EXPECT_TRUE(fMinimizer.set(rosenbrock_f, rosenbrock_df, rosenbrock_fdf, X, stepSize, 0.1));
+
+    //! Third, initilize the minimizer
+    EXPECT_TRUE(fMinimizer.init());
+
+    // Forth, iterate until we reach the absolute tolerance of 1e-3
+
+    // Fail:-1, Success:0, Continue:1
+    int status = 1;
+    int iter = 0;
+    while (iter < 5000 && status == 1)
+    {
+        iter++;
+        EXPECT_TRUE(fMinimizer.iterate());
+        double *gradient = fMinimizer.getGradient();
+        status = fMinimizer.testGradient(gradient, 1e-3);
+    }
+
+    //! Check to see if we succeeded
+    EXPECT_TRUE(status == 0);
+
+    std::cout << fMinimizer.getName() << ", on Rosenbrock function : " << iter << " iters, f(x)=" << fMinimizer.getMin() << std::endl;
+    std::cout << "Converged to minimum at x=";
+
+    double *x = fMinimizer.getX();
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << x[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char **argv)
--- tests/numerics/multimin/simplexnm2rnd_test.cpp
+++ tests/numerics/multimin/simplexnm2rnd_test.cpp
@@ -1,5 +1,6 @@
 #include "core/core.hpp"
 #include "numerics/multimin/simplexnm2rnd.hpp"
+#include "numerics/testfunctions/optimizationtestfunctions.hpp"
 #include "gtest/gtest.h"
 
 /*! 
@@ -7,7 +8,60 @@
  */
 TEST(simplexNM2Rnd_test, HandlesMinimizerConstruction)
 {
-    simplexNM2Rnd<double> snmr;
+    simplexNM2Rnd<double> fMinimizer;
+
+    //! First we have to set the minimizer dimension
+    EXPECT_TRUE(fMinimizer.reset(2));
+}
+
+/*!
+ * \brief Test if simplexNM2Rnd can handle a Rosenbrock function
+ * 
+ */
+TEST(simplexNM2Rnd_test, HandlesRosenbrockFunction)
+{
+    //! Starting point
+    std::vector<double> X = {-1.2, 1.0};
+
+    //! By default we consider stepSize 1
+    std::vector<double> stepSize(2, 1);
+
+    //! Create an instance of the minimizer
+    simplexNM2Rnd<double> fMinimizer;
+
+    //! First we have to set the minimizer dimension
+    EXPECT_TRUE(fMinimizer.reset(2));
+
+    //! Second, we have to set the function, input vector and stepsize
+    EXPECT_TRUE(fMinimizer.set(rosenbrock_f, X, stepSize));
+
+    //! Third, initilize the minimizer
+    EXPECT_TRUE(fMinimizer.init());
+
+    // Forth, iterate until we reach the absolute tolerance of 1e-3
+
+    // Fail:-1, Success:0, Continue:1
+    int status = 1;
+    int iter = 0;
+    while (iter < 5000 && status == 1)
+    {
+        iter++;
+        EXPECT_TRUE(fMinimizer.iterate());
+        status = fMinimizer.testSize(1e-3);
+    }
+
+    //! Check to see if we succeeded
+    EXPECT_TRUE(status == 0);
+
+    std::cout << fMinimizer.getName() << ", on Rosenbrock function : " << iter << " iters, f(x)=" << fMinimizer.getMin() << std::endl;
+    std::cout << "Converged to minimum at x=";
+
+    double *x = fMinimizer.getX();
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << x[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char **argv)
