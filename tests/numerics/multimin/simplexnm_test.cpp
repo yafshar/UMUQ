78a0eded9c03f7efb01e4cacdd4843881ccecdf3
ya.afshar <ya.afshar@gmail.com>
2018-08-18 23:36:26 -0400

    update the test files for multimin minimization

--- tests/numerics/multimin/simplexnm_test.cpp
+++ tests/numerics/multimin/simplexnm_test.cpp
@@ -1,4 +1,5 @@
 #include "core/core.hpp"
+#include "numerics/function/functionminimizer.hpp"
 #include "numerics/multimin/simplexnm.hpp"
 #include "numerics/testfunctions/optimizationtestfunctions.hpp"
 #include "gtest/gtest.h"
@@ -55,6 +56,56 @@ TEST(simplexNM_test, HandlesRosenbrockFunction)
     EXPECT_TRUE(status == 0);
 
     std::cout << fMinimizer.getName() << ", on Rosenbrock function : " << iter << " iters, f(x)=" << fMinimizer.getMin() << std::endl;
+    std::cout << "Converged to minimum at x=";
+
+    double *x = fMinimizer.getX();
+    for (int i = 0; i < 2; i++)
+    {
+        std::cout << x[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+/*!
+ * \brief Test if simplexNM can handle a Roth function
+ * 
+ */
+TEST(simplexNM_test, HandlesRothFunction)
+{
+    //! Starting point
+    std::vector<double> X = {4.5, 3.5};
+
+    //! By default we consider stepSize 1
+    std::vector<double> stepSize(2, 1);
+
+    //! Create an instance of the minimizer
+    simplexNM<double> fMinimizer;
+
+    //! First we have to set the minimizer dimension
+    EXPECT_TRUE(fMinimizer.reset(2));
+
+    //! Second, we have to set the function, input vector, stepsize and tolerance
+    EXPECT_TRUE(fMinimizer.set(roth_f, X, stepSize));
+
+    //! Third, initilize the minimizer
+    EXPECT_TRUE(fMinimizer.init());
+
+    // Forth, iterate until we reach the absolute tolerance of 1e-3
+
+    // Fail:-1, Success:0, Continue:1
+    int status = 1;
+    int iter = 0;
+    while (iter < 5000 && status == 1)
+    {
+        iter++;
+        EXPECT_TRUE(fMinimizer.iterate());
+        status = fMinimizer.testSize(1e-3);
+    }
+
+    //! Check to see if we succeeded
+    EXPECT_TRUE(status == 0);
+
+    std::cout << fMinimizer.getName() << ", on Roth function : " << iter << " iters, f(x)=" << fMinimizer.getMin() << std::endl;
     std::cout << "Converged to minimum at x=";
 
     double *x = fMinimizer.getX();
