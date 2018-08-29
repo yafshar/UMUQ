b48eb9ecfe65da176633acfc3dfb25c5a302ca01
ya.afshar <ya.afshar@gmail.com>
2018-08-29 10:48:46 -0400

    minor

--- tests/inference/tmcmc/tmcmc_test.cpp
+++ tests/inference/tmcmc/tmcmc_test.cpp
@@ -6,13 +6,20 @@
 //! Tests tmcmc
 TEST(tmcmc_test, HandlesConstruction)
 {
-	tmcmc<double> t;
+    tmcmc<double> t;
 }
 
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::AddGlobalTestEnvironment(new torcEnvironment);
+
+    // Get the event listener list.
+    ::testing::TestEventListeners &listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    // Adds UMUQ listener; Google Test owns this pointer
+    listeners.Append(new UMUQEventListener);
 
     return RUN_ALL_TESTS();
 }
