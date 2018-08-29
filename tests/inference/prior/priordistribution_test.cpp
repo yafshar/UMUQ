a11581bbe06eaf5cb3f735322f88186fbe4370cf
ya.afshar <ya.afshar@gmail.com>
2018-08-29 10:48:31 -0400

    resolve the issue in calling google test assert

--- tests/inference/prior/priordistribution_test.cpp
+++ tests/inference/prior/priordistribution_test.cpp
@@ -6,12 +6,12 @@
 //! uniform
 double logpriorpdf(std::vector<double> const &lowerbound, std::vector<double> const &upperbound)
 {
-    double res{};
+    double sum{};
     for (std::size_t i = 0; i < upperbound.size(); i++)
     {
-        res += -std::log(upperbound[i] - lowerbound[i]);
+        sum += -std::log(upperbound[i] - lowerbound[i]);
     }
-    return res;
+    return sum;
 }
 
 //! Tests priorDistribution
@@ -22,25 +22,32 @@ TEST(priorDistribution_test, HandlesConstruction)
 
     EXPECT_EQ(prior.getpriorType(), 0);
 
-    std::cout << prior.getpriorType() << std::endl;
-
     // B0              0.05           10.0
     // B1              3.0             4.0
     // B2              6.01           15.0
     // B3              0.0001          1.0
     std::vector<double> lowerbound = {0.05, 3.0, 6.01, 0.0001};
     std::vector<double> upperbound = {10.0, 4.0, 15.0, 1.0000};
-    double x[4];
-
+    
+    //! Set the prior
     prior.set(lowerbound.data(), upperbound.data());
 
-    // EXPECT_DOUBLE_EQ(prior.logpdf(x), logpriorpdf(lowerbound, upperbound));
+    double x[] = {5, 3.5, 12., 0.001};
+
+    EXPECT_DOUBLE_EQ(prior.logpdf(x), logpriorpdf(lowerbound, upperbound));
 }
 
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::AddGlobalTestEnvironment(new torcEnvironment);
+
+    // Get the event listener list.
+    ::testing::TestEventListeners &listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    // Adds UMUQ listener; Google Test owns this pointer
+    listeners.Append(new UMUQEventListener);
 
     return RUN_ALL_TESTS();
 }
