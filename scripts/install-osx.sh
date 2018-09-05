123da6b9e8fa1b7e2c722a8c8483d2575062244d
ya.afshar <ya.afshar@gmail.com>
2018-09-05 16:03:25 -0400

    update the test environemnt

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -9,12 +9,12 @@ fi
 
 if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew update;
+	brew install gcc@5; 
+	brew link --overwrite gcc@5;
 	
 	brew outdated cmake || brew upgrade cmake ;
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
-
-	brew install gcc@5;
 	
 	brew install --build-from-source --cc=gcc-5 --cxx=g++-5 --fc=gfortran-5 mpich;
 	brew install --build-from-source --cc=gcc-5 --cxx=g++-5 flann;
