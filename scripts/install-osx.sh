17d331c32d640ef65be90bab5eb8b54a642e5b51
ya.afshar <ya.afshar@gmail.com>
2018-09-05 17:38:37 -0400

    update the test environemnt

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -8,21 +8,24 @@ if [ "${TRAVIS_OS_NAME}" != osx ]; then
 fi
 
 if [ "${TRAVIS_SUDO}" = "true" ]; then
+	brew update;
+
 	brew install gcc;
 	
 	brew outdated cmake || brew upgrade cmake ;
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
 	
-	export GCC_VERSION=`gfortran -dumpversion |cut -d. -f1`  
 	export HOMEBREW_CC=gcc-${GCC_VERSION}
 	export HOMEBREW_CXX=g++-${GCC_VERSION}
 	export HOMEBREW_CPP=cpp-${GCC_VERSION}
 	export HOMEBREW_LD=gcc-${GCC_VERSION}
 	export HOMEBREW_FC=gfortran-${GCC_VERSION}
-	
+
 	brew install mpich;
 	brew install flann;
+	
+	brew update;
 fi
 
 
