d9f6d20a45da90ce5893af9feec4162e7c50689e
ya.afshar <ya.afshar@gmail.com>
2018-09-05 17:19:16 -0400

    update the test environemnt

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -9,14 +9,16 @@ fi
 
 if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew update;
-	brew install gcc@5; 
+	
+	brew install gcc;
+	GCC_VERSION = `gfortran -dumpversion |cut -d. -f1`  
 	
 	brew outdated cmake || brew upgrade cmake ;
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
 	
-	brew install --build-from-source --cc=gcc-5 --cxx=g++-5 --fc=gfortran-5 mpich;
-	brew install --build-from-source --cc=gcc-5 --cxx=g++-5 flann;
+	brew install --build-from-source --cc=gcc-${GCC_VERSION} --cxx=g++-${GCC_VERSION} --fc=gfortran-${GCC_VERSION} mpich;
+	brew install --build-from-source --cc=gcc-${GCC_VERSION} --cxx=g++-${GCC_VERSION} flann;
 	
 	brew update;
 fi
