61ab1c2d9f5cb83f1e3a9fccbe70f945761c0976
ya.afshar <ya.afshar@gmail.com>
2018-09-09 12:32:53 -0400

    debug and complete the test case

--- tests/inference/prior/priordistribution_test.cpp
+++ tests/inference/prior/priordistribution_test.cpp
@@ -8,23 +8,23 @@
 umuq::psrandom<double> prng(123);
 
 //! uniform PDF
-double priorpdf(std::vector<double> const &lowerbound, std::vector<double> const &upperbound)
+double priorpdf(std::vector<double> const &param1, std::vector<double> const &param2)
 {
     double sum{1};
-    for (std::size_t i = 0; i < upperbound.size(); i++)
+    for (std::size_t i = 0; i < param2.size(); i++)
     {
-        sum *= 1. / (upperbound[i] - lowerbound[i]);
+        sum *= 1. / (param2[i] - param1[i]);
     }
     return sum;
 }
 
 //! uniform Log(PDF)
-double logpriorpdf(std::vector<double> const &lowerbound, std::vector<double> const &upperbound)
+double logpriorpdf(std::vector<double> const &param1, std::vector<double> const &param2)
 {
     double sum{};
-    for (std::size_t i = 0; i < upperbound.size(); i++)
+    for (std::size_t i = 0; i < param2.size(); i++)
     {
-        sum += -std::log(upperbound[i] - lowerbound[i]);
+        sum += -std::log(param2[i] - param1[i]);
     }
     return sum;
 }
@@ -53,6 +53,9 @@ TEST(priorDistribution_test, HandlesConstruction)
     EXPECT_DOUBLE_EQ(prior.pdf(x), priorpdf(lowerbound, upperbound));
     EXPECT_DOUBLE_EQ(prior.logpdf(x), logpriorpdf(lowerbound, upperbound));
 
+    // Initialize the PRNG or set the state of the PRNG
+    EXPECT_TRUE(prng.setState());
+
     //! Set the PRNG
     EXPECT_TRUE(prior.setRandomGenerator(&prng));
 
@@ -77,29 +80,32 @@ TEST(priorDistribution_test, HandlesCompositePriorConstruction)
     EXPECT_EQ(prior.getpriorType(), 4);
 
     //Composite  Prior type   Parameter1      Parameter2
-    // C0            0          0.05            10.0
-    // C1            1          0.0              1.0
-    std::vector<double> param1 = {0.05, 0.0};
-    std::vector<double> param2 = {10.0, 1.0};
-    std::vector<int> compositeprior = {0, 1};
+    // C0            1          0.0              1.0
+    // C1            0          0.05            10.0
+    std::vector<double> param1 = {0.0, 0.05};
+    std::vector<double> param2 = {1.0, 10.0};
+    //! For the first dimension it is a Gaussian distribution with \f$ \mu=0 \f$ and \f$ \sigma = 1 \f$
+    //! second dimension has theuniform distribution
+    std::vector<int> compositeprior = {1, 0};
 
     // //! Set the prior
-    // prior.set(param1, param2, compositeprior);
+    prior.set(param1, param2, compositeprior);
 
-    // double x[] = {5, 3.5};
+    double x[] = {3.5, 5.};
 
-    // EXPECT_DOUBLE_EQ(prior.pdf(x), priorpdf(std::vector<double>{param1[0]}, std::vector<double>{param2[0]}));
-    // EXPECT_DOUBLE_EQ(prior.logpdf(x), logpriorpdf(std::vector<double>{param1[0]}, std::vector<double>{param2[0]}));
+    EXPECT_DOUBLE_EQ(prior.pdf(x), 1 / M_S2PI * std::exp(-x[0] * x[0] / 2.) * priorpdf(std::vector<double>{param1[1]}, std::vector<double>{param2[1]}));
+    EXPECT_DOUBLE_EQ(prior.logpdf(x), -0.5 * M_L2PI - x[0] * x[0] / 2. + logpriorpdf(std::vector<double>{param1[1]}, std::vector<double>{param2[1]}));
 
-    // //! Set the PRNG
-    // EXPECT_TRUE(prior.setRandomGenerator(&prng));
+    // Initialize the PRNG or set the state of the PRNG
+    EXPECT_TRUE(prng.setState());
 
-    // //! Sampling from this prior distribution
-    // EXPECT_TRUE(prior.sample(x));
+    //! Set the PRNG
+    EXPECT_TRUE(prior.setRandomGenerator(&prng));
 
-    // EXPECT_TRUE(x[0] >= param1[0] && x[0] <= param2[0]);
+    //! Sampling from this prior distribution
+    EXPECT_TRUE(prior.sample(x));
 
-    // std::cout << x[0] << " " << x[1] << std::endl;
+    EXPECT_TRUE(x[1] >= param1[1] && x[1] <= param2[1]);
 }
 
 int main(int argc, char **argv)
