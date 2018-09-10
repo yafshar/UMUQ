22aabe4d37eb52fab27fdd337dfc02f48b1f41be
ya.afshar <ya.afshar@gmail.com>
2018-09-09 20:02:06 -0400

    update the TMCMC class

--- src/inference/tmcmc/tmcmc.hpp
+++ src/inference/tmcmc/tmcmc.hpp
@@ -8,6 +8,7 @@
 #include "numerics/stats.hpp"
 #include "inference/prior/priordistribution.hpp"
 #include "io/io.hpp"
+#include "misc/funcallcounter.hpp"
 
 namespace umuq
 {
@@ -19,8 +20,29 @@ namespace umuq
 namespace tmcmc
 {
 
+/*!
+ * \brief TORC environemnt object
+ * 
+ * \tparam T Data type
+ */
 template <typename T>
 std::unique_ptr<torcEnvironment<T>> torc;
+
+//! Create an instance of funcallcounter object
+funcallcounter fc;
+
+template <typename T, class F>
+void tmcmcInitTask(long long const TMCMCObj, T const *SamplePoints, int const *nSamplePoints, T *Fvalue, int const *nFvalue, int const *WorkInformation);
+
+template <typename T, class F>
+void tmcmcUpdateTask(long long const TMCMCObj, T const *SamplePoints, int const *nSamplePoints, T *Fvalue, int const *nFvalue, int const *WorkInformation);
+
+template <typename T, class F>
+using TMCMTASKTYPE = void (*)(long long const, T const *, int const *, T *, int const *, int const *);
+
+//! True if TMCMC Tasks have been registered, and false otherwise (logical).
+template <typename T, class F>
+static bool tmcmcTaskRegistered = false;
 
 /*! \class tmcmc
  * \brief 
@@ -106,12 +128,17 @@ public:
 
   bool iterate();
 
+  bool prepareNewGeneration(database<T> &leaders);
+
 private:
   inline bool iterate0();
 
 public:
   //! Input file name
   std::string inputFilename;
+
+  //! stream data for getting the problem size and variables from the input file
+  stdata<T> Data;
 
   //! Current data
   database<T> currentData;
@@ -129,9 +156,6 @@ public:
   fitFunction<T, F> fitfun;
 
 private:
-  //! stream data for getting the problem size and variables from the input file
-  stdata<T> Data;
-
   //! Prior distrdibution object
   priorDistribution<T> prior;
 
@@ -139,7 +163,16 @@ private:
   psrandom<T> prng;
 
   //! Sample points
-  std::vector<T> inputSamplePoints;
+  std::vector<T> samplePoints;
+
+  //! Function values
+  std::vector<T> Fvalue;
+
+  //! Array of the work inofmrtaion
+  int workInformation[3];
+
+  //! Muex object
+  std::mutex m;
 };
 
 template <typename T, class F>
@@ -173,20 +206,14 @@ inline bool tmcmc<T, F>::setInputFileName(char const *fileName)
 template <typename T, class F>
 bool tmcmc<T, F>::reset(char const *fileName)
 {
+  //! Check to see if the input file exists in the PATH or not?
   if (setInputFileName(fileName))
   {
-    // Read the input problem size and variables from an input file
+    //! Read the input problem size and variables from an input file
     if (Data.load(inputFilename))
     {
-      // Creating a database based on the read information
+      //! Creating a database based on the read information
       currentData = std::move(database<T>(Data.nDim, Data.populationSize));
-
-      { //! Compute the total size of data points
-        auto sum = std::accumulate(Data.eachPopulationSize.begin(), Data.eachPopulationSize.end(), 0);
-
-        // Creating a database based on the read information
-        fullData = std::move(database<T>(Data.nDim, sum));
-      }
 
       //! Creating the running inofrmation data
       runData = std::move(runinfo<T>(Data.nDim, Data.maxGenerations));
@@ -194,11 +221,13 @@ bool tmcmc<T, F>::reset(char const *fileName)
       //! Seed the PRNG
       if (!prng.setSeed(Data.seed))
       {
-        UMUQWARNING("The Psudo random number generator has been seeded & initilized before!")
+        UMUQWARNING("The Pseudo random number generator has been seeded & initilized before!")
       }
 
       //! Assign the correct size
-      inputSamplePoints.resize(Data.nDim);
+      samplePoints.resize(Data.nDim);
+
+      Fvalue.resize(1);
 
       //! Construct a prior Distribution object
       prior = std::move(priorDistribution<T>(Data.nDim, Data.priorType));
@@ -206,9 +235,9 @@ bool tmcmc<T, F>::reset(char const *fileName)
       //! Set the prior parameters
       return prior.set(Data.priorParam1, Data.priorParam2, Data.compositePriorDistribution);
     }
-    UMUQFAILRETURN("Failed to initilize the data from Input file!");
+    UMUQFAILRETURN("Failed to initilize the data from the Input file!");
   }
-  UMUQFAILRETURN("Requested File does not exist in the current PATH!!");
+  UMUQFAILRETURN("Input file for the input TMCMC parameter does not exist in the current PATH!!");
 }
 
 template <typename T, class F>
@@ -253,21 +282,39 @@ bool tmcmc<T, F>::init(char const *fileName)
   {
     if (setInputFileName(fileName))
     {
-      //! Initializa the fitting function
+      //! Initializa the fitting function, this should be done before initializaing the TORC environment
       fitfun.init();
 
       //! Create a torc environment object
       torc<T>.reset(new torcEnvironment<T>);
 
+      //! Register tasks
+      {
+        std::lock_guard<std::mutex> lock(m);
+
+        // Check if TMCMC tasks have been not been registered, do it
+        if (!tmcmcTaskRegistered<T, F>)
+        {
+          torc_register_task((void *)tmcmcInitTask<T, F>);
+          torc_register_task((void *)tmcmcUpdateTask<T, F>);
+          tmcmcTaskRegistered<T, F> = true;
+        }
+      }
+
       //! Set up the TORC environemnt
       torc<T>->SetUp();
 
-      //! Set the State of psudo random number generating object
-      return prng.setState();
+      //! Set the State of pseudo random number generating object
+      if (prng.setState())
+      {
+        //! Set the Random Number Generator object in prior
+        return prior.setRandomGenerator(&prng);
+      }
+      UMUQFAILRETURN("Failed to initialize the PRNG or set the state of that!");
     }
-    UMUQFAILRETURN("Input file does not exist!");
+    UMUQFAILRETURN("Input file for the input TMCMC parameter does not exist in the current PATH!!");
   }
-  UMUQFAILRETURN("Fitting function is not assigned! \n Fitting function must be set before calling this routine!");
+  UMUQFAILRETURN("Fitting function is not assigned! \n Fitting function must be set before initializing the TMCMC object!");
 }
 
 template <typename T, class F>
@@ -283,10 +330,143 @@ bool tmcmc<T, F>::iterate()
   //! If it is a fresh run, or the restart data is corrupted or is not available
   if (!iterate0())
   {
+    //! Generation number
+    workInformation[0] = runData.Generation;
+
+    //! Step number
+    workInformation[2] = 0;
+
+    int nFvalue = Fvalue.size();
+
     for (int i = 0; i < Data.eachPopulationSize[0]; i++)
     {
+      //! Sample number
+      workInformation[1] = i;
+
+      //! Create the input sample points from the prior distribution
+      prior.sample(samplePoints);
+
+      //! Create tasks
+      torc_create(-1, (void (*)())tmcmcInitTask<T, F>, 6,
+                  1, MPIDatatype<long long>, CALL_BY_REF,
+                  Data.nDim, MPIDatatype<T>, CALL_BY_COP,
+                  1, MPIDatatype<int>, CALL_BY_COP,
+                  1, MPIDatatype<T>, CALL_BY_COP,
+                  1, MPIDatatype<int>, CALL_BY_COP,
+                  3, MPIDatatype<int>, CALL_BY_COP,
+                  reinterpret_cast<long long>(this), samplePoints.data(),
+                  &Data.nDim, Fvalue.data(), &nFvalue, workInformation);
     }
+
+    torc_enable_stealing();
+    torc_waitall();
+    torc_disable_stealing();
+
+    fc.count();
+
+    std::cout << "server: Generation " << runData.Generation << ": total elapsed time = "
+              << "secs, generation elapsed time = "
+              << "secs for function calls = " << fc.getLocalFunctionCallsNumber() << std::endl;
+
+    //! Reset the local counter to zero
+    fc.reset();
+
+    if (Data.saveData)
+    {
+      if (!runData.save("curgen_db", runData.Generation))
+      {
+        UMUQFAILRETURN("Failed to write down the current data information!");
+      }
+    }
+
+    runData.save();
   }
+
+  //! Create memory for leader selection
+  database<T> leaders(Data.nDim, Data.eachPopulationSize[runData.Generation]);
+}
+
+/* process curgen_db -> calculate statitics */
+/* compute probs based on F values */
+/* draw new samples (nchains or user-specified) */
+/* find unique samples: fill the (new) leaders table */
+/* count how many times they appear -> nsteps */
+/* return the new sample size (number of chains) */
+template <typename T, class F>
+bool tmcmc<T, F>::prepareNewGeneration(database<T> &leaders)
+{
+  //! Create an instance of the statistics object
+  stats s;
+
+  //! Compute vectors of mean and standard deviation for each dimension
+  std::vector<T> mean(currentData.ndimParray);
+  std::vector<T> stddev(currentData.ndimParray);
+
+  int nSize = static_cast<int>(currentData.getSize()) * currentData.ndimParray;
+
+  for (int i = 0; i < currentData.ndimParray; i++)
+  {
+    mean[i] = s.mean<T, T>(currentData.Parray.get() + i, nSize, currentData.ndimParray);
+    stddev[i] = s.stddev<T, T>(currentData.Parray.get() + i, nSize, currentData.ndimParray, mean[i]);
+  }
+
+#ifdef DEBUG
+  io f;
+  std::cout << "Generation No = " << runData.Generation << std::endl;
+  f.printMatrix<T>("Means", mean.data(), 1, currentData.ndimParray);
+  f.printMatrix<T>("Stddev", stddev.data(), 1, currentData.ndimParray);
+#endif
+
+  //! Now we check to find unique points
+  
+
+}
+
+template <typename T, class F>
+void tmcmcInitTask(long long const TMCMCObj, T const *SamplePoints, int const *nSamplePoints, T *Fvalue, int const *nFvalue, int const *WorkInformation)
+{
+  auto tmcmcObj = reinterpret_cast<tmcmc<T, F> *>(TMCMCObj);
+
+  //! If we have set the fittiting function
+  if (tmcmcObj->fitfun)
+  {
+    int const nSamples = *nSamplePoints;
+    int const nFunvals = *nFvalue;
+
+    //! Create an array of work information
+    std::vector<int> workInformation{WorkInformation, WorkInformation + 3};
+
+    //! Create an array of sample points
+    std::vector<T> samplePoints{SamplePoints, SamplePoints + nSamples};
+
+    //! Increment function call counter
+    fc.increment();
+
+    //! Call the fitting function
+    Fvalue[0] = tmcmcObj->fitfun.f(samplePoints.data(), nSamples, Fvalue, nFunvals, workInformation.data());
+
+    // T *fv = Fvalue;
+    // fv += (nFunvals > 1 ? 1 : 0);
+
+    //! Update the data
+    tmcmcObj->currentData.update(samplePoints.data(), Fvalue[0]);
+
+    return;
+  }
+  UMUQFAIL("Fitting function is not assigned! \n Fitting function must be set before initializing the TMCMC object!");
+}
+
+template <typename T, class F>
+void tmcmcUpdateTask(long long const TMCMCObj, T const *SamplePoints, int const *nSamplePoints, T *Fvalue, int const *nFvalue, int const *WorkInformation)
+{
+  auto tmcmcObj = reinterpret_cast<tmcmc<T, F> *>(TMCMCObj);
+
+  //! If we have set the fittiting function
+  if (tmcmcObj->fitfun)
+  {
+    return;
+  }
+  UMUQFAIL("Fitting function is not assigned! \n Fitting function must be set before initializing the TMCMC object!");
 }
 
 } // namespace tmcmc
