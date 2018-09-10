651a6ca61997e4ef59fddff2c5ef85151bd16f25
ya.afshar <ya.afshar@gmail.com>
2018-09-10 19:39:42 -0400

    updating the name

--- tests/data/runinfo_test.cpp
+++ tests/data/runinfo_test.cpp
@@ -9,7 +9,7 @@ TEST(runinfo_test, HandlesConstruction)
 
     EXPECT_EQ(0, runinfoObj.nDim);
     EXPECT_EQ(0, runinfoObj.maxGenerations);
-    EXPECT_EQ(0, runinfoObj.Generation);
+    EXPECT_EQ(0, runinfoObj.currentGeneration);
 
     EXPECT_TRUE(runinfoObj.reset(2, 40));
 
@@ -26,8 +26,8 @@ TEST(runinfo_test, HandlesConstruction)
         {
             EXPECT_DOUBLE_EQ(0.0, runinfoObj.CoefVar[i]);
         }
-        EXPECT_DOUBLE_EQ(0.0, runinfoObj.p[i]);
-        EXPECT_EQ(0, runinfoObj.currentuniques[i]);
+        EXPECT_DOUBLE_EQ(0.0, runinfoObj.generationProbabilty[i]);
+        EXPECT_EQ(0, runinfoObj.currentUniques[i]);
         EXPECT_DOUBLE_EQ(0.0, runinfoObj.logselection[i]);
         EXPECT_DOUBLE_EQ(0.0, runinfoObj.acceptance[i]);
     }
@@ -46,13 +46,13 @@ TEST(runinfo_test, HandlesIO)
         EXPECT_EQ(2, runinfoObj.nDim);
         EXPECT_EQ(10, runinfoObj.maxGenerations);
 
-        runinfoObj.Generation = 9;
+        runinfoObj.currentGeneration = 9;
 
         for (int i = 0; i < runinfoObj.maxGenerations; i++)
         {
             runinfoObj.CoefVar[i] = static_cast<double>(i);
-            runinfoObj.p[i] = static_cast<double>(i * i);
-            runinfoObj.currentuniques[i] = i;
+            runinfoObj.generationProbabilty[i] = static_cast<double>(i * i);
+            runinfoObj.currentUniques[i] = i;
             runinfoObj.logselection[i] = static_cast<double>(i * i * i);
             runinfoObj.acceptance[i] = static_cast<double>(i) / 10.;
             runinfoObj.meantheta[i * runinfoObj.nDim] = static_cast<double>(i);
@@ -77,8 +77,8 @@ TEST(runinfo_test, HandlesIO)
         for (int i = 0; i < runinfoObj.maxGenerations; i++)
         {
             EXPECT_DOUBLE_EQ(static_cast<double>(i), runinfoObj.CoefVar[i]);
-            EXPECT_DOUBLE_EQ(static_cast<double>(i * i), runinfoObj.p[i]);
-            EXPECT_EQ(i, runinfoObj.currentuniques[i]);
+            EXPECT_DOUBLE_EQ(static_cast<double>(i * i), runinfoObj.generationProbabilty[i]);
+            EXPECT_EQ(i, runinfoObj.currentUniques[i]);
             EXPECT_DOUBLE_EQ(static_cast<double>(i * i * i), runinfoObj.logselection[i]);
             EXPECT_DOUBLE_EQ(static_cast<double>(i) / 10., runinfoObj.acceptance[i]);
             EXPECT_DOUBLE_EQ(static_cast<double>(i), runinfoObj.meantheta[i * runinfoObj.nDim]);
