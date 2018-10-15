1d04065fe6edf8b2e023f5a87e078a23525d69ea
ya.afshar <ya.afshar@gmail.com>
2018-10-15 12:03:16 -0400

    minor correction on variables naming to make them more expressive

--- tests/numerics/polynomial_test.cpp
+++ tests/numerics/polynomial_test.cpp
@@ -5,43 +5,44 @@
 /*!
  * \ingroup Test_Module
  * 
- * \brief Tests binomial coefficient c(n, k) of 0.
+ * \brief Tests binomial coefficient \f$ C(n, k) \f$ of 0.
  * 
  */
-TEST(binomial_coefficient_test, HandlesZeroInput)
+TEST(binomialCoefficient_test, HandlesZeroInput)
 {
     //Create an instance of a polynomial object
     umuq::polynomial<double> p(1);
 
-    EXPECT_EQ(1, p.binomial_coefficient(1, 0));
-    EXPECT_EQ(0, p.binomial_coefficient(0, 1));
-    EXPECT_EQ(1, p.binomial_coefficient(0, 0));
-    EXPECT_EQ(1, p.binomial_coefficient(10, 0));
+    EXPECT_EQ(1, p.binomialCoefficient(1, 0));
+    EXPECT_EQ(0, p.binomialCoefficient(0, 1));
+    EXPECT_EQ(1, p.binomialCoefficient(0, 0));
+    EXPECT_EQ(1, p.binomialCoefficient(10, 0));
 }
 
 /*!
  * \ingroup Test_Module
  * 
- * \brief Tests binomial coefficient c(n, k)
+ * \brief Tests binomial coefficient \f$ C(n, k) \f$
  * 
  */
-TEST(binomial_coefficient_test, HandlesOtherInput)
+TEST(binomialCoefficient_test, HandlesOtherInput)
 {
     //Create an instance of a polynomial object
     umuq::polynomial<double> p(1);
 
-    EXPECT_EQ(10, p.binomial_coefficient(10, 1));
-    EXPECT_EQ(1, p.binomial_coefficient(10, 10));
-    EXPECT_EQ(6, p.binomial_coefficient(4, 2));
-    EXPECT_EQ(10, p.binomial_coefficient(5, 2));
+    EXPECT_EQ(10, p.binomialCoefficient(10, 1));
+    EXPECT_EQ(1, p.binomialCoefficient(10, 10));
+    EXPECT_EQ(6, p.binomialCoefficient(4, 2));
+    EXPECT_EQ(10, p.binomialCoefficient(5, 2));
 }
 
 /*! 
  * \ingroup Test_Module
  * 
- * \brief Tests binomial coefficient c(n, k) of 0.
+ * \brief Tests binomial coefficient \f$ C(n, k) \f$ of 0.
  *
- * For example:
+ * For example:<br>
+ * \verbatim
  *  d = 2
  *  r = 2
  *
@@ -52,28 +53,30 @@ TEST(binomial_coefficient_test, HandlesOtherInput)
  *  alpha[ 8],[ 9] = 1, 1 = x^1 y^1
  *  alpha[10],[11] = 0, 2 = x^0 y^2
  *
- *  monomial_basis(2,2) = {1,    x,   y,  x^2, xy,  y^2}
- *                alpha = {0,0, 1,0, 0,1, 2,0, 1,1, 0,2}
+ *  monomialBasis(2,2) = {1,    x,   y,  x^2, xy,  y^2}
+ *               alpha = {0,0, 1,0, 0,1, 2,0, 1,1, 0,2}
  *
  *
- *  monomial_basis(3,2) = {1,       x,     y,     z,    x^2,  xy,    xz,   y^2,    yz,    z^2  }
- *                alpha = {0,0,0, 1,0,0, 0,1,0, 0,0,1, 2,0,0 1,1,0, 1,0,1, 0,2,0, 0,1,1, 0,0,2 }
+ *  monomialBasis(3,2) = {1,       x,     y,     z,    x^2,  xy,    xz,   y^2,    yz,    z^2  }
+ *               alpha = {0,0,0, 1,0,0, 0,1,0, 0,0,1, 2,0,0 1,1,0, 1,0,1, 0,2,0, 0,1,1, 0,0,2 }
+ * 
+ * \endverbatim
  */
-TEST(monomial_basis_test, HandlesInput)
+TEST(monomialBasis_test, HandlesInput)
 {
     int dim = 2;
     int degree = 2;
 
     umuq::polynomial<double> p(dim, degree);
 
-    int *coeff = p.monomial_basis();
+    int *coeff = p.monomialBasis();
 
     EXPECT_TRUE(coeff != nullptr);
 
-    int num = dim * p.binomial_coefficient(dim + degree, degree);
+    int num = dim * p.binomialCoefficient(dim + degree, degree);
 
     EXPECT_EQ(12, num);
-    EXPECT_EQ(p.monomialsize(), p.binomial_coefficient(dim + degree, degree));
+    EXPECT_EQ(p.monomialsize(), p.binomialCoefficient(dim + degree, degree));
 
     int alpha[12] = {0, 0,
                      1, 0,
@@ -92,13 +95,13 @@ TEST(monomial_basis_test, HandlesInput)
     //reset the polynomial to the new dimension
     p.reset(dim, degree);
 
-    coeff = p.monomial_basis();
+    coeff = p.monomialBasis();
     EXPECT_TRUE(coeff != nullptr);
 
-    num = dim * p.binomial_coefficient(dim + degree, degree);
+    num = dim * p.binomialCoefficient(dim + degree, degree);
 
     EXPECT_EQ(30, num);
-    EXPECT_EQ(p.monomialsize(), p.binomial_coefficient(dim + degree, degree));
+    EXPECT_EQ(p.monomialsize(), p.binomialCoefficient(dim + degree, degree));
 
     int beta[30] = {0, 0, 0,
                     1, 0, 0,
