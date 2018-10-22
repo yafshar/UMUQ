4c914fe6f0fdb6a628524dc31026fe6656ff5602
Yaser Afshar <ya.afshar@gmail.com>
2018-10-21 22:49:51 -0400

    completing the legendre polynomial test

--- tests/numerics/polynomials/legendrepolynomial_test.cpp
+++ tests/numerics/polynomials/legendrepolynomial_test.cpp
@@ -1,5 +1,5 @@
 #include "core/core.hpp"
-#include "numerics/polynomials/legendrepolynomial.hpp"
+#include "numerics/polynomials.hpp"
 #include "gtest/gtest.h"
 
 /*! 
@@ -7,35 +7,35 @@
  */
 TEST(LegendreBasis_test, HandlesInput)
 {
-    // Create an instance of a polynomial object
+    // Create an instance of a Legendre polynomial object
     umuq::LegendrePolynomial<double> LPolynomial(1);
 
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(0, 0.5), 1.);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(0, -0.5), 1.);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(1, 1.0e-8), 1.0e-08);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(1, 0.5), 0.5);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(1, -0.5), -0.5);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(2, 0.0), -0.5);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(2, 0.5), -0.125);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(2, 1.0), 1.0);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(3, -0.5), 0.4375);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(3, 0.5), -0.4375);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(3, 1.0), 1.0);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(10, -0.5), -0.18822860717773438);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(10, 1.0e-8), -0.24609374999999864648);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(10, 0.5), -0.18822860717773437500);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(10, 1.0), 1.0);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(99, -0.5), 0.08300778172138770477);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(99, 1.0e-8), -7.958923738716563193e-08);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(99, 0.5), -0.08300778172138770477);
-    EXPECT_NEAR(LPolynomial.legendre_p(99, 0.999), -0.3317727359254778874, 1e-14);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(99, 1.0), 1.0);
-    EXPECT_DOUBLE_EQ(LPolynomial.legendre_p(1000, 1.0), 1.0);
-    EXPECT_NEAR(LPolynomial.legendre_p(1000, -0.5), -0.019168251091650277878, 1e-15);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(0, 0.5), 1.);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(0, -0.5), 1.);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(1, 1.0e-8), 1.0e-08);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(1, 0.5), 0.5);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(1, -0.5), -0.5);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(2, 0.0), -0.5);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(2, 0.5), -0.125);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(2, 1.0), 1.0);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(3, -0.5), 0.4375);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(3, 0.5), -0.4375);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(3, 1.0), 1.0);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(10, -0.5), -0.18822860717773438);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(10, 1.0e-8), -0.24609374999999864648);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(10, 0.5), -0.18822860717773437500);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(10, 1.0), 1.0);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(99, -0.5), 0.08300778172138770477);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(99, 1.0e-8), -7.958923738716563193e-08);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(99, 0.5), -0.08300778172138770477);
+    EXPECT_NEAR(LPolynomial.legendre(99, 0.999), -0.3317727359254778874, 1e-14);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(99, 1.0), 1.0);
+    EXPECT_DOUBLE_EQ(LPolynomial.legendre(1000, 1.0), 1.0);
+    EXPECT_NEAR(LPolynomial.legendre(1000, -0.5), -0.019168251091650277878, 1e-15);
 
     double *PolynomialArray = nullptr;
 
-    PolynomialArray = LPolynomial.legendre_p_array(100, 0.5);
+    PolynomialArray = LPolynomial.legendre_array(100, 0.5);
 
     EXPECT_DOUBLE_EQ(PolynomialArray[0], 1.);
     EXPECT_DOUBLE_EQ(PolynomialArray[1], 0.5);
