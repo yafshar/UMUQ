45fa566118eec7a4726d7922985dc1bd7b5aca6b
ya.afshar <ya.afshar@gmail.com>
2018-11-01 18:04:28 -0400

    test

--- scripts/install-osx-gcc.sh
+++ scripts/install-osx-gcc.sh
@@ -16,22 +16,17 @@ if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew install gcc;
 	export GCC_VERSION=`gfortran -dumpversion |cut -d. -f1` 
 
-	export HOMEBREW_CC=gcc-${GCC_VERSION}
-	export HOMEBREW_CXX=g++-${GCC_VERSION}
-	export HOMEBREW_FC=gfortran-${GCC_VERSION}
-	export HOMEBREW_CPP=cpp-${GCC_VERSION}
-
 	(cd /usr/local && sudo chown -R $(whoami) bin etc include lib sbin share var opt Cellar Caskroom Frameworks)
     
-	brew reinstall gnu-sed; #--with-default-names;
-	brew reinstall grep; #--with-default-names;
+	brew reinstall gnu-sed --with-default-names;
+	brew reinstall grep --with-default-names;
 
 	brew outdated cmake || brew upgrade cmake ;
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
 	brew outdated wget || brew upgrade wget ;
 
-	brew install mpich --build-from-source
+	brew reinstall --cc=gcc-${GCC_VERSION} --build-from-source mpich
 	
 	# wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
 	# tar zxvf mpich-3.2.1.tar.gz
