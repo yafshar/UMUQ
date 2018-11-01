883281bee3e1eebd6a7ea384265a3715c49ec2a4
ya.afshar <ya.afshar@gmail.com>
2018-11-01 08:02:06 -0400

    update install osx script

--- scripts/install-osx-gcc.sh
+++ scripts/install-osx-gcc.sh
@@ -15,6 +15,11 @@ if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew install gcc;
 	export GCC_VERSION=`gfortran -dumpversion |cut -d. -f1` 
 
+	export HOMEBREW_CC=gcc-${GCC_VERSION}
+	export HOMEBREW_CXX=g++-${GCC_VERSION}
+	export HOMEBREW_FC=gfortran-${GCC_VERSION}
+	export HOMEBREW_CPP=cpp-${GCC_VERSION}
+
 	brew reinstall grep --with-default-names;
 	brew reinstall gnu-sed --with-default-names;
 
@@ -22,10 +27,12 @@ if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
 	brew outdated wget || brew upgrade wget ;
+
+	brew install mpich --build-from-source
 	
-	wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
-	tar zxvf mpich-3.2.1.tar.gz
-	(cd mpich-3.2.1 && ./configure CC=gcc-${GCC_VERSION} CXX=g++-${GCC_VERSION} FC=gfortran-${GCC_VERSION} --enable-threads=multiple > /dev/null && make -j 2 && sudo make install > /dev/null)
+	# wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
+	# tar zxvf mpich-3.2.1.tar.gz
+	# (cd mpich-3.2.1 && ./configure CC=gcc-${GCC_VERSION} CXX=g++-${GCC_VERSION} FC=gfortran-${GCC_VERSION} --enable-threads=multiple > /dev/null && make -j 2 && sudo make install > /dev/null)
 	
 	brew update;
 fi
