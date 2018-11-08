0cd754984eb578f93069c50d6cb96b1e8ec2b764
ya.afshar <ya.afshar@gmail.com>
2018-11-08 18:07:23 -0500

    Finalization of the travis scripts

--- scripts/install-osx.sh
+++ scripts/install-osx.sh
@@ -10,29 +10,30 @@ fi
 if [ "${TRAVIS_SUDO}" = "true" ]; then
 	brew update;
 
-	if [ "${TRAVIS_OSX_IMAGE}" = "xcode8" ]; then
-		# Create the keychain with a password
-    	KEY_CHAIN=ios-build.keychain
-		security create-keychain -p travis $KEY_CHAIN
-		# Make the custom keychain default, so xcodebuild will use it for signing
-		security default-keychain -s $KEY_CHAIN
-		# Unlock the keychain
-		security unlock-keychain -p travis $KEY_CHAIN
-    	# Set keychain locking timeout to 7200 seconds
-    	security set-keychain-settings -t 7200 -u $KEY_CHAIN	
-		
+	if [ "${TRAVIS_OSX_IMAGE}" = "xcode8" ] || [ "${TRAVIS_OSX_IMAGE}" = "xcode8.3" ] ; then
+		brew install gcc@6 > out 2>&1 &
+ 		brew_install_gcc_id=$!
+ 	
+		while kill -0 "$brew_install_gcc_id" >/dev/null 2>&1; do
+			sleep 300
+			tail ./out
+		done
+
+ 		echo "GCC installation is finished!"
+		rm -fr ./out
+
 		export GCC_VERSION=`gfortran-6 -dumpversion | cut -d. -f1`  
 	fi
 
 	brew reinstall grep --with-default-names;
 	brew reinstall gnu-sed --with-default-names;
-
+	
 	brew outdated cmake || brew upgrade cmake ;
 	brew outdated autoconf || brew upgrade autoconf ;
 	brew outdated automake || brew upgrade automake ;
 	brew outdated wget || brew upgrade wget ;
 	
-	if [ "${TRAVIS_OSX_IMAGE}" = "xcode8" ]; then
+	if [ "${TRAVIS_OSX_IMAGE}" = "xcode8" ] || [ "${TRAVIS_OSX_IMAGE}" = "xcode8.3" ] ; then
 		wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
 		tar zxvf mpich-3.2.1.tar.gz
 		(cd mpich-3.2.1 && ./configure CC=gcc-${GCC_VERSION} CXX=g++-${GCC_VERSION} FC=gfortran-${GCC_VERSION} --enable-threads=multiple > /dev/null && make -j 2 && sudo make install > /dev/null)
