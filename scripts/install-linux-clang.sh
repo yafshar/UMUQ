2f5c0e40107a1c107986f0583b58e614e9841e15
ya.afshar <ya.afshar@gmail.com>
2018-11-14 08:57:12 -0500

    finalizing travis test

--- scripts/install-linux-clang.sh
+++ scripts/install-linux-clang.sh
@@ -10,26 +10,10 @@ fi
 if [ "${TRAVIS_SUDO}" = "true" ]; then
 	sudo apt-get update 
 
-    if [ -e "/usr/bin/gcc" ]; then
-        sudo unlink /usr/bin/gcc;
-    fi
-    if [ -e "/usr/bin/g++" ]; then
-        sudo unlink /usr/bin/g++;
-    fi
-    if [ -e "/usr/bin/gfortran" ]; then
-        sudo unlink /usr/bin/gfortran;
-    fi
-
-	sudo ln -s /usr/bin/gcc-5 /usr/bin/gcc
-	sudo ln -s /usr/bin/g++-5 /usr/bin/g++
-	sudo ln -s /usr/bin/gfortran-5 /usr/bin/gfortran
-	sudo ln -s /usr/bin/clang-3.8 /usr/bin/clang
-	sudo ln -s /usr/bin/clang++-3.8 /usr/bin/clang++
-
 	wget http://www.mpich.org/static/downloads/3.2.1/mpich-3.2.1.tar.gz
 	tar zxvf mpich-3.2.1.tar.gz
 	cd mpich-3.2.1
-	./configure CC=clang-3.8 CXX=clang++3.8 FC=gfortran-5 --enable-threads=multiple > out 2>&1 & 
+	./configure CC=clang CXX=clang++ FC=gfortran-5 --enable-threads=multiple > out 2>&1 & 
 	config_install_mpich_id=$! 
 	while kill -0 "$config_install_mpich_id" > /dev/null 2>&1; do 
 		sleep 300
