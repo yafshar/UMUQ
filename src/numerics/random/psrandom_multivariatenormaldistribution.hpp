f46f8f22e6068cce82b81546dde33036d911b054
ya.afshar <ya.afshar@gmail.com>
2018-11-29 10:55:08 -0500

    Updatng the random classes with the new interface for better extension possibility and ease of use

--- src/numerics/random/psrandom_multivariatenormaldistribution.hpp
+++ src/numerics/random/psrandom_multivariatenormaldistribution.hpp
@@ -12,115 +12,145 @@ namespace randomdist
  * 
  * \brief Multivariate normal distribution
  * 
- * NOTE: 
- * - This should be called after setting the State of psrandom object \sa psrandom
+ * The [multivariate normal distribution](https://en.wikipedia.org/wiki/Multivariate_normal_distribution) of a 
+ * \f$n \text{-dimensional}\f$ random vector \f$ {\mathbf X} = \left[X_1, \cdots, X_n \right]^T \f$
+ * can be written in the following notation:<br>
+ * \f$ {\mathbf X} \sim \mathcal{N} (\mu, \Sigma) \f$<br>
+ * with \f$n \text{-dimensional}\f$ mean vector <br>
+ * \f$ \mu = {\mathbf E}[{\mathbf X}] = [{\mathbf E}[X_1], \cdots, {\mathbf E}[X_n]]^T, \f$
+ * and \f$ n \times n \f$ covariance matrix <br>
+ * \f$ \Sigma= {\mathbf E}[({\mathbf X}-\mu)({\mathbf X}-\mu)^T] = [\text{Covariance}[X_i, X_j]; 1\le i,j \le n] \f$ 
  * 
+ * Reference:<br>
+ * https://en.wikipedia.org/wiki/Multivariate_normal_distribution
  */
 template <typename RealType = double>
 class multivariateNormalDistribution
 {
  public:
    /*!
-   * \brief constructor
-   *
-   * \param inMean        Mean vector of size \f$n\f$
-   * \param inCovariance  Input Variance-covariance matrix of size \f$n \times n\f$
-   */
-   multivariateNormalDistribution(EVectorX<RealType> const &inMean, EMatrixX<RealType> const &inCovariance);
+    * \brief Construct a new multivariateNormalDistribution object
+    * 
+    * \param Mean        \f$ n \text{-dimensional}\f$ mean vector
+    * \param Covariance  \f$ n \times n \f$ covariance matrix
+    */
+   multivariateNormalDistribution(EVectorX<RealType> const &Mean, EMatrixX<RealType> const &Covariance);
 
    /*!
-   * \brief constructor
-   * 
-   * \param inMean        Input mean vector of size \f$n\f$
-   * \param inCovariance  Input variance-covariance matrix of size \f$n \times n\f$
-   * \param n             Vector size
-   */
-   multivariateNormalDistribution(RealType const *inMean, RealType const *inCovariance, int const n);
+    * \brief Construct a new multivariateNormalDistribution object
+    * 
+    * \param Mean        \f$ n \text{-dimensional}\f$ mean vector
+    * \param Covariance  \f$ n \times n \f$ covariance matrix
+    * \param n           Vector size (\f$n \text{-dimensional}\f$ vector)
+    */
+   multivariateNormalDistribution(RealType const *Mean, RealType const *Covariance, int const n);
 
    /*!
-   * \brief constructor (default mean = 0)
-   *
-   * \param inCovariance  Input variance-covariance matrix of size \f$n \times n\f$
-   */
-   explicit multivariateNormalDistribution(EMatrixX<RealType> const &inCovariance);
+    * \brief Construct a new multivariateNormalDistribution object
+    * 
+    * \param Covariance  \f$ n \times n \f$ covariance matrix
+    */
+   explicit multivariateNormalDistribution(EMatrixX<RealType> const &Covariance);
 
    /*!
-   * \brief constructor (default mean = 0)
-   * 
-   * \param inCovariance  Input variance-covariance matrix of size \f$n \times n\f$
-   * \param n             Vector size
-   */
-   multivariateNormalDistribution(RealType const *inCovariance, int const n);
+    * \brief Construct a new multivariateNormalDistribution object
+    * 
+    * \param Covariance  \f$ n \times n \f$ covariance matrix
+    * \param n           Vector size (\f$n \text{-dimensional}\f$ vector)
+    */
+   multivariateNormalDistribution(RealType const *Covariance, int const n);
 
    /*!
-   * \brief constructor (default mean = 0, covariance=I)
-   * 
-   * \param n vector size
-   */
+    * \brief Construct a new multivariateNormalDistribution object
+    * 
+    * \param n  Vector size (\f$n \text{-dimensional}\f$ vector)
+    */
    explicit multivariateNormalDistribution(int const n);
 
    /*!
-   * \brief Move constructor, construct a new multivariateNormalDistribution object from input multivariateNormalDistribution object
-   * 
-   * \param other  Input multivariateNormalDistribution object
-   */
+    * \brief Move constructor, construct a new multivariateNormalDistribution object 
+    * 
+    * \param other multivariateNormalDistribution object
+    */
    multivariateNormalDistribution(multivariateNormalDistribution<RealType> &&other);
 
    /*!
-   * \brief Move assignment operator
-   * 
-   * \param other  Input multivariateNormalDistribution object
-   * \return multivariateNormalDistribution& 
-   */
+    * \brief Move assignment operator
+    * 
+    * \param other  multivariateNormalDistribution object
+    * 
+    * \return multivariateNormalDistribution& 
+    */
    multivariateNormalDistribution<RealType> &operator=(multivariateNormalDistribution<RealType> &&other);
 
    /*!
-   * \brief Destroy the multivariatenormal Distribution object
-   * 
-   */
+    * \brief Destroy the multivariateNormalDistribution object
+    */
    ~multivariateNormalDistribution();
 
    /*!
-   * \returns a vector with multivariate normal distribution
-   */
-   EVectorX<RealType> operator()();
+    * \returns A vector of random numbers x according to a multivariate normal distribution \f$ \mathcal{N} (\mu, \Sigma) \f$
+    */
+   inline EVectorX<RealType> operator()();
 
    /*!
-   * \returns a vector with multivariate normal distribution
-   */
-   EVectorX<RealType> dist();
+    * \returns A vector of random numbers x according to a multivariate normal distribution \f$ \mathcal{N} (\mu, \Sigma) \f$
+    */
+   inline EVectorX<RealType> dist();
+
+   /*!
+    * \brief Fill the array of dataPoints with random samples from a multivariate normal distribution \f$ \mathcal{N} (\mu, \Sigma) \f$ 
+    * 
+    * \param dataPoints          Array of data points, where each point is a \f$n \text{-dimensional}\f$ 
+	 *                            point (dataPointDimension) and we have nDataPoints of them.
+	 *                            On return each data point is a random sample according to the multivariate 
+	 *                            normal distribution \f$ \mathcal{N} (\mu, \Sigma) \f$ 
+    * \param dataPointDimension  Data point dimension (\f$n \text{-dimensional}\f$ point)
+    * \param nDataPoints         Number of data points
+    */
+   inline void dist(RealType *dataPoints, int const dataPointDimension, int const nDataPoints);
+
+   /*!
+    * \brief Fill the eMatrix with random samples from a multivariate normal distribution \f$ \mathcal{N} (\mu, \Sigma) \f$ 
+    * 
+	 * \param eMatrix  Matrix of random numbers, where each column is an \f$n \text{-dimensional}\f$ point
+	 *                 and there are number of columns of the eMatrix points. <br>
+	 *                 On return each column is a random sample according to the multivariate normal 
+    *                 distribution \f$ \mathcal{N} (\mu, \Sigma) \f$
+    */
+   inline void dist(EMatrixX<RealType> &eMatrix);
 
  private:
-   // Make it noncopyable
+   /*! Make it noncopyable */
    multivariateNormalDistribution(multivariateNormalDistribution<RealType> const &) = delete;
 
-   // Make it not assignable
+   /*! Make it not assignable */
    multivariateNormalDistribution<RealType> &operator=(multivariateNormalDistribution<RealType> const &) = delete;
 
  public:
-   //! Vector of size \f$n\f$
+   /*! Vector of size \f$n\f$ */
    EVectorX<RealType> mean;
 
-   //! Variance-covariance matrix of size \f$ n \times n \f$
+   /*! Variance-covariance matrix of size \f$ n \times n \f$ */
    EMatrixX<RealType> covariance;
 
  private:
-   //! Matrix of size \f$n \times n\f$
+   /*! Matrix of size \f$n \times n\f$ */
    EMatrixX<RealType> transform;
 
  public:
-   //! LU decomposition of a matrix with complete pivoting
+   /*! LU decomposition of a matrix with complete pivoting */
    Eigen::FullPivLU<EMatrixX<RealType>> lu;
 
  private:
-   //! Generates random numbers according to the Normal (or Gaussian) random number distribution
+   /*! Generates random numbers according to the Normal (or Gaussian) random number distribution */
    std::normal_distribution<RealType> d;
 };
 
 template <typename RealType>
-multivariateNormalDistribution<RealType>::multivariateNormalDistribution(EVectorX<RealType> const &inMean, EMatrixX<RealType> const &inCovariance) : mean(inMean),
-                                                                                                                                                     covariance(inCovariance),
-                                                                                                                                                     lu(inCovariance)
+multivariateNormalDistribution<RealType>::multivariateNormalDistribution(EVectorX<RealType> const &Mean, EMatrixX<RealType> const &Covariance) : mean(Mean),
+                                                                                                                                                 covariance(Covariance),
+                                                                                                                                                 lu(Covariance)
 {
    if (!std::is_floating_point<RealType>::value)
    {
@@ -132,9 +162,9 @@ multivariateNormalDistribution<RealType>::multivariateNormalDistribution(EVector
 }
 
 template <typename RealType>
-multivariateNormalDistribution<RealType>::multivariateNormalDistribution(RealType const *inMean, RealType const *inCovariance, int const n) : mean(EMapTypeConst<RealType>(inMean, n, 1)),
-                                                                                                                                              covariance(EMapTypeConst<RealType>(inCovariance, n, n)),
-                                                                                                                                              lu(EMapTypeConst<RealType>(inCovariance, n, n))
+multivariateNormalDistribution<RealType>::multivariateNormalDistribution(RealType const *Mean, RealType const *Covariance, int const n) : mean(EMapTypeConst<RealType>(Mean, n, 1)),
+                                                                                                                                          covariance(EMapTypeConst<RealType>(Covariance, n, n)),
+                                                                                                                                          lu(EMapTypeConst<RealType>(Covariance, n, n))
 {
    if (!std::is_floating_point<RealType>::value)
    {
@@ -146,18 +176,12 @@ multivariateNormalDistribution<RealType>::multivariateNormalDistribution(RealTyp
 }
 
 template <typename RealType>
-multivariateNormalDistribution<RealType>::multivariateNormalDistribution(EMatrixX<RealType> const &inCovariance) : multivariateNormalDistribution(EVectorX<RealType>::Zero(inCovariance.rows()), inCovariance)
-{
-   if (!std::is_floating_point<RealType>::value)
-   {
-      UMUQFAIL("This type is not supported in this class!");
-   }
-}
+multivariateNormalDistribution<RealType>::multivariateNormalDistribution(EMatrixX<RealType> const &Covariance) : multivariateNormalDistribution(EVectorX<RealType>::Zero(Covariance.rows()), Covariance) {}
 
 template <typename RealType>
-multivariateNormalDistribution<RealType>::multivariateNormalDistribution(RealType const *inCovariance, int const n) : mean(EVectorX<RealType>::Zero(n)),
-                                                                                                                      covariance(EMapTypeConst<RealType>(inCovariance, n, n)),
-                                                                                                                      lu(EMapTypeConst<RealType>(inCovariance, n, n))
+multivariateNormalDistribution<RealType>::multivariateNormalDistribution(RealType const *Covariance, int const n) : mean(EVectorX<RealType>::Zero(n)),
+                                                                                                                    covariance(EMapTypeConst<RealType>(Covariance, n, n)),
+                                                                                                                    lu(EMapTypeConst<RealType>(Covariance, n, n))
 {
    if (!std::is_floating_point<RealType>::value)
    {
@@ -202,17 +226,38 @@ template <typename RealType>
 multivariateNormalDistribution<RealType>::~multivariateNormalDistribution() {}
 
 template <typename RealType>
-EVectorX<RealType> multivariateNormalDistribution<RealType>::operator()()
+inline EVectorX<RealType> multivariateNormalDistribution<RealType>::operator()()
 {
    int const me = PRNG_initialized ? torc_i_worker_id() : 0;
    return mean + transform * EVectorX<RealType>{mean.size()}.unaryExpr([&](RealType const x) { return d(NumberGenerator[me]); });
 }
 
 template <typename RealType>
-EVectorX<RealType> multivariateNormalDistribution<RealType>::dist()
+inline EVectorX<RealType> multivariateNormalDistribution<RealType>::dist()
 {
    int const me = PRNG_initialized ? torc_i_worker_id() : 0;
    return mean + transform * EVectorX<RealType>{mean.size()}.unaryExpr([&](RealType const x) { return d(NumberGenerator[me]); });
+}
+
+template <typename RealType>
+inline void multivariateNormalDistribution<RealType>::dist(RealType *dataPoints, int const dataPointDimension, int const nDataPoints)
+{
+   int const me = PRNG_initialized ? torc_i_worker_id() : 0;
+   EMapType<RealType, Eigen::ColMajor> eMatrix(dataPoints, dataPointDimension, nDataPoints);
+   for (auto i = 0; i < nDataPoints; ++i)
+   {
+      eMatrix.col(i) = mean + transform * EVectorX<RealType>{mean.size()}.unaryExpr([&](RealType const x) { return d(NumberGenerator[me]); });
+   }
+}
+
+template <typename RealType>
+inline void multivariateNormalDistribution<RealType>::dist(EMatrixX<RealType> &eMatrix)
+{
+   int const me = PRNG_initialized ? torc_i_worker_id() : 0;
+   for (auto i = 0; i < eMatrix.cols(); ++i)
+   {
+      eMatrix.col(i) = mean + transform * EVectorX<RealType>{mean.size()}.unaryExpr([&](RealType const x) { return d(NumberGenerator[me]); });
+   }
 }
 
 } // namespace randomdist
