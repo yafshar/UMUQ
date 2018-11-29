f8a0a3061cc9377023550ec464ec57bbb51955d1
ya.afshar <ya.afshar@gmail.com>
2018-11-29 11:01:14 -0500

    Restrucring the test cases and putting the test files in the right location

---
+++ tests/inference/tmcmc/datatype_test.cpp
@@ -0,0 +1,24 @@
+#include "core/core.hpp"
+#include "environment.hpp"
+#include "inference/tmcmc/datatype.hpp"
+#include "gtest/gtest.h"
+
+// Tests datatype which is using database object
+TEST(datatype_test, HandlesGlobalData)
+{
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::AddGlobalTestEnvironment(new umuq::torcEnvironment);
+
+    // Get the event listener list.
+    ::testing::TestEventListeners &listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    // Adds UMUQ listener; Google Test owns this pointer
+    listeners.Append(new umuq::UMUQEventListener);
+
+    return RUN_ALL_TESTS();
+}
