984be2248d00edc8dd2f59adebfaa6266cfd5775
ya.afshar <ya.afshar@gmail.com>
2018-12-06 17:44:16 -0500

    adding the global timer for debugging purposes

--- src/global.hpp
+++ src/global.hpp
@@ -1,13 +1,25 @@
 #ifndef UMUQ_GLOBAL_H
 #define UMUQ_GLOBAL_H
 
+#include "misc/timer.hpp"
+
 namespace umuq
 {
- /*! \class global 
+/*! \class global 
  * \brief Encapsulates global objects and functions
  * 
- */   
+ */
 
-} // umuq
+/*!
+ * \brief Global timer 
+ * 
+ * \returns umuqTimer 
+ */
+#ifdef DEBUG
+extern umuqTimer gTimer;
+#endif
+umuqTimer gTimer(false);
+
+} // namespace umuq
 
 #endif // UMUQ_GLOBAL
