d71c3660f971dd6d4108c157b5bf3200899be3b5
ya.afshar <ya.afshar@gmail.com>
2018-12-07 10:54:46 -0500

    adding new global variables for plotting and pseudo random number generator

--- src/global.hpp
+++ src/global.hpp
@@ -2,23 +2,46 @@
 #define UMUQ_GLOBAL_H
 
 #include "misc/timer.hpp"
+#include "io/pyplot.hpp"
+#include "numerics/eigenlib.hpp"
+#include "numerics/random/psrandom.hpp"
 
 namespace umuq
 {
-/*! \class global 
- * \brief Encapsulates global objects and functions
- * 
+
+/*! 
+ * \defgroup Global_Module Global module
+ * This is the global module of %UMUQ encapsulates global objects necessary in many classes of %UMUQ.
  */
 
 /*!
+ * \ingroup Global_Module 
+ * 
  * \brief Global timer 
  * 
  * \returns umuqTimer 
  */
 #ifdef DEBUG
-extern umuqTimer gTimer;
+extern umuq::umuqTimer gTimer;
 #endif
-umuqTimer gTimer(false);
+umuq::umuqTimer gTimer(false);
+
+/*!
+ * \ingroup Global_Module 
+ * 
+ * \brief Create a global instance of the Pyplot from Pyplot library
+ * 
+ */
+extern umuq::matplotlib_223::pyplot plt;
+umuq::matplotlib_223::pyplot plt;
+
+/*!
+ * \ingroup Global_Module
+ * 
+ * \brief Get an instance of a seeded pseudo random object
+ */
+extern umuq::psrandom prng;
+umuq::psrandom prng(12345678);
 
 } // namespace umuq
 
