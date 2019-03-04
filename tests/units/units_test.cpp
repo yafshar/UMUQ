14259859c042cd8368274d8a8e6e7e92961aa733
Yaser Afshar <ya.afshar@gmail.com>
2019-03-04 14:59:36 -0500

    minor update

---
+++ tests/units/units_test.cpp
@@ -0,0 +1,59 @@
+#include "core/core.hpp"
+#include "units/units.hpp"
+#include "gtest/gtest.h"
+
+/*!
+ * \ingroup Test_Module
+ *  
+ * \brief Tests units class 
+ *
+ */
+TEST(units_test, HandlesConstruction)
+{
+	// Create an instance of the units class
+	umuq::units u("electron");
+
+	EXPECT_EQ(u.getUnitStyle(), umuq::UnitStyle::ELECTRON);
+	EXPECT_EQ(u.getUnitStyleName(), "ELECTRON");
+
+	// Set the convert style
+	EXPECT_TRUE(u.convertToStyle("metal"));
+	
+	double l = 1.0;
+	u.convertLength(l);
+
+	// 1 Bohr = 0.529177208 Angstrom
+	EXPECT_EQ(l, 0.529177208);
+
+	// 1 Hartree = 27.211396 eV 
+	std::vector<double> EnergyVector{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	u.convertEnergy(EnergyVector);
+	int n = 1;
+	for (auto i : EnergyVector)
+	{
+		EXPECT_EQ(i, n * 27.211396);
+		n++;
+	}
+
+	EXPECT_EQ(umuq::getUnitStyleName(umuq::UnitStyle::METAL), "METAL");
+
+	std::vector<double > LengthVector{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	EXPECT_TRUE(umuq::convert<umuq::LengthUnit>(LengthVector, umuq::UnitStyle::ELECTRON, umuq::UnitStyle::METAL));
+	
+	EXPECT_FALSE(umuq::convert<umuq::UnitStyle>(LengthVector, umuq::UnitStyle::ELECTRON, umuq::UnitStyle::METAL));
+	
+	n = 1;
+	for (auto i : LengthVector)
+	{
+		EXPECT_EQ(i, n * 0.529177208);
+		n++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
