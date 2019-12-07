76f453e34e64b0d1bf542f18cbcf3ca6e94a8c61
Yaser Afshar <ya.afshar@gmail.com>
2019-12-07 15:13:21 -0600

    updating the header inclusion with the new environment update

--- src/numerics/polynomials/polynomialbase.hpp
+++ src/numerics/polynomials/polynomialbase.hpp
@@ -51,19 +51,20 @@ inline namespace polynomials
  * If we replace the monomials in the Vandermonde matrix by different polynomials, the resulting matrix is a
  * Vandermonde-like matrix.
  */
+
 template <typename DataType>
 class polynomialBase
 {
   public:
-	/*!
-	 * \brief Construct a new polynomialBase object
-	 *
+    /*!
+     * \brief Construct a new polynomialBase object
+     *
      * \param dim              Dimension
      * \param PolynomialOrder  Polynomial order (the default order or degree of r in a space of dim dimensions is 2)
-	 */
-	polynomialBase(int const dim, int const PolynomialOrder = 2);
+     */
+    polynomialBase(int const dim, int const PolynomialOrder = 2);
 
-	/*!
+    /*!
      * \brief Reset the dimension of the problem and rhe desired order
      *
      * Reset the dimension and order values to the new ones
@@ -71,20 +72,20 @@ class polynomialBase
      * \param dim              New Dimension
      * \param PolynomialOrder  New Order (the default order or degree of \b r in a space of \b dim dimensions is 2)
      */
-	void reset(int const dim, int const PolynomialOrder = 2);
+    void reset(int const dim, int const PolynomialOrder = 2);
 
-	/*!
+    /*!
      * \brief Computes the [binomial coefficient](https://en.wikipedia.org/wiki/Binomial_coefficient)
-	 * \f$ C(n, k) \f$.
+     * \f$ C(n, k) \f$.
      *
      * -# A binomial coefficient \f$ C(n, k) \f$ can be defined as the coefficient of \f$ x ^ k \f$ in
-	 * the expansion of \f$ (1 + x) ^ n. \f$ <br>
+     * the expansion of \f$ (1 + x) ^ n. \f$ <br>
      * -# A binomial coefficient \f$ C(n, k) \f$ also gives the number of ways, disregarding order,
-	 * that k objects can be chosen from among n objects; <br>
+     * that k objects can be chosen from among n objects; <br>
      * More formally, the number of k-element subsets (or k-combinations) of an n-element set.
      *
      * The formula used is:
-	 *
+     *
      * \f$ C(n, k) = \frac{n!}{ n! (n-k)! } \f$
      *
      * \param n  Input parameter
@@ -92,26 +93,16 @@ class polynomialBase
      *
      * \returns The binomial coefficient \f$ C(n, k) \f$
      */
-	int binomialCoefficient(int const n, int const k);
+    int binomialCoefficient(int const n, int const k);
 
-	/*!
+    /*!
      * \brief Here, \f$\alpha=\f$ all the monomials in a \b d dimensional space, with total degree \b r.
      *
      * \returns A pointer to monomial sequence
      */
-	virtual int *monomialBasis();
+    virtual int *monomialBasis();
 
-	/*!
-     * \brief Evaluates a monomial at a point x.
-     *
-     * \param  x      The abscissa values. (The coordinates of the evaluation points)
-     * \param  value  The (monomial value) array value of the monomial at point x
-	 *
-     * \returns The size of the monomial array
-     */
-	virtual int monomialValue(DataType const *x, DataType *&value);
-
-	/*!
+    /*!
      * \brief Evaluates a monomial at a point x.
      *
      * \param  x      The abscissa values. (The coordinates of the evaluation points)
@@ -119,288 +110,298 @@ class polynomialBase
      *
      * \returns The size of the monomial array
      */
-	virtual int monomialValue(DataType const *x, std::vector<DataType> &value);
+    virtual int monomialValue(DataType const *x, DataType *&value);
 
-	/*!
-	 * \brief Evaluates monomial derivatives at origin.
-	 *
-	 * \param beta   In multi-dimensional notation \f$ \beta=\left(\beta_1, \cdots, \beta_d \right). \f$<br>
-	 *               Notation for partial derivatives:<br>
-	 *               \f$  D^\beta = \frac{\partial^{|\beta|}}{\partial x_1^{\beta_1} \partial x_2^{\beta_2}\cdots\partial x_d^{\beta_d}}. \f$
-	 * \param value  The (monomial derivative value) array value of the monomial derivatives at zero point
-	 *
-	 * \returns int The size of the monomial array
-	 */
-	virtual int monomialDerivative(int const *beta, DataType *&value);
+    /*!
+     * \brief Evaluates a monomial at a point x.
+     *
+     * \param  x      The abscissa values. (The coordinates of the evaluation points)
+     * \param  value  The (monomial value) array value of the monomial at point x
+     *
+     * \returns The size of the monomial array
+     */
+    virtual int monomialValue(DataType const *x, std::vector<DataType> &value);
 
-	/*!
-	 * \brief Evaluates monomial derivatives at origin.
-	 *
-	 * \param beta   In multi-dimensional notation \f$ \beta=\left(\beta_1, \cdots, \beta_d \right). \f$<br>
-	 *               Notation for partial derivatives:<br>
-	 *               \f$  D^\beta = \frac{\partial^{|\beta|}}{\partial x_1^{\beta_1} \partial x_2^{\beta_2}\cdots\partial x_d^{\beta_d}}. \f$
-	 * \param value  The (monomial derivative value) array value of the monomial derivatives at zero point
-	 *
-	 * \returns int The size of the monomial array
-	 */
-	virtual int monomialDerivative(int const *beta, std::vector<DataType> &value);
+    /*!
+     * \brief Evaluates monomial derivatives at origin.
+     *
+     * \param beta   In multi-dimensional notation \f$ \beta=\left(\beta_1, \cdots, \beta_d \right). \f$<br>
+     *               Notation for partial derivatives:<br>
+     *               \f$  D^\beta = \frac{\partial^{|\beta|}}{\partial x_1^{\beta_1} \partial x_2^{\beta_2}\cdots\partial x_d^{\beta_d}}. \f$
+     * \param value  The (monomial derivative value) array value of the monomial derivatives at zero point
+     *
+     * \returns int The size of the monomial array
+     */
+    virtual int monomialDerivative(int const *beta, DataType *&value);
 
-	/*!
+    /*!
+     * \brief Evaluates monomial derivatives at origin.
+     *
+     * \param beta   In multi-dimensional notation \f$ \beta=\left(\beta_1, \cdots, \beta_d \right). \f$<br>
+     *               Notation for partial derivatives:<br>
+     *               \f$  D^\beta = \frac{\partial^{|\beta|}}{\partial x_1^{\beta_1} \partial x_2^{\beta_2}\cdots\partial x_d^{\beta_d}}. \f$
+     * \param value  The (monomial derivative value) array value of the monomial derivatives at zero point
+     *
+     * \returns int The size of the monomial array
+     */
+    virtual int monomialDerivative(int const *beta, std::vector<DataType> &value);
+
+    /*!
      * \brief Get the monomial size
      *
      * \return Monomial size
      */
-	inline int monomialsize() const;
+    inline int monomialsize() const;
 
-	/*!
+    /*!
      * \brief Get the dimension
      *
      * \return Dimension
      */
-	inline int dim() const;
+    inline int dim() const;
 
-	/*!
+    /*!
      * \brief Polynomial order
      *
      * \return Polynomial order
      */
-	inline int order() const;
+    inline int order() const;
 
   protected:
-	/*!
+    /*!
      * \brief Use a reverse lexicographic order for next monomial, degrees between 0 and \b r
      *  all monomials in a \b d dimensional space, with order of accuracy \b r.
      *
      * \param x  Current monomial on input and next monomial on the output (last value in the sequence is r).
      */
-	bool graded_reverse_lexicographic_order(int *x);
+    bool graded_reverse_lexicographic_order(int *x);
 
   protected:
-	/*!
+    /*!
      * \brief Delete a polynomialBase object copy construction
      *
      * Avoiding implicit generation of the copy constructor.
      */
-	polynomialBase(polynomialBase<DataType> const &) = delete;
+    polynomialBase(polynomialBase<DataType> const &) = delete;
 
-	/*!
+    /*!
      * \brief Delete a polynomialBase object assignment
      *
      * Avoiding implicit copy assignment.
      */
-	polynomialBase<DataType> &operator=(polynomialBase<DataType> const &) = delete;
+    polynomialBase<DataType> &operator=(polynomialBase<DataType> const &) = delete;
 
   protected:
-	//! Dimension
-	int nDim;
+    //! Dimension
+    int nDim;
 
-	//! Order of accuracy
-	int Order;
+    //! Order of accuracy
+    int Order;
 
-	//! The size of the monomial array
-	int monomialSize;
+    //! The size of the monomial array
+    int monomialSize;
 
-	//! Array of monomial sequence
-	std::unique_ptr<int[]> alpha;
+    //! Array of monomial sequence
+    std::unique_ptr<int[]> alpha;
 };
 
 template <typename DataType>
 polynomialBase<DataType>::polynomialBase(int const dim, int const PolynomialOrder) : nDim(dim), Order(PolynomialOrder)
 {
-	if (nDim <= 0)
-	{
-		UMUQFAIL("Can not have dimension ", nDim, " <= 0!");
-	}
+    if (nDim <= 0)
+    {
+        UMUQFAIL("Can not have dimension ", nDim, " <= 0!");
+    }
 
-	if (Order < 0)
-	{
-		UMUQFAIL("Maximum accuracy order ", Order, " < 0!");
-	}
+    if (Order < 0)
+    {
+        UMUQFAIL("Maximum accuracy order ", Order, " < 0!");
+    }
 
-	monomialSize = binomialCoefficient(nDim + Order, Order);
-	if (monomialSize == 0)
-	{
-		UMUQFAIL("Monomial size of zero degree is requested!");
-	}
+    monomialSize = binomialCoefficient(nDim + Order, Order);
+    if (monomialSize == 0)
+    {
+        UMUQFAIL("Monomial size of zero degree is requested!");
+    }
 }
 
 template <typename DataType>
 void polynomialBase<DataType>::reset(int const dim, int const PolynomialOrder)
 {
-	nDim = dim;
-	if (nDim <= 0)
-	{
-		UMUQFAIL("Can not have dimension ", nDim, " <= 0!");
-	}
+    nDim = dim;
+    if (nDim <= 0)
+    {
+        UMUQFAIL("Can not have dimension ", nDim, " <= 0!");
+    }
 
-	Order = PolynomialOrder;
-	if (Order < 0)
-	{
-		UMUQFAIL("Maximum accuracy order ", Order, " < 0!");
-	}
+    Order = PolynomialOrder;
+    if (Order < 0)
+    {
+        UMUQFAIL("Maximum accuracy order ", Order, " < 0!");
+    }
 
-	monomialSize = binomialCoefficient(nDim + Order, Order);
-	if (monomialSize == 0)
-	{
-		UMUQFAIL("The requested monomial size of zero is wrong!");
-	}
+    monomialSize = binomialCoefficient(nDim + Order, Order);
+    if (monomialSize == 0)
+    {
+        UMUQFAIL("The requested monomial size of zero is wrong!");
+    }
 
-	alpha.reset(nullptr);
+    alpha.reset(nullptr);
 }
 
 template <typename DataType>
 int polynomialBase<DataType>::binomialCoefficient(int const n, int const k)
 {
-	if ((k < 0) || (n < 0))
-	{
-		UMUQFAIL("Fatal error! k=", k, " or n=", n, " < 0!");
-	}
-	if (k < n)
-	{
-		if (k == 0)
-		{
-			return 1;
-		}
-		if ((k == 1) || (k == n - 1))
-		{
-			return n;
-		}
+    if ((k < 0) || (n < 0))
+    {
+        UMUQFAIL("Fatal error! k=", k, " or n=", n, " < 0!");
+    }
+    if (k < n)
+    {
+        if (k == 0)
+        {
+            return 1;
+        }
+        if ((k == 1) || (k == n - 1))
+        {
+            return n;
+        }
 
-		int mn = std::min(k, n - k);
-		int mx = std::max(k, n - k);
-		int value = mx + 1;
-		for (int i = 2; i <= mn; i++)
-		{
-			value = (value * (mx + i)) / i;
-		}
+        int mn = std::min(k, n - k);
+        int mx = std::max(k, n - k);
+        int value = mx + 1;
+        for (int i = 2; i <= mn; i++)
+        {
+            value = (value * (mx + i)) / i;
+        }
 
-		return value;
-	}
-	else if (k == n)
-	{
-		return 1;
-	}
+        return value;
+    }
+    else if (k == n)
+    {
+        return 1;
+    }
 
-	UMUQWARNING("The binomial coefficient is undefined for k=", k, " > n=", n, " !");
-	return 0;
+    UMUQWARNING("The binomial coefficient is undefined for k=", k, " > n=", n, " !");
+    return 0;
 }
 
 template <typename DataType>
 int *polynomialBase<DataType>::monomialBasis()
 {
-	UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
-	return nullptr;
+    UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
+    return nullptr;
 }
 
 template <typename DataType>
 int polynomialBase<DataType>::monomialValue(DataType const *x, DataType *&value)
 {
-	UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
-	return -1;
+    UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
+    return -1;
 }
 
 template <typename DataType>
 int polynomialBase<DataType>::monomialValue(DataType const *x, std::vector<DataType> &value)
 {
-	UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
-	return -1;
+    UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
+    return -1;
 }
 
 template <typename DataType>
 int polynomialBase<DataType>::monomialDerivative(int const *beta, DataType *&value)
 {
-	UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
-	return -1;
+    UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
+    return -1;
 }
 
 template <typename DataType>
 int polynomialBase<DataType>::monomialDerivative(int const *beta, std::vector<DataType> &value)
 {
-	UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
-	return -1;
+    UMUQFAIL("This is a virtual method in the base class and not implemented on purpose!");
+    return -1;
 }
 
 template <typename DataType>
 inline int polynomialBase<DataType>::monomialsize() const
 {
-	return monomialSize;
+    return monomialSize;
 }
 
 template <typename DataType>
 inline int polynomialBase<DataType>::dim() const
 {
-	return nDim;
+    return nDim;
 }
 
 template <typename DataType>
 inline int polynomialBase<DataType>::order() const
 {
-	return Order;
+    return Order;
 }
 
 template <typename DataType>
 bool polynomialBase<DataType>::graded_reverse_lexicographic_order(int *x)
 {
-	if (Order == 0)
-	{
-		return true;
-	}
+    if (Order == 0)
+    {
+        return true;
+    }
 
-	int asum = std::accumulate(x, x + nDim, 0);
+    int asum = std::accumulate(x, x + nDim, 0);
 
-	if (asum < 0)
-	{
-		UMUQFAILRETURN("Input sums < 0!");
-	}
+    if (asum < 0)
+    {
+        UMUQFAILRETURN("Input sums < 0!");
+    }
 
-	if (Order < asum)
-	{
-		UMUQFAILRETURN("Input sums > maximum degree r!");
-	}
+    if (Order < asum)
+    {
+        UMUQFAILRETURN("Input sums > maximum degree r!");
+    }
 
-	if (x[0] == Order)
-	{
-		x[0] = 0;
-		x[nDim - 1] = 0;
-	}
-	else
-	{
-		int i;
-		int tmp;
+    if (x[0] == Order)
+    {
+        x[0] = 0;
+        x[nDim - 1] = 0;
+    }
+    else
+    {
+        int i;
+        int tmp;
 
-		// Seeking the first index in which x > 0.
-		int j = 0;
-		for (i = 1; i < nDim; i++)
-		{
-			if (x[i] > 0)
-			{
-				j = i;
-				break;
-			}
-		}
+        // Seeking the first index in which x > 0.
+        int j = 0;
+        for (i = 1; i < nDim; i++)
+        {
+            if (x[i] > 0)
+            {
+                j = i;
+                break;
+            }
+        }
 
-		if (j == 0)
-		{
-			tmp = x[0];
-			x[0] = 0;
-			x[nDim - 1] = tmp + 1;
-		}
-		else if (j < nDim - 1)
-		{
-			x[j] = x[j] - 1;
-			tmp = x[0] + 1;
-			x[0] = 0;
-			x[j - 1] = x[j - 1] + tmp;
-		}
-		else
-		{
-			tmp = x[0];
-			x[0] = 0;
-			x[j - 1] = tmp + 1;
-			x[j] = x[j] - 1;
-		}
-	}
-	return true;
+        if (j == 0)
+        {
+            tmp = x[0];
+            x[0] = 0;
+            x[nDim - 1] = tmp + 1;
+        }
+        else if (j < nDim - 1)
+        {
+            x[j] = x[j] - 1;
+            tmp = x[0] + 1;
+            x[0] = 0;
+            x[j - 1] = x[j - 1] + tmp;
+        }
+        else
+        {
+            tmp = x[0];
+            x[0] = 0;
+            x[j - 1] = tmp + 1;
+            x[j] = x[j] - 1;
+        }
+    }
+    return true;
 }
 
 } // namespace polynomials
